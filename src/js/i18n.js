a5aaa87a0b908c8b4610550f1f2d5f9607d86b02
VicAlarDev <vic.alarcondev@gmail.com>
2024-02-26 14:59:23 -0400

    prueba 48

--- src/js/i18n.js
+++ src/js/i18n.js
@@ -25,29 +25,17 @@ const lngs = {
           .init({
             debug: true,
             fallbackLng: 'en',
+            ns: ['common', 'login'], // Especifica los namespaces que deseas cargar
+            defaultNS: 'login',
             resources: {
               en: {
                 translation: {
-                  login: {
-                    labelCorreo: 'Email',
-                    tittleButton: 'Continue',
-                    noAccount: 'Don\'t have an account?',
-                    forgotPassword: 'Forgot your password?',
-                    spanAccount: 'Sign up',
-                    title: "Kalstein Plus Development Enviroment"
-                }
+
                 }
               },
               es: {
                 translation: {
-                  login: {
-                    labelCorreo: 'Correo Electronico',
-                    tittleButton: 'Continuar',
-                    noAccount: '¿No tienes una cuenta?',
-                    forgotPassword: '¿Olvidaste tu contraseña?',
-                    spanAccount: 'Registrate',
-                    title: "Kalstein Plus Development Enviroment"
-                  }
+
                 }
               }
             }
