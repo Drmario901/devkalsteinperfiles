5fb87d67b3f06d502478fa1b910217ba50bcad6a
VicAlarDev <vic.alarcondev@gmail.com>
2024-02-26 09:46:35 -0400

    Script i18n,js

--- src/templates-php/accounts/login.php
+++ src/templates-php/accounts/login.php
@@ -92,50 +92,5 @@
     }
 </script>
 
-<script>
-    $(function () {
-    i18next
-        .use(i18nextBrowserLanguageDetector)
-        .init({
-            debug: true,
-            fallbackLng: 'en',
-            resources: {
-                en: {
-                    translation: {
-                        tittleButton: "Continue",
-                        forgotPassword: "Forgot password?",
-                        description: "This is a simple internationalization example.",
-                        labelEmail: "Email",
-                    }
-                },
-                es: {
-                    translation: {
-                        tittleButton: "Continuar",
-                        description: "Este es un ejemplo simple de internacionalización.",
-                        forgotPassword: "¿Olvidaste tu contraseña?",
-                        labelEmail: "Correo",
-                    }
-                }
-                // Añade más idiomas según sea necesario
-            }
-        }, function (err, t) {
-            if (err) return console.error(err);
-            // Inicialización de jquery-i18next
-            jqueryI18next.init(i18next, $, { useOptionsAttr: true });
-
-            // Localización de elementos HTML
-            $('body').localize();
-
-            // Cambiar idioma al seleccionar una opción en el select
-            $('#language-select').on('change', function () {
-                var lang = $(this).val();
-                i18next.changeLanguage(lang, function (err, t) {
-                    if (err) return console.error(err);
-                    // Localización de elementos HTML al cambiar de idioma
-                    $('body').localize();
-                });
-            });
-        });
-});
-
+<script src="../../js/i18n.js">
 </script>
