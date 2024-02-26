01ebc7cf9b7b27ea5361bbc13dd257bebdb4a4cd
VicAlarDev <vic.alarcondev@gmail.com>
2024-02-26 12:45:31 -0400

    pruebaaa idiomas

--- src/js/i18n.js
+++ src/js/i18n.js
@@ -14,18 +14,12 @@ const rerender = () => {
             resources: {
                 en: {
                     translation: {
-                        tittleButton: "Continue",
-                        forgotPassword: "Forgot password?",
-                        description: "This is a simple internationalization example.",
-                        labelEmail: "Email",
+
                     }
                 },
                 es: {
                     translation: {
-                        tittleButton: "Continuar",
-                        description: "Este es un ejemplo simple de internacionalización.",
-                        forgotPassword: "¿Olvidaste tu contraseña?",
-                        labelEmail: "Correo",
+
                     }
                 }
                 // Añade más idiomas según sea necesario
