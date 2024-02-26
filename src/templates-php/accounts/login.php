0270105a03b7579e02794ecc34c6daef0981cbf3
VicAlarDev <vic.alarcondev@gmail.com>
2024-02-26 09:39:38 -0400

    prueba i18n

--- src/templates-php/accounts/login.php
+++ src/templates-php/accounts/login.php
@@ -17,6 +17,10 @@
 <link rel="icon" type="image/x-icon" href="https://kalstein.us/wp-content/plugins/kalsteinPerfiles/src/images/favicon.ico">
 <script src="https://cdn.tailwindcss.com"></script>
 <script src='https://kit.fontawesome.com/3cff919dc3.js' crossorigin='anonymous'></script>
+<script src="https://cdn.jsdelivr.net/npm/i18next@21.6.10/i18next.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery-i18next@1.2.1/jquery-i18next.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/i18next-browser-languagedetector@6.1.3/i18nextBrowserLanguageDetector.min.js"></script>
+
 <div class='container'>
     <div id='contentLogin' class='row align-items-start'>
         <div class='col'>
@@ -27,7 +31,7 @@
                     <div class='col-md' style='margin-top: 1rem;'>
                         <div class='form-floating input-wrapper'>
                             <input type='email' class='form-control' id='emailUser' placeholder='name@example.com' style='height: 3rem; outline: 1px solid #213280; font-size: 1.4em; padding-right: 3rem;' autofocus>
-                            <label for='emailUser'>Correo electrónico</label>                               
+                            <label for='emailUser' data-i18n="labelEmail">Correo electrónico</label>                               
                         </div>
                         <div class='emailError' style='display: none;'><p style='color: #de3a46; font-weight: bold;'>El correo no es válido</p></div>
                         <div class='emailNoRegister' style='display: none;'><p style='color: #de3a46; font-weight: bold;'>Correo electrónico no registrado</p></div>
@@ -39,9 +43,9 @@
                                 <i class='fa-sharp fa-solid fa-eye eye-03'></i>
                             </div>
                             <div class='passwordIncorrect' style='display: none;'><p style='color: #de3a46; font-weight: bold;'>Contraseña incorrecta</p></div>
-                            <p style='margin-top: 0.5rem; margin-left: 10px; font-size: 1.2em;'><span class='forgotpw' style='color: #213280; cursor: pointer; font-weight: bold;'><a href='#' id='btnForgotPassword'>¿Olvidaste tu contraseña?</a></span></p>
+                            <p style='margin-top: 0.5rem; margin-left: 10px; font-size: 1.2em;'><span class='forgotpw' style='color: #213280; cursor: pointer; font-weight: bold;'><a href='#' id='btnForgotPassword' data-i18n="forgotPassword">¿Olvidaste tu contraseña?</a></span></p>
                         </div>
-                        <button type='button' class='btn' style='background-color: #213280; color: #fff; margin-top:1rem; width: 100%; height: 3rem;' id='btnContinueLogIn'>Continuar</button>
+                        <button type='button' class='btn' style='background-color: #213280; color: #fff; margin-top:1rem; width: 100%; height: 3rem;' id='btnContinueLogIn' data-i18n="tittleButton">Continuar</button>
                         <button type='button' class='btn' style='background-color: #213280; color: #fff; margin-top:1rem; width: 100%; height: 3rem; display: none;' id='btnContinueLogIn2'>Continuar</button>
                     </div>
                     <p style='margin-top: 1rem; margin-bottom: 4rem; font-size: 1.2em;'>¿No posees una cuenta? <span class='singup' style='color: #213280; cursor: pointer; font-weight: bold;'><a href='https://plataforma.kalstein.net/registrarse/<?php echo $search != '' ? "?search=$search" : '' ?>'>Regístrate</a></span></p>
@@ -81,4 +85,52 @@
         }
       }
     }
+</script>
+
+<script>
+    $(function () {
+    i18next
+        .use(i18nextBrowserLanguageDetector)
+        .init({
+            debug: true,
+            fallbackLng: 'en',
+            resources: {
+                en: {
+                    translation: {
+                        tittleButton: "Continue",
+                        forgotPassword: "Forgot password?",
+                        description: "This is a simple internationalization example.",
+                        labelEmail: "Email",
+                    }
+                },
+                es: {
+                    translation: {
+                        tittleButton: "Continuar",
+                        description: "Este es un ejemplo simple de internacionalización.",
+                        forgotPassword: "¿Olvidaste tu contraseña?",
+                        labelEmail: "Correo",
+                    }
+                }
+                // Añade más idiomas según sea necesario
+            }
+        }, function (err, t) {
+            if (err) return console.error(err);
+            // Inicialización de jquery-i18next
+            jqueryI18next.init(i18next, $, { useOptionsAttr: true });
+
+            // Localización de elementos HTML
+            $('body').localize();
+
+            // Cambiar idioma al seleccionar una opción en el select
+            $('#language-select').on('change', function () {
+                var lang = $(this).val();
+                i18next.changeLanguage(lang, function (err, t) {
+                    if (err) return console.error(err);
+                    // Localización de elementos HTML al cambiar de idioma
+                    $('body').localize();
+                });
+            });
+        });
+});
+
 </script>
