30c6e1eb27f13d344d50da6a3441d2d4447dd725
Edithson <edithson@kalstein.net>
2024-02-27 11:10:21 -0400

    7654e

--- set-cookie.php
+++ set-cookie.php
@@ -1,21 +1,24 @@
 <?php
 
-//AJAX IS MANDATORY.
-
+//AJAX IS MANDATORY
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['lang'])) {
-        $language = $_POST['lang'];
-        setcookie('language', $language, time() + (86400 * 30), "/");
-        echo "Language set to: " . $language . "\n";
-    }
 
-    if (isset($_POST['country'])) {
+    if (isset($_POST['lang']) && isset($_POST['country'])) {
+        $language = $_POST['lang'];
         $country = $_POST['country'];
+
+        setcookie('language', $language, time() + (86400 * 30), "/");
         setcookie('country', $country, time() + (86400 * 30), "/");
+
+        echo "Language set to: " . $language . "\n";
         echo "Country set to: " . $country . "\n";
+    } else {
+
+        echo "Both language and country need to be provided.\n";
     }
 } else {
-    echo "No data received.";
+
+    echo "No POST data received.\n";
 }
 
 ?>
