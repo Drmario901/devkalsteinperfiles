e9a516b0e84a129d248c1094edf13958dd0a0619
VicAlarDev <vic.alarcondev@gmail.com>
2024-02-27 12:42:39 -0400

    login arreglado

--- src/templates-php/accounts/login.php
+++ src/templates-php/accounts/login.php
@@ -55,7 +55,7 @@
                     </div>
                     <div style="display: flex; align-items: center;">
                         <p style="margin-top: 1rem; margin-bottom: 4rem; font-size: 1.2em; margin-right: 0.5rem;" data-i18n="account:noAccount">¿No posees una cuenta?</p>
-                        <span class='singup' style='color: #213280; cursor: pointer; font-weight: bold; margin-top: -2.8rem;'>
+                        <span class='singup' style='color: #213280; cursor: pointer; font-weight: bold; margin-top: -3rem;'>
                             <a data-i18n="account:registrate" href='https://plataforma.kalstein.net/registrarse/<?php echo $search != '' ? "?search=$search" : '' ?>'>Regístrate</a>
                         </span>
                     </div>
