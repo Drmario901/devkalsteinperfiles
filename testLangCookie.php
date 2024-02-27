aad06c32df7c8aa1beaeda76e74b87f2ab92114c
Edithson <edithson@kalstein.net>
2024-02-27 10:50:34 -0400

    Prueba cookie idioma y país

--- testLangCookie.php
+++ testLangCookie.php
@@ -1,26 +1,18 @@
 <?php
 
-//COOKIE IDIOMA
-/*if (isset($_GET['lang'])) {
+if (isset($_GET['lang']) && isset($_GET['country'])) {
     $language = $_GET['lang'];
-    setcookie('language', $language, time() + (86400 * 30), "/");
-    
-    echo "Language set to: " . $language;
-} else {
-    
-    echo isset($_COOKIE['language']) ? "Language: " . $_COOKIE['language'] : "No language set.";
-}
-
-echo "<br/>"; */
-
-//COOKIE PAÍS
-if (isset($_GET['country'])) {
     $country = $_GET['country'];
+
+    setcookie('language', $language, time() + (86400 * 30), "/");
     setcookie('country', $country, time() + (86400 * 30), "/");
 
+    echo "Language set to: " . $language;
+    echo "<br/>";
     echo "Country set to: " . $country;
 } else {
-    
+    echo isset($_COOKIE['language']) ? "Language: " . $_COOKIE['language'] : "No language set.";
+    echo "<br/>";
     echo isset($_COOKIE['country']) ? "Country: " . $_COOKIE['country'] : "No country set.";
 }
 
