05aa9088c5e693b48f433823e6af8c2e36e5624e
VicAlarDev <vic.alarcondev@gmail.com>
2024-02-27 12:41:10 -0400

    login arreglado?

--- src/templates-php/accounts/login.php
+++ src/templates-php/accounts/login.php
@@ -54,8 +54,8 @@
                         <button type='button' class='btn' style='background-color: #213280; color: #fff; margin-top:1rem; width: 100%; height: 3rem; display: none;' id='btnContinueLogIn2' data-i18n="account:tittleButton">Continuar</button>
                     </div>
                     <div style="display: flex; align-items: center;">
-                        <p style="margin-top: 1rem; margin-bottom: 4rem; font-size: 1.2em; margin-right: 0.5rem; margin-top: -2.8rem;" data-i18n="account:noAccount">¿No posees una cuenta?</p>
-                        <span class='singup' style='color: #213280; cursor: pointer; font-weight: bold;'>
+                        <p style="margin-top: 1rem; margin-bottom: 4rem; font-size: 1.2em; margin-right: 0.5rem;" data-i18n="account:noAccount">¿No posees una cuenta?</p>
+                        <span class='singup' style='color: #213280; cursor: pointer; font-weight: bold; margin-top: -2.8rem;'>
                             <a data-i18n="account:registrate" href='https://plataforma.kalstein.net/registrarse/<?php echo $search != '' ? "?search=$search" : '' ?>'>Regístrate</a>
                         </span>
                     </div>
