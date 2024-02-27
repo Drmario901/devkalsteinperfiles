0182449311521e66f3e6ec2fe108e999c9d2c74a
VicAlarDev <vic.alarcondev@gmail.com>
2024-02-27 09:18:40 -0400

    Tengo mucha fe

--- src/js/i18n.js
+++ src/js/i18n.js
@@ -11,53 +11,55 @@ const rerender = () => {
   });
 }
 
-$(function () {
+jQuery(document).ready(function($){
+  $(function () {
   i18next
-      // detect user language
-      .use(i18nextBrowserLanguageDetector)
-      // init i18next
-      .init({
-          debug: true,
-          fallbackLng: 'en',
-          ns: ['login', 'prueba'],
-          defaultNS: 'login', // Establecer el namespace predeterminado
-          resources: {
-              en: {
-              },
-              es: {
-              }
+    // detect user language
+    .use(i18nextBrowserLanguageDetector)
+    // init i18next
+    .init({
+      debug: true,
+      fallbackLng: 'en',
+      ns: ['login', 'prueba'],
+      defaultNS: 'login', // Establecer el namespace predeterminado
+      resources: {
+        en: {
+        },
+        es: {
+        }
+      }
+    }, (err, t) => {
+      if (err) return console.error(err);
+
+      // Cargar los archivos JSON para cada idioma y namespace
+      Promise.all(Object.keys(lngs).map(lng =>
+        Promise.all(i18next.options.ns.map(ns =>
+          fetch(`locales/${lng}/${ns}.json`).then(response => response.json())
+            .then(translation =>
+              i18next.addResourceBundle(lng, ns, translation)
+            )
+        ))
+      )).then(() => {
+        // Inicializar jquery-i18next
+        jqueryI18next.init(i18next, $, { useOptionsAttr: true });
+
+        // fill language switcher
+        Object.keys(lngs).map((lng) => {
+          const opt = new Option(lngs[lng].nativeName, lng);
+          if (lng === i18next.resolvedLanguage) {
+            opt.setAttribute("selected", "selected");
           }
-      }, (err, t) => {
-        if (err) return console.error(err);
-
-        // Cargar los archivos JSON para cada idioma y namespace
-        Promise.all(Object.keys(lngs).map(lng =>
-            Promise.all(i18next.options.ns.map(ns =>
-                fetch(`locales/${lng}/${ns}.json`).then(response => response.json())
-                    .then(translation =>
-                        i18next.addResourceBundle(lng, ns, translation)
-                    )
-            ))
-        )).then(() => {
-            // Inicializar jquery-i18next
-            jqueryI18next.init(i18next, $, { useOptionsAttr: true });
-
-            // fill language switcher
-            Object.keys(lngs).map((lng) => {
-                const opt = new Option(lngs[lng].nativeName, lng);
-                if (lng === i18next.resolvedLanguage) {
-                    opt.setAttribute("selected", "selected");
-                }
-                $('#languageSwitcher').append(opt);
-            });
-            $('#languageSwitcher').change((a, b, c) => {
-                const chosenLng = $(this).find("option:selected").attr('value');
-                i18next.changeLanguage(chosenLng, () => {
-                    rerender();
-                });
-            });
-
-            rerender();
+          $('#languageSwitcher').append(opt);
         });
+        $('#languageSwitcher').change((a, b, c) => {
+          const chosenLng = $(this).find("option:selected").attr('value');
+          i18next.changeLanguage(chosenLng, () => {
+            rerender();
+          });
+        });
+
+        rerender();
       });
+    });
+  });
 });
