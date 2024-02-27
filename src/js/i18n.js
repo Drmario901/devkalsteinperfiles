6510702836cc0f8f7a416a549337c7d10f19e841
VicAlarDev <vic.alarcondev@gmail.com>
2024-02-27 09:14:38 -0400

    Tengo fe

--- src/js/i18n.js
+++ src/js/i18n.js
@@ -1,76 +1,63 @@
-
 const lngs = {
-    en: { nativeName: 'English' },
-    es: { nativeName: 'Spanish' }
-  };
-  
+  en: { nativeName: 'English' },
+  es: { nativeName: 'Spanish' }
+};
 
-  const rerender = () => {
-    // Traduce cada elemento individualmente
-    $('[data-i18n]').each(function() {
-        const key = $(this).attr('data-i18n');
-        $(this).text(i18next.t(key));
-    });
+const rerender = () => {
+  // Traduce cada elemento individualmente
+  $('[data-i18n]').each(function() {
+      const key = $(this).attr('data-i18n');
+      $(this).text(i18next.t(key));
+  });
 }
 
-
-
-  jQuery(document).ready(function($){
-    $(function () {
-        // use plugins and options as needed, for options, detail see
-        i18next
-          // detect user language
-          .use(i18nextBrowserLanguageDetector)
-          // init i18next
-          .init({
-            debug: true,
-            fallbackLng: 'en',
-            defaultNS: 'login',
-            ns: ['login'], // Especifica los namespaces que deseas cargar
-            resources: {
+$(function () {
+  i18next
+      // detect user language
+      .use(i18nextBrowserLanguageDetector)
+      // init i18next
+      .init({
+          debug: true,
+          fallbackLng: 'en',
+          ns: ['login', 'prueba'],
+          defaultNS: 'login', // Establecer el namespace predeterminado
+          resources: {
               en: {
               },
               es: {
               }
           }
-          }, (err, t) => {
-            if (err) return console.error(err);
+      }, (err, t) => {
+        if (err) return console.error(err);
+
+        // Cargar los archivos JSON para cada idioma y namespace
+        Promise.all(Object.keys(lngs).map(lng =>
+            Promise.all(i18next.options.ns.map(ns =>
+                fetch(`locales/${lng}/${ns}.json`).then(response => response.json())
+                    .then(translation =>
+                        i18next.addResourceBundle(lng, ns, translation)
+                    )
+            ))
+        )).then(() => {
+            // Inicializar jquery-i18next
             jqueryI18next.init(i18next, $, { useOptionsAttr: true });
-      
+
             // fill language switcher
             Object.keys(lngs).map((lng) => {
-              const opt = new Option(lngs[lng].nativeName, lng);
-              if (lng === i18next.resolvedLanguage) {
-                opt.setAttribute("selected", "selected");
-              }
-              $('#languageSwitcher').append(opt);
+                const opt = new Option(lngs[lng].nativeName, lng);
+                if (lng === i18next.resolvedLanguage) {
+                    opt.setAttribute("selected", "selected");
+                }
+                $('#languageSwitcher').append(opt);
             });
-
-            Promise.all(Object.keys(lngs).map(lng =>
-              Promise.all(i18next.options.ns.map(ns =>
-                  fetch(`https://dev.kalstein.plus/plataforma/wp-content/plugins/kalsteinPerfiles/src/locales/${lng}/${ns}.json`)
-                      .then(response => response.json())
-                      .then(translation => {
-                          const resources = {};
-                          console.log(translation);
-                          console.log(ns);
-                          resources[ns] = translation;
-                          i18next.addResourceBundle(lng, ns, resources);
-                      })
-              ))
-          )).then(() => {
-              // Una vez cargados todos los recursos, se llama a rerender
-              rerender();
-          });
-
             $('#languageSwitcher').change((a, b, c) => {
-              const chosenLng = $(this).find("option:selected").attr('value');
-              i18next.changeLanguage(chosenLng, () => {
-                rerender();
-              });
+                const chosenLng = $(this).find("option:selected").attr('value');
+                i18next.changeLanguage(chosenLng, () => {
+                    rerender();
+                });
             });
-      
+
             rerender();
-          });
+        });
       });
-  });
+});
