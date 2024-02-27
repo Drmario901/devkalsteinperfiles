fd8c1912d9cddc224d75cba3875689ea713dcfda
Edithson <edithson@kalstein.net>
2024-02-27 09:55:03 -0400

    Test cookie lang and country.

--- testLangCookie.php
+++ testLangCookie.php
@@ -1,13 +1,25 @@
 <?php
 
+//COOKIE IDIOMA
 if (isset($_GET['lang'])) {
     $language = $_GET['lang'];
 
     setcookie('language', $language, time() + (86400 * 30), "/");
 
-    header('Location: readLanguage.php');
+    echo "Language: " . $_COOKIE['language'];
 } else {
-    echo "No language.";
+    echo "No country.";
+}
+
+//COOKIE PAÍS
+if (isset($_GET['country'])) {
+    $language = $_GET['country'];
+
+    setcookie('country', $language, time() + (86400 * 30), "/");
+
+    echo "Country: " . $_COOKIE['country'];
+} else {
+    echo "No country.";
 }
 
 ?>
