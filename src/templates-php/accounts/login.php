8a237a0ad2a966b520c093462696dc3432a3e791
Edithson <edithson@kalstein.net>
2024-02-27 10:06:33 -0400

    Prueba de cookie en Login.

--- src/templates-php/accounts/login.php
+++ src/templates-php/accounts/login.php
@@ -10,6 +10,29 @@
         }
     }
 
+    //COOKIE IDIOMA
+    if (isset($_GET['lang'])) {
+        $language = $_GET['lang'];
+        setcookie('language', $language, time() + (86400 * 30), "/");
+        
+        echo "Language: " . $language;
+    } else {
+
+        echo isset($_COOKIE['language']) ? "Language: " . $_COOKIE['language'] : "No language set.";
+    }
+
+    echo "<br/>"; 
+
+    //COOKIE PAÍS
+    if (isset($_GET['country'])) {
+        $country = $_GET['country'];
+        setcookie('country', $country, time() + (86400 * 30), "/");
+
+        echo "Country: " . $country;
+    } else {
+        echo isset($_COOKIE['country']) ? "Country: " . $_COOKIE['country'] : "No country set.";
+    }
+
     session_write_close();
 ?>
 
