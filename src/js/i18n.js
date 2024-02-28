11a6dd3e7f38e6c6c75c7304d4e3c1a04d772031
VicAlarDev <vic.alarcondev@gmail.com>
2024-02-28 14:35:31 -0400

    testttt

--- src/js/i18n.js
+++ src/js/i18n.js
@@ -1,3 +1,5 @@
+import sprintf from 'i18next-sprintf-postprocessor';
+
 const lngs = {
   en: { nativeName: 'English' },
   es: { nativeName: 'Spanish' },
@@ -11,50 +13,76 @@ const lngs = {
   fr: { nativeName: 'French' }
 };
 
-jQuery(document).ready(function($) {
-  // Función para obtener el idioma de la cookie
-  const getLanguage = () => {
-    const cookie = document.cookie.split('; ').find(row => row.startsWith('language='));
-    return cookie ? cookie.split('=')[1] : 'en';
-  };
+// Obtener language from cookie
+const getLanguage = () => {
+  const cookie = document.cookie.split('; ').find(row => row.startsWith('language='));
+  return cookie ? cookie.split('=')[1] : 'en';
+};
 
-  // Función para rerenderizar las traducciones
-  const rerender = () => {
+const rerender = () => {
+  // Traduce cada elemento individualmente
+  jQuery(document).ready(function($){
     $('[data-i18n]').each(function() {
-      const key = $(this).data('i18n');
-      $(this).text(i18next.t(key));
+        const key = $(this).attr('data-i18n');
+        $(this).text(i18next.t(key));
     });
-  };
+  });
+}
 
-  // Inicializar i18next y jquery-i18next
-  i18next.init({
-    debug: true,
-    lng: getLanguage(),
-    fallbackLng: 'en',
-    ns: ['account', 'prueba', 'client', 'support', 'moderator', 'distribuidor'],
-    defaultNS: 'account', // Establecer el namespace predeterminado
-    resources: {}
-  }, (err, t) => {
-    if (err) return console.error(err);
+// Inicializar i18next y jquery-i18next
+jQuery(document).ready(function($){
+  $(function () {
+  i18next
+  .use(sprintf)
+    // detect user language
+    // init i18next
+    .init({
+      debug: true,
+      lng: getLanguage(),
+      fallbackLng: 'en',
+      ns: ['account', 'prueba', 'client', 'support', 'moderator', 'distribuidor'],
+      defaultNS: 'account', // Establecer el namespace predeterminado
+      resources: {}
+    }, (err, t) => {
+      if (err) return console.error(err);
 
-    // Cargar los archivos JSON para cada idioma y namespace
-    Promise.all(Object.keys(lngs).map(lng =>
-      Promise.all(i18next.options.ns.map(ns =>
-        fetch(`https://dev.kalstein.plus/plataforma/wp-content/plugins/kalsteinPerfiles/src/locales/${lng}/${ns}.json`)
-        .then(response => response.json())
-        .then(translation => i18next.addResourceBundle(lng, ns, translation))
-      ))
-    )).then(() => {
-      // Después de cargar todas las traducciones, cambiar el idioma y rerenderizar
-      const currentLanguage = getLanguage();
-      if (i18next.resolvedLanguage !== currentLanguage) {
-        console.log('Changing language to', currentLanguage);
-        i18next.changeLanguage(currentLanguage, () => {
-          rerender();
+      // Cargar los archivos JSON para cada idioma y namespace
+      Promise.all(Object.keys(lngs).map(lng =>
+        Promise.all(i18next.options.ns.map(ns =>
+          fetch(`https://dev.kalstein.plus/plataforma/wp-content/plugins/kalsteinPerfiles/src/locales/${lng}/${ns}.json`).then(response => response.json())
+            .then(translation =>
+              i18next.addResourceBundle(lng, ns, translation)
+            )
+        ))
+      )).then(() => {
+        // Inicializar jquery-i18next
+        
+        //Check if the language from cookie is the same as the language initialized
+        if (i18next.resolvedLanguage !== getLanguage()) {
+            i18next.changeLanguage(getLanguage(), () => {
+            rerender();
+          });
+        }
+
+        jqueryI18next.init(i18next, $, { useOptionsAttr: true });
+
+        // fill language switcher
+        /* Object.keys(lngs).map((lng) => {
+          const opt = new Option(lngs[lng].nativeName, lng);
+          if (lng === i18next.resolvedLanguage) {
+            opt.setAttribute("selected", "selected");
+          }
+          $('#languageSwitcher').append(opt);
         });
-      } else {
+        $('#languageSwitcher').change((a, b, c) => {
+          const chosenLng = $(this).find("option:selected").attr('value');
+          i18next.changeLanguage(chosenLng, () => {
+            rerender();
+          });
+        }); */
+
         rerender();
-      }
+      });
     });
   });
 });
