855877f26123be016b1d0d3dd6f34d7f530cfd0c
VicAlarDev <vic.alarcondev@gmail.com>
2024-02-29 08:08:02 -0400

    modify links distribuidor

--- src/templates-php/distributor/navbar.php
+++ src/templates-php/distributor/navbar.php
@@ -40,7 +40,7 @@
             <ul class="navbar-list">
                 <div class="d-flex flex-row">
                     <li title="Abrir editor de la Tienda Virtual">
-                        <a href="https://plataforma.kalstein.net/template-editor/dashboard.php" class='navbar-link icon-box'>
+                        <a href="https://dev.kalstein.plus/plataforma/template-editor/dashboard.php" class='navbar-link icon-box'>
                             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 640 512" class="icon_editor_tienda">
                                 <path d="M36.8 192H603.2c20.3 0 36.8-16.5 36.8-36.8c0-7.3-2.2-14.4-6.2-20.4L558.2 21.4C549.3 8 534.4 0 518.3 0H121.7c-16 0-31 8-39.9 21.4L6.2 134.7c-4 6.1-6.2 13.2-6.2 20.4C0 175.5 16.5 192 36.8 192zM64 224V384v80c0 26.5 21.5 48 48 48H336c26.5 0 48-21.5 48-48V384 224H320V384H128V224H64zm448 0V480c0 17.7 14.3 32 32 32s32-14.3 32-32V224H512z"/>
                             </svg>
@@ -48,7 +48,7 @@
                     </li>
 
                     <li>
-                        <a href='https://plataforma.kalstein.net/index.php/distribuidor/inbox/' class='navbar-link icon-box'>
+                        <a href='https://dev.kalstein.plus/plataforma/index.php/distribuidor/inbox/' class='navbar-link icon-box'>
                             <span class='material-symbols-rounded icon position-relative'>
                                 mail
                                 <span id='messagesBaloon' class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style='font-family: sans-serif; font-size: 10px' hidden>
@@ -59,7 +59,7 @@
                     </li>
         
                     <li>
-                        <a href='https://plataforma.kalstein.net/index.php/distribuidor/configuracion/' id='btnEditProfilePr01' class='navbar-link icon-box'>
+                        <a href='https://dev.kalstein.plus/plataforma/index.php/distribuidor/configuracion/' id='btnEditProfilePr01' class='navbar-link icon-box'>
                             <span class='material-symbols-rounded  icon'>settings</span>
                         </a>
                     </li>
@@ -92,50 +92,50 @@
             <ul class="navbar-list">
                 
                 <li>
-                    <a id="link-home" href="https://plataforma.kalstein.net/index.php/distribuidor/dashboard" class="navbar-link icon-box">
+                    <a id="link-home" href="https://dev.kalstein.plus/plataforma/index.php/distribuidor/dashboard" class="navbar-link icon-box">
                         <span>Dashboard</span>
                     </a>
                 </li>
     
                 <li>
-                    <a id="link-stock" href="https://plataforma.kalstein.net/index.php/distribuidor/productos" class="navbar-link icon-box">
+                    <a id="link-stock" href="https://dev.kalstein.plus/plataforma/index.php/distribuidor/productos" class="navbar-link icon-box">
                         <span data-i18n="distribuidor:linkProducts">Productos</span>
                     </a>
                 </li>
     
                 <li>
-                    <a id="link-list-order" href="https://plataforma.kalstein.net/index.php/distribuidor/ordenes" class="navbar-link icon-box">
+                    <a id="link-list-order" href="dev.kalstein.plus/plataforma/index.php/distribuidor/ordenes" class="navbar-link icon-box">
                         <span data-i18n="distribuidor:linkOrders">Órdenes</span>
                     </a>
                 </li>
 
                 <li>
-                    <a id="link-catalogs" href="https://plataforma.kalstein.net/index.php/distribuidor/catalogos" class="navbar-link icon-box">
+                    <a id="link-catalogs" href="https://dev.kalstein.plus/plataforma/index.php/distribuidor/catalogos" class="navbar-link icon-box">
                         <span data-i18n="distribuidor:linkCatalogs">Catálogos</span>
                     </a>
                 </li>
 
                 <li>
-                    <a id="link-shop" href="https://plataforma.kalstein.net/index.php/distribuidor/tienda" class="navbar-link icon-box">
+                    <a id="link-shop" href="https://dev.kalstein.plus/plataforma/index.php/distribuidor/tienda" class="navbar-link icon-box">
                         <span data-i18n="distribuidor:linkShop">Tienda</span>
                     </a>
                 </li>
     
                 <li>
-                    <a id="link-sales" href="https://plataforma.kalstein.net/index.php/distribuidor/ventas" class="navbar-link icon-box">
+                    <a id="link-sales" href="https://dev.kalstein.plus/plataforma/index.php/distribuidor/ventas" class="navbar-link icon-box">
                         <span data-i18n="distribuidor:linkRepVentas">Reporte de ventas</span>
                     </a>
                 </li>
 
                 <li class='generate-quote'> <!-- only style class-->
-                    <a id='btnGenQuote' href="https://plataforma.kalstein.net/index.php/distribuidor/productos/agregar" class='navbar-link icon-box text-white' style='color: white !important;'>
+                    <a id='btnGenQuote' href="https://dev.kalstein.plus/plataforma/index.php/distribuidor/productos/agregar" class='navbar-link icon-box text-white' style='color: white !important;'>
                         <span data-i18n="distribuidor:btnAgregarProducto">AGREGAR UN PRODUCTO</span>
                     </a>
                 </li>
             </ul>
         </div>
-        <script src="https://plataforma.kalstein.net/wp-content/plugins/kalsteinPerfiles/src/manufacturer/js/inbox-notification.js"></script>
-        <script src="https://plataforma.kalstein.net/wp-content/plugins/kalsteinCotizacion/assets/js/script.cot2.lite.js"></script>
+        <script src="https://dev.kalstein.plus/plataforma/wp-content/plugins/kalsteinPerfiles/src/manufacturer/js/inbox-notification.js"></script>
+        <script src="https://dev.kalstein.plus/plataforma/wp-content/plugins/kalsteinCotizacion/assets/js/script.cot2.lite.js"></script>
     </nav>
 </div>
 <div class="container flex-column">
