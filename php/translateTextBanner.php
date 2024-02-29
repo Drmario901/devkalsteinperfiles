0ac063491c035b74c047cc8ecb485bc42e9d9d29
Jorge <jorgeab.hope@gmail.com>
2024-02-29 11:54:38 -0400

    xddddd

--- php/translateTextBanner.php
+++ php/translateTextBanner.php
@@ -5,9 +5,9 @@ require __DIR__. '/../../../php/translations.php';
 function translateTextBanner($banner) {
     // Include the translations file
     $language = isset($_COOKIE['language']) ? $_COOKIE['language'] : 'en';
-            // Determinar el texto del banner según el idioma
-            $banner_text_translation = isset($translations[$language][$banner]) ? $translations[$language][$banner] : $translations['en'][$banner];
-            // Incluir el banner.php pasando el texto traducido y el nombre del usuario
-            $banner_text = $banner_text_translation;
-            return $banner_text;
+    // Determinar el texto del banner según el idioma
+    $banner_text_translation = isset($translations[$language][$banner]) ? $translations[$language][$banner] : $translations['en'][$banner];
+    // Incluir el banner.php pasando el texto traducido y el nombre del usuario
+    $banner_text = $banner_text_translation;
+    return $banner_text;
 }
