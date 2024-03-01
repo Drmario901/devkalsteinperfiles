4cfd06996558a6963e20ad41f013785bf3e9b946
VicAlarDev <vic.alarcondev@gmail.com>
2024-03-01 13:28:35 -0400

    aaaaa

--- src/js/dashboard.script.js
+++ src/js/dashboard.script.js
@@ -10,6 +10,7 @@ jQuery(document).ready(function($){
             .then(response => response.json())
             .then(translation => {
                 // save in a global variable
+                console.log(translation, 'translation')
                 alertsTranslations[lng] = translation;
             });
     }; 
