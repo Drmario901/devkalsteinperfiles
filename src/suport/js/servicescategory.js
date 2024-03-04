ac7c6fa1619a661278f5b15c6970f92b7070138c
VicAlarDev <vic.alarcondev@gmail.com>
2024-03-04 12:52:48 -0400

    services support

--- src/suport/js/servicescategory.js
+++ src/suport/js/servicescategory.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
     category();  
     function category(consulta) {
         $.ajax({
-            url: 'https://dev.kalstein.plus/plataforma/wp-content/plugins/kalsteinPerfiles/php/suport/category_product.php',
+            url: 'https://dev.kalstein.plus/wp-content/plugins/kalsteinPerfiles/php/suport/category_product.php',
             type: 'POST',
             data: { consulta },
         })
