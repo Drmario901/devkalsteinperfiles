be340e10c21847d9462f013dd678f379cd4fe841
VicAlarDev <vic.alarcondev@gmail.com>
2024-03-05 09:08:37 -0400

    prueba

--- php/catalog.php
+++ php/catalog.php
@@ -78,11 +78,11 @@ $html .= "
     <div id='currentPageIndicatorCatalog'>Page: $page</div>
     <form id='form-previous-catalog' action='' method='get' style='margin-right: 8px'>
         <input id='previous' type='hidden' name='o' value='$prevPage'>
-        <input type='submit' style='color: black !important; border: 1px solid #555 !important' data-i17n='client:previo' value='&laquo; Anterior'>
+        <input type='submit' style='color: black !important; border: 1px solid #555 !important' value='&laquo; Anterior'>
     </form>
     <form id='form-next-catalog' action='' method='get'>
         <input id='next' class='next' type='hidden' name='o' value='$nextPage'>
-        <input type='submit' style='color: black !important; border: 1px solid #555 !important' data-i17n='client:next' value='Próximo &raquo;'>
+        <input type='submit' style='color: black !important; border: 1px solid #555 !important' value='Próximo &raquo;'>
     </form>
 </div>
 <input id='hiddenPage' type='hidden' value='$page'>";
