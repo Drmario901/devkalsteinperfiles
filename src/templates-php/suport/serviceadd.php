d6429c4310263d9c520c39e977dbd38475858af0
Diego Horesok <diegolancer27@gmail.com>
2024-03-06 15:07:33 -0400

    lineas de error para probar

--- src/templates-php/suport/serviceadd.php
+++ src/templates-php/suport/serviceadd.php
@@ -1,5 +1,8 @@
 <div class="container">
     <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
     // Evitar iniciar sesión más de una vez
     if (session_status() == PHP_SESSION_NONE) {
         session_start();
