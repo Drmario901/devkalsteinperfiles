a5d4ba9667ea601a870997fc992768c8bce9288b
Diego Horesok <diegolancer27@gmail.com>
2024-03-07 13:52:21 -0400

    cambios category_product

--- php/suport/category_product.php
+++ php/suport/category_product.php
@@ -1,26 +1,31 @@
-<?php
-require 'conexion.php';
+<?php 
+	require __DIR__ . '/../conexion.php';
 
+	$salida = "<option selected value='0' style='color: #000 !important;'>Choisir une option</option>";
 
-
-
-$salida = "<option selected value='' style='color: #000 !important;' >Elige una opción</option>";
-$consulta = "SELECT DISTINCT catalog_category_es FROM `wp_catalogs_es` ORDER BY `catalog_category_es` ASC";
-
-$resultado = $conexion->query($consulta);
-$categorys = array();
-
-if ($resultado->num_rows > 0) {
-    while ($row = $resultado->fetch_assoc()) {
-        $category_description = $row['catalog_category_es'];
-        array_push($categorys, $category_description);
-    }
-}
-
-foreach ($categorys as $value) {
-    $salida .= "<option value='$value' style='color: #000 !important;'>$value</option>";
-}
-
-echo $salida;
-$conexion->close();
-?>
+	$consulta = "SELECT * FROM `wp_categories` ORDER BY `wp_categories`.`categorie_description` ASC";		
+		
+	$resultado = $conexion->query($consulta);
+	$categorys = [];	
+		
+	if ($resultado->num_rows > 0) {
+		while ($value = $resultado->fetch_assoc()) {
+            //$products = $value["categorie_description"];
+           if (in_array($value['categorie_description_fr'], $categorys)){
+			}else{
+				array_push($categorys, $value['categorie_description_fr']);
+			}
+			
+		}
+	} 
+	
+	foreach ($categorys as $value) {
+		$salida.="                    
+		<option style='color: #000 !important;' value='".$value."'>".$value."</option>
+			";
+	}
+	
+	echo $salida;
+	print_r($categorys);
+	$conexion->close();
+ ?>
