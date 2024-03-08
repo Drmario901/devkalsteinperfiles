b0f9bb8ced46674c8bb8bae6590a111e41243fa5
VicAlarDev <vic.alarcondev@gmail.com>
2024-03-08 11:16:52 -0400

    prueba

--- src/templates-php/client/wooPreview.php
+++ src/templates-php/client/wooPreview.php
@@ -552,22 +552,17 @@ if (!empty($row[$productDescription])){
 
     //Guardar en la base de datos
     $updateQuery2 = "UPDATE wp_k_products SET $productDescription = ? WHERE product_aid = ?";
-    $safeValue = $conexion->real_escape_string($updateQuery2);
-
-    //query
-    $query = "UPDATE wp_k_products SET 'product_description_'.$lang = '$translatedDescription' WHERE product_aid = '$p_id'";
 
     $stmt = $conexion->prepare($updateQuery2);
     $stmt->bind_param('si', $translatedDescription, $p_id);
 
     if ($stmt->execute()) {
-        echo "Record updated successfully";
+        //echo "Record updated successfully";
     } else {
-        echo "Error updating record: ". $stmt->error;
+       // echo "Error updating record: ". $stmt->error;
 
     }
 }
-
 
 
 $technicalDescriptionLang = 'product_technical_description_'.$lang;
