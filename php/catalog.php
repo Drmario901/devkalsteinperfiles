ea7602f95b95f6fbcce4b577d3fabb50f8fe6b37
Diego Horesok <diegolancer27@gmail.com>
2024-03-08 12:57:01 -0400

    prueba en catalog.php

--- php/catalog.php
+++ php/catalog.php
@@ -1,6 +1,12 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require __DIR__ . '/conexion.php';
 require __DIR__ . '/translateText.php';
+
+
 
 /* require 'translateText.php';
  */
