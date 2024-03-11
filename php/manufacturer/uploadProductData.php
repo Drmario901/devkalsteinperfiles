71b662f48d0c19927de08d79ca79b794cfcb94c3
VicAlarDev <vic.alarcondev@gmail.com>
2024-03-11 10:57:00 -0400

    magia

--- php/manufacturer/uploadProductData.php
+++ php/manufacturer/uploadProductData.php
@@ -1,5 +1,8 @@
 <?php
     session_start();
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
     require __DIR__ . '/../conexion.php';
     require __DIR__.'/validateProductData.php';
 
