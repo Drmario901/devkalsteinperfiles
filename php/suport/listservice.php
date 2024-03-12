8c778a40617a485fddb819a9790f038daa9aeea3
Diego Horesok <diegolancer27@gmail.com>
2024-03-12 11:54:19 -0400

    propbando...

--- php/suport/listservice.php
+++ php/suport/listservice.php
@@ -1,3 +1,4 @@
+
 <?php
     ini_set( 'display_errors', 1 );
     ini_set('error_reporting', E_ALL);
@@ -5,101 +6,104 @@
     session_start();
     require __DIR__ . '/../conexion.php';
 
-    $resp = array();
-    $acc_id = $_SESSION['emailAccount'];
-    $cate = $_POST['category'];
-    $a = $_POST['status'];
-    $resp['html'] = '';
+    try {
+        $resp = array();
+        $acc_id = $_SESSION['emailAccount'];
+        $cate = $_POST['category'];
+        $a = $_POST['status'];
+        $resp['html'] = '';
 
-    $perPage = 5;
-    $page = $_POST['page'];
+        $perPage = 5;
+        $page = $_POST['page'];
 
-    $offset = ($page - 1) * $perPage;
-    $limit = $perPage;
+        $offset = ($page - 1) * $perPage;
+        $limit = $perPage;
 
-    $queryAll = "SELECT COUNT(*) count FROM wp_servicios WHERE SE_correo = '$acc_id'";
+        $queryAll = "SELECT COUNT(*) count FROM wp_servicios WHERE SE_correo = '$acc_id'";
 
-    if ($cate != '0'){
-        $queryAll .= "AND SE_category LIKE '%$cate%'";
-    }
-
-    if ($a != '0'){
-        $queryAll .= "AND SE_estado = '$a'";
-    }
-
-    $resultAll = $conexion->query($queryAll);
-
-    // si estoy pidiendo una página que sobrepasa el total, regreso hasta la última página válida
-    $All = $resultAll->fetch_assoc()['count'];
-    if($All != null){
-        if ($All <= ($page - 1) * $perPage){
-            $page = intdiv($All, $perPage) + ($All % $perPage > 0 ? 1 : 0);
+        if ($cate != '0'){
+            $queryAll .= " AND SE_category LIKE '%$cate%'";
         }
-        $page = max(intval($page), 1);
-    }
 
-    // establezco los límites de la paginación
-    $resp['total'] = $All;
-    $resp['pagina'] = $page;
-    $offset = ($page - 1) * $perPage;
-    $limit = $perPage;
+        if ($a != '0'){
+            $queryAll .= " AND SE_estado = '$a'";
+        }
 
-    $consulta = "SELECT * FROM wp_servicios WHERE SE_correo = '$acc_id'";
+        $resultAll = $conexion->query($queryAll);
 
-    if ($cate != '0'){
-        $consulta .= "AND SE_category LIKE '%$cate%'";
-    }
+        $All = $resultAll->fetch_assoc()['count'];
+        if($All != null){
+            if ($All <= ($page - 1) * $perPage){
+                $page = intdiv($All, $perPage) + ($All % $perPage > 0 ? 1 : 0);
+            }
+            $page = max(intval($page), 1);
+        }
 
-    if ($a != '0'){
-        $consulta .= "AND SE_estado = '$a'";
-    }
+        $resp['total'] = $All;
+        $resp['pagina'] = $page;
+        $offset = ($page - 1) * $perPage;
+        $limit = $perPage;
 
-    $consulta .="LIMIT $offset, $limit";
+        $consulta = "SELECT * FROM wp_servicios WHERE SE_correo = '$acc_id'";
 
-    $resultado = $conexion->query($consulta);
+        if ($cate != '0'){
+            $consulta .= " AND SE_category LIKE '%$cate%'";
+        }
 
-    if ($resultado->num_rows > 0) {
-        $i = $offset + 1; 
-        while ($value = $resultado->fetch_assoc()) {
-            $id = $value['SE_id'];
-            $service = $value['SE_servicio'];
-            $usuario = $value['SE_agente_soporte'];
-            $correo = $value['SE_correo'];
-            $date = $value['SE_fecha'];
-            $date = new DateTime($date);
-            $fecha = date_format($date, 'Y-m-d');
-            $estado = $value['SE_estado'];
-            $category = $value['SE_category'];
-            $desccription = $value['SE_description'];
-            $company = $value['SE_company'];
-            $resp['html'] .= <<<HTML
-                <tr>
-                    <td>$id</td>
-                    <td>$service</td>
-                    <td>$category</td>
-                    <td>$company</td>
-                    <td>$usuario</td>
-                    <td>$correo</td>
-                    <td> <button class='material-symbols-rounded' type='button' name='view' id='btn-service-details' value='$id'>visibility</button></td>
-                    <td>$fecha</td>
-                    <td>$estado</td>
-                    <td>
-                        <button class='material-symbols-rounded' id='btnEditService' value='$id'>Edit</button>
-                        <button class='material-symbols-rounded' id='btnDeleteService' value='$id'>Delete</button>
-                    </td>
-                </tr>
+        if ($a != '0'){
+            $consulta .= " AND SE_estado = '$a'";
+        }
+
+        $consulta .=" LIMIT $offset, $limit";
+
+        $resultado = $conexion->query($consulta);
+
+        if ($resultado->num_rows > 0) {
+            $i = $offset + 1; 
+            while ($value = $resultado->fetch_assoc()) {
+                $id = $value['SE_id'];
+                $service = $value['SE_servicio'];
+                $usuario = $value['SE_agente_soporte'];
+                $correo = $value['SE_correo'];
+                $date = $value['SE_fecha'];
+                $date = new DateTime($date);
+                $fecha = date_format($date, 'Y-m-d');
+                $estado = $value['SE_estado'];
+                $category = $value['SE_category'];
+                $desccription = $value['SE_description'];
+                $company = $value['SE_company'];
+                $resp['html'] .= <<<HTML
+                    <tr>
+                        <td>$id</td>
+                        <td>$service</td>
+                        <td>$category</td>
+                        <td>$company</td>
+                        <td>$usuario</td>
+                        <td>$correo</td>
+                        <td> <button class='material-symbols-rounded' type='button' name='view' id='btn-service-details' value='$id'>visibility</button></td>
+                        <td>$fecha</td>
+                        <td>$estado</td>
+                        <td>
+                            <button class='material-symbols-rounded' id='btnEditService' value='$id'>Edit</button>
+                            <button class='material-symbols-rounded' id='btnDeleteService' value='$id'>Delete</button>
+                        </td>
+                    </tr>
+                HTML;
+                $i++; 
+            }
+        } else {
+            $resp['html'] = <<<HTML
+                <div class='row contentNoDataQuote' style='position: relative; top: 0; left: 0; right: 0; bottom: 0; width: 100%;'>
+                    <center><span class='material-symbols-rounded icon'>sentiment_dissatisfied</span></center>
+                    <center><p style='color: #000;'>Aucune donnée n'a été trouvée</p></center>
+                </div>
             HTML;
-            $i++; 
         }
-    } else {
-        $resp['html'] = <<<HTML
-            <div class='row contentNoDataQuote' style='position: relative; top: 0; left: 0; right: 0; bottom: 0; width: 100%;'>
-                <center><span class='material-symbols-rounded icon'>sentiment_dissatisfied</span></center>
-                <center><p style='color: #000;'>Aucune donnée n'a été trouvée</p></center>
-            </div>
-        HTML;
-    }
 
-    echo json_encode($resp);
-    $conexion->close();
+        echo json_encode($resp);
+    } catch (Exception $e) {
+        echo 'Excepción capturada: ',  $e->getMessage(), "\n";
+    } finally {
+        $conexion->close();
+    }
 ?>
