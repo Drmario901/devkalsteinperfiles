c0c944a594310b15de2f3c2a3f180520c8bd380d
Diego Horesok <diegolancer27@gmail.com>
2024-03-13 11:44:53 -0400

    asdasad

--- src/suport/js/quotes.js
+++ src/suport/js/quotes.js
@@ -125,6 +125,26 @@ jQuery(document).ready(function ($) {
 });
 
 jQuery(document).ready(function ($) {
+  const cookieLng = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith("language="))
+    .split("=")[1];
+  let alertsTranslations = {};
+
+  // cargar json de traducciones
+  const loadTranslations = (lng) => {
+    return fetch(
+      `https://dev.kalstein.plus/plataforma/wp-content/plugins/kalsteinPerfiles/src/locales/${lng}/alert.json`
+    )
+      .then((response) => response.json())
+      .then((translation) => {
+        // save in a global variable
+        alertsTranslations = translation;
+      });
+  };
+
+  loadTranslations(cookieLng);
+
   $(document).on("click", "#btn-details", function () {
     console.log("asdsadasdd");
 
