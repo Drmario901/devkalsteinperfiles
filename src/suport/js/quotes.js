50c504e323a8ced840b5b41c953a86746199fddf
Diego Horesok <diegolancer27@gmail.com>
2024-03-14 08:52:03 -0400

    dfgdgf

--- src/suport/js/quotes.js
+++ src/suport/js/quotes.js
@@ -69,7 +69,7 @@ jQuery(document).ready(function ($) {
 
   function quoteUpdateStatus(cotizacion_id, cotizacion_status, customerName) {
     $.ajax({
-      url: "https://dev.kalstein.plus/plataforma/wp-content/plugins/kalsteinPerfiles/php/suport/updateStatus.php",
+      url: "https://dev.kalstein.plus/plataforma/wp-content/plugins/kalsteinPerfiles/php/suport/updateCotizacion.php",
       method: "POST",
       data: {
         cotizacion_id,
