69ab4c0fb642039abead484148b86d6ebfa823d8
Jorge <jorgeab.hope@gmail.com>
2024-03-14 14:31:45 -0400

    xddd

--- php/suport/conexion.php
+++ php/suport/conexion.php
@@ -1,6 +1,9 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+    require_once '../conexion.php';
 
-require_once '../conexion.php';
     // $hostdb = "localhost";
     // $userdb = "kalsteinplus";
     // $passdb = "OqA;}vKA94PO";
