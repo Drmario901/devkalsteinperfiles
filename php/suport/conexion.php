262cf6e0669cdff0042d0b9f00d3f7e905b3937e
Jorge <jorgeab.hope@gmail.com>
2024-03-14 14:46:40 -0400

    xdddddddddddd

--- php/suport/conexion.php
+++ php/suport/conexion.php
@@ -1,20 +1,20 @@
 <?php
 
-    require_once '../conexionbuena.php';
+    // require_once '../conexionbuena.php';
 
-    // $hostdb = "localhost";
-    // $userdb = "kalsteinplus";
-    // $passdb = "OqA;}vKA94PO";
-    // $namedb = "kalsteinplus_he270716_wp_es";
-    // /* $namedb2 = "he270716_wp4"; */
+    $hostdb = "localhost";
+    $userdb = "kalsteinplus";
+    $passdb = "OqA;}vKA94PO";
+    $namedb = "kalsteinplus_he270716_wp_es";
+    /* $namedb2 = "he270716_wp4"; */
 
-    // $conexion = new mysqli($hostdb, $userdb, $passdb, $namedb);
-    // $acentos = $conexion->query("SET NAMES 'utf8'");
+    $conexion = new mysqli($hostdb, $userdb, $passdb, $namedb);
+    $acentos = $conexion->query("SET NAMES 'utf8'");
 
-    // /* $conexion2 = new mysqli($hostdb, $userdb, $passdb, $namedb2);
-    // $acentos = $conexion->query("SET NAMES 'utf8'"); */
+    /* $conexion2 = new mysqli($hostdb, $userdb, $passdb, $namedb2);
+    $acentos = $conexion->query("SET NAMES 'utf8'"); */
 
-    // if ($conexion->connect_error) {
-    //     die("<script>alert('Error de conexión: " . $conexion->connect_error . "');</script>");
-    // }
+    if ($conexion->connect_error) {
+        die("<script>alert('Error de conexión: " . $conexion->connect_error . "');</script>");
+    }
 ?>
