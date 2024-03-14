4e88a609ff38568a26e907736dc9ef7b903bb0aa
Jorge <jorgeab.hope@gmail.com>
2024-03-14 14:18:54 -0400

    probando...

--- php/conexion.php
+++ php/conexion.php
@@ -1,21 +1,128 @@
 <?php
-    $hostdb = "localhost";
-    $userdb = "kalsteinplus";
-    $passdb = "OqA;}vKA94PO";
-    $namedb = "kalsteinplus_he270716_wp_es";
-    /*$namedb2 = "he270716_wp4";
-    $namedb3 = "he270716_wp3";*/
+    $lang = isset($_COOKIE['language']) ? $_COOKIE['language'] : 'en';
+    // Configuraciones de conexion para cada idioma
+    $configuraciones = [
+        'plus' => [
+            'host' => 'localhost', 
+            'username' => 'kalsteinplus', 
+            'password' => 'OqA;}vKA94PO', 
+            'database' => 'kalsteinplus_he270716_wp_es'
+        ],
+        'de' => [
+            'host' => 'localhost', 
+            'username' => 'user_de', 
+            'password' => 'pass_de', 
+            'database' => 'db_de'
+        ],
+        'ee' => [
+            'host' => 'localhost', 
+            'username' => 'user_ee', 
+            'password' => 'pass_ee', 
+            'database' => 'db_ee'
+        ],
+        'en' => [
+            'host' => 'localhost', 
+            'username' => 'user_en', 
+            'password' => 'pass_en', 
+            'database' => 'db_en'
+        ],
+        'es' => [
+            'host' => 'localhost', 
+            'username' => 'user_es', 
+            'password' => 'pass_es', 
+            'database' => 'db_es'
+        ],
+        'fr' => [
+            'host' => 'localhost', 
+            'username' => 'user_fr', 
+            'password' => 'pass_fr', 
+            'database' => 'db_fr'
+        ],
+        'it' => [
+            'host' => 'localhost', 
+            'username' => 'user_it', 
+            'password' => 'pass_it', 
+            'database' => 'db_it'
+        ],
+        'nl' => [
+            'host' => 'localhost', 
+            'username' => 'user_nl', 
+            'password' => 'pass_nl', 
+            'database' => 'db_nl'
+        ],
+        'pl' => [
+            'host' => 'localhost', 
+            'username' => 'user_pl', 
+            'password' => 'pass_pl', 
+            'database' => 'db_pl'
+        ],
+        'pt' => [
+            'host' => 'localhost', 
+            'username' => 'user_pt', 
+            'password' => 'pass_pt', 
+            'database' => 'db_pt'
+        ],
+        'se' => [
+            'host' => 'localhost', 
+            'username' => 'user_se', 
+            'password' => 'pass_se', 
+            'database' => 'db_se'
+        ]
+    ];
 
-    $conexion = new mysqli($hostdb, $userdb, $passdb, $namedb);
+    // Funcion para obtener la configuracion de conexion basada en el idioma
+    function obtenerConfiguracion($idioma, $configuraciones) {
+        // Si la configuracion es 'plus'
+        if ($idioma == 'plus') {
+            return $configuraciones['plus'];
+        } elseif (array_key_exists($idioma, $configuraciones)) {
+            // Si el idioma esta definido
+            return $configuraciones[$idioma];
+        } else {
+            // Si el idioma no está definido
+            return $configuraciones['en'];
+        }
+    }
+
+    // Deteccion del Idioma con la Cookie de idiomas
+    $idiomaUsuario =  'plus';
+
+    // Ejecutar la funcion para obtener la configuracion de conexion
+    $configuracionUsuario = obtenerConfiguracion($idiomaUsuario, $configuraciones);
+
+    // Establecer la conexion
+    $conexion = new mysqli(
+        $configuracionUsuario['host'],
+        $configuracionUsuario['username'],
+        $configuracionUsuario['password'],
+        $configuracionUsuario['database']
+    );
+
+    // Establecer codificacion a UTF-8
     $acentos = $conexion->query("SET NAMES 'utf8'");
 
-    /*$conexion2 = new mysqli($hostdb, $userdb, $passdb, $namedb2);
-    $acentos = $conexion->query("SET NAMES 'utf8'");
-
-    $conexion3 = new mysqli($hostdb, $userdb, $passdb, $namedb3);
-    $acentos = $conexion->query("SET NAMES 'utf8'");*/
-
+    // Verificar la conexion
     if ($conexion->connect_error) {
         die("<script>alert('Error de conexión: " . $conexion->connect_error . "');</script>");
     }
+
+    // $hostdb = "localhost";
+    // $userdb = "kalsteinplus";
+    // $passdb = "OqA;}vKA94PO";
+    // $namedb = "kalsteinplus_he270716_wp_es";
+    // /*$namedb2 = "he270716_wp4";
+    // $namedb3 = "he270716_wp3";*/
+
+    // $conexion = new mysqli($hostdb, $userdb, $passdb, $namedb);
+    // $acentos = $conexion->query("SET NAMES 'utf8'");
+
+    // /*$conexion2 = new mysqli($hostdb, $userdb, $passdb, $namedb2);
+    // $acentos = $conexion->query("SET NAMES 'utf8'");
+
+    // $conexion3 = new mysqli($hostdb, $userdb, $passdb, $namedb3);
+    // $acentos = $conexion->query("SET NAMES 'utf8'");*/
+
+    // if ($conexion->connect_error) {
+    //     die("<script>alert('Error de conexión: " . $conexion->connect_error . "');</script>");
+    // }
 ?>
--- php/suport/conexion.php
+++ php/suport/conexion.php
@@ -1,17 +1,19 @@
 <?php
-    $hostdb = "localhost";
-    $userdb = "kalsteinplus";
-    $passdb = "OqA;}vKA94PO";
-    $namedb = "kalsteinplus_he270716_wp_es";
-    /* $namedb2 = "he270716_wp4"; */
 
-    $conexion = new mysqli($hostdb, $userdb, $passdb, $namedb);
-    $acentos = $conexion->query("SET NAMES 'utf8'");
+require_once '../conexion.php';
+    // $hostdb = "localhost";
+    // $userdb = "kalsteinplus";
+    // $passdb = "OqA;}vKA94PO";
+    // $namedb = "kalsteinplus_he270716_wp_es";
+    // /* $namedb2 = "he270716_wp4"; */
 
-    /* $conexion2 = new mysqli($hostdb, $userdb, $passdb, $namedb2);
-    $acentos = $conexion->query("SET NAMES 'utf8'"); */
+    // $conexion = new mysqli($hostdb, $userdb, $passdb, $namedb);
+    // $acentos = $conexion->query("SET NAMES 'utf8'");
 
-    if ($conexion->connect_error) {
-        die("<script>alert('Error de conexión: " . $conexion->connect_error . "');</script>");
-    }
+    // /* $conexion2 = new mysqli($hostdb, $userdb, $passdb, $namedb2);
+    // $acentos = $conexion->query("SET NAMES 'utf8'"); */
+
+    // if ($conexion->connect_error) {
+    //     die("<script>alert('Error de conexión: " . $conexion->connect_error . "');</script>");
+    // }
 ?>
