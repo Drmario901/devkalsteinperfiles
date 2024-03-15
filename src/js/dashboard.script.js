2900e3655cc8c9dfe48dd5e12fc343c2a46548c3
VicAlarDev <vic.alarcondev@gmail.com>
2024-03-15 11:22:39 -0400

    Aaaaaaaaaaaaaaaaaaa

--- src/js/dashboard.script.js
+++ src/js/dashboard.script.js
@@ -3187,14 +3187,12 @@ jQuery(document).ready(function ($) {
       var options =
         "<option selected='' style='text-align: center;' value='0'>" +
         valor +
-        "</option><option value='1'>Procesar</option><option value='2'>Cancelar</option>";
+        `</option><option value="1">${alertsTranslations.procesar}</option><option value="2">${alertsTranslations.cancelar}</option>`;
     } else {
       if (valor === alertsTranslations.procesar) {
-        var options =
-          "<option selected='' style='text-align: center;' value='1'>Procesar</option><option value='2'>Cancel</option>";
+        var options = `</option><option value="1">${alertsTranslations.procesar}</option><option value="2">${alertsTranslations.cancelar}</option>`;
       } else if (valor === alertsTranslations.cancelado) {
-        var options =
-          "<option selected='' style='text-align: center;' value='2'>Cancelar</option><option value='0'>Pending</option>";
+        var options = `</option><option value="1">${alertsTranslations.cancelar}</option><option value="2">${alertsTranslations.pendiente}</option>`;
       }
     }
 
