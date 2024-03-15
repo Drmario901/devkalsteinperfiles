0ea695dd150b6970a4aba8a0dd826f29b1391973
Edithson <edithson@kalstein.net>
2024-03-15 15:28:19 -0400

    Taylor Swift

--- testGeo.php
+++ testGeo.php
@@ -7,19 +7,14 @@ use GeoIp2\Database\Reader;
 $databasePath = '/home/kalsteinplus/public_html/dev.kalstein.plus/plataforma/wp-content/plugins/kalsteinPerfiles/src/GeoDB/GeoLite2-Country.mmdb';
 
 try {
-
     $reader = new Reader($databasePath);
 
     $ipAddress = $_SERVER['REMOTE_ADDR'];
 
-    $location = $reader->city($ipAddress);
+    $country = $reader->country($ipAddress);
 
     echo "IP: " . $ipAddress . "\n";
-    echo "Country: " . $location->country->name . "\n";
-    echo "Region: " . $location->mostSpecificSubdivision->name . "\n";
-    echo "City: " . $location->city->name . "\n";
-    echo "ZIP Code: " . $location->postal->code . "\n";
-    echo "Latitude: " . $location->location->latitude . ", Longitud: " . $location->location->longitude . "\n";
+    echo "Country: " . $country->country->name . "\n";
 
 } catch (\GeoIp2\Exception\AddressNotFoundException $e) {
     echo "La dirección IP no fue encontrada en la base de datos.\n";
