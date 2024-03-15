1372c4bf8d1653cac1150907b3a07484169a8808
Diego Horesok <diegolancer27@gmail.com>
2024-03-15 09:43:07 -0400

    asasdsd

--- src/templates-php/suport/service.php
+++ src/templates-php/suport/service.php
@@ -2,7 +2,9 @@
 
     <?php
 
-    
+        ini_set( 'display_errors', 1 );
+        ini_set('error_reporting', E_ALL);
+        error_reporting( E_ALL | E_STRICT );
 
         include 'navdar.php';
 
