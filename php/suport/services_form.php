97f7219d6cdb28e11101f8e7a1b4ed4a1155dfd4
Diego Horesok <diegolancer27@gmail.com>
2024-03-15 09:45:15 -0400

    asdasd

--- php/suport/services_form.php
+++ php/suport/services_form.php
@@ -134,6 +134,9 @@
 </style>
 
 <?php 
+    ini_set( 'display_errors', 1 );
+    ini_set('error_reporting', E_ALL);
+    error_reporting( E_ALL | E_STRICT );
 session_start(); $a = !$add ? 'A' : '' 
 ?>
 
