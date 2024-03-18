0936c8bdbd264eed6154dffd77ee632aa42e226f
Edithson <edithson@kalstein.net>
2024-03-18 11:33:50 -0400

    New test

--- set-cookie.php
+++ set-cookie.php
@@ -5,37 +5,26 @@ use GeoIp2\Database\Reader;
 
 $databasePath = '/home/kalsteinplus/public_html/dev.kalstein.plus/plataforma/wp-content/plugins/kalsteinPerfiles/src/GeoDB/GeoLite2-Country.mmdb';
 
-// AJAX IS MANDATORY
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $language = $_POST['lang'] ?? null;
-    $country = $_POST['country'] ?? null;
+  
+    $language = $_POST['lang'] ?? 'en'; 
 
-    if (empty($language) || empty($country)) {
-        try {
-            $reader = new Reader($databasePath);
-            $record = $reader->country($_SERVER['REMOTE_ADDR']);
-            
-            $country = $record->country->isoCode;
+    try {
+        $reader = new Reader($databasePath);
+        $record = $reader->country($_SERVER['REMOTE_ADDR']);
+        
+        $country = $record->country->isoCode;
 
-            $language = substr($country, 0, 2); 
-
-            setcookie('language', $language, time() + (86400 * 30), "/");
-            setcookie('country', $country, time() + (86400 * 30), "/");
-
-            echo "Language set to: " . $language . "\n";
-            echo "Country set to: " . $country . "\n";
-        } catch (Exception $e) {
-            echo "Error determining location: " . $e->getMessage();
-        }
-    } else {
         setcookie('language', $language, time() + (86400 * 30), "/");
         setcookie('country', $country, time() + (86400 * 30), "/");
 
         echo "Language set to: " . $language . "\n";
         echo "Country set to: " . $country . "\n";
+    } catch (Exception $e) {
+        echo "Error determining location: " . $e->getMessage();
     }
 } else {
-    echo "No POST data received.\n";
+    echo "No POST data received. Ensure language is being sent correctly.\n";
 }
 
 ?>
