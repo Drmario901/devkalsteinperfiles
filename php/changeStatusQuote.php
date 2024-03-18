3ecd908809227eacd678ef2aa5bf2a51359b2e5c
Diego Horesok <diegolancer27@gmail.com>
2024-03-18 10:27:46 -0400

    asdadsads

--- php/changeStatusQuote.php
+++ php/changeStatusQuote.php
@@ -1,51 +1,30 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+    session_start();
+    if(isset($_SESSION["emailAccount"])){
+        $email = $_SESSION["emailAccount"];
+    }
 
-session_start();
-if (isset($_SESSION["emailAccount"])) {
-    $email = $_SESSION["emailAccount"];
-} else {
-    // Considera manejar el caso donde no existe la sesión o redirigir al usuario.
-    // exit('No se ha iniciado sesión.'); // Por ejemplo.
-}
+    require __DIR__ . '/conexion.php';
 
-require __DIR__ . '/conexion.php';
+    $consulta = $_POST['consulta'];
+    $id = $_POST['consulta2'];
 
-// Debes asegurarte de que estás recibiendo los datos 'consulta' y 'consulta2' y validarlos.
-if (!isset($_POST['consulta'], $_POST['consulta2'])) {
-    exit('Los datos necesarios no están presentes.');
-}
+    $consulta = $consulta == 'Pending' ? '0' : $consulta;
+    $consulta = $consulta == 'Process' ? '1' : $consulta;
+    $consulta = $consulta == 'Cancel' ? '2' : $consulta;
 
-$consulta = $_POST['consulta'];
-$id = $_POST['consulta2'];
+    $query = "UPDATE wp_cotizacion SET cotizacion_status = '$consulta' WHERE cotizacion_id = '$id'";
+    if ($conexion->query($query) === TRUE){
+        $registerUpdate = "INSERT INTO wp_register_updates(aid_updates, account_id, updates_date, update_description) VALUES ('', '$email', CURRENT_TIMESTAMP, 'The status of QUO$id was changed')";
+        $conexion->query($registerUpdate);
+        $update = 'correcto';
+    }else{
+        $update = 'correcto';
+    }
+    
+    $datos = array(
+        'update' => $update
+    );
 
-// Es mejor usar sentencias preparadas para evitar inyecciones SQL.
-if (!in_array($consulta, ['0', '3', '2'], true)) {
-    exit('Valor de consulta no válido.'.$consulta);
-}
-
-$statusMap = ['Pending' => '0', 'Process' => '3', 'Cancel' => '2'];
-$consulta = $statusMap[$consulta];
-
-// Preparar la consulta para evitar inyecciones SQL
-$query = $conexion->prepare("UPDATE wp_cotizacion SET cotizacion_status = ? WHERE cotizacion_id = ?");
-$query->bind_param("si", $consulta, $id);
-if ($query->execute()) {
-    $registerUpdate = $conexion->prepare("INSERT INTO wp_register_updates(account_id, updates_date, update_description) VALUES (?, CURRENT_TIMESTAMP, ?)");
-    $description = "The status of QUO$id was changed";
-    $registerUpdate->bind_param("ss", $email, $description);
-    $registerUpdate->execute();
-    $update = 'correcto';
-} else {
-    $update = 'incorrecto'; // Esto debe ser 'incorrecto' si la actualización falla.
-}
-
-$datos = array(
-    'update' => $update
-);
-
-echo json_encode($datos, JSON_FORCE_OBJECT);
-$conexion->close();
-?>
+    echo json_encode($datos, JSON_FORCE_OBJECT);
+    $conexion->close();
