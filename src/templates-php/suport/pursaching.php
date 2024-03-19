e95f66f0cb72b7e494b08a186313299c6d877580
Jorge <jorgeab.hope@gmail.com>
2024-03-19 15:48:13 -0400

    Pruebas 4

--- src/templates-php/suport/pursaching.php
+++ src/templates-php/suport/pursaching.php
@@ -8,7 +8,7 @@
         ini_set('display_errors', 1);
         ini_set('display_startup_errors', 1);
         error_reporting(E_ALL);
-            session_start(); 
+             
             if (isset($_SESSION['emailAccount'])){
                 $email = $_SESSION['emailAccount'];
         
