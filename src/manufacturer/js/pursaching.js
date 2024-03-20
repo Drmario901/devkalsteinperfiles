061dbaf16b53999e959c6812170c681faffb1004
Diego Horesok <diegolancer27@gmail.com>
2024-03-20 15:06:56 -0400

    aaaaaa

--- src/manufacturer/js/pursaching.js
+++ src/manufacturer/js/pursaching.js
@@ -1,15 +1,17 @@
-jQuery(document).ready(function($){
-    document.querySelector('#c-panel06 .banner').setAttribute('hidden', '');
+jQuery(document).ready(function ($) {
+  document.querySelector("#c-panel06 .banner").setAttribute("hidden", "");
 
-    $('.my-quotes-nav').attr('hidden', '');
-    
-    if(document.querySelector('#search-bar-get-category').value != ''){
-        $('#i-search').val($('#search-bar-get-text').val());
-        $('#filterSearchCategorie').text($('#search-bar-get-category').val());
-        $('#btnSearch').click();
-    }
-    else {
-        $('#btnGenQuote').click();
-    }
+  $(".my-quotes-nav").attr("hidden", "");
 
+  if (document.querySelector("#search-bar-get-category").value != "") {
+    $("#i-search").val($("#search-bar-get-text").val());
+    $("#filterSearchCategorie").text($("#search-bar-get-category").val());
+    $("#btnSearch").click();
+  } else {
+    $("#btnGenQuote").click();
+  }
+
+  $("#btnSearch").on("click", function () {
+    console.log("hice click");
+  });
 });
