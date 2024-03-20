a7fce08ee3af4593f74731b4aa7395fac7ebe7a2
Diego Horesok <diegolancer27@gmail.com>
2024-03-20 13:17:01 -0400

    aaaa

--- php/showDataQuoteRecent.php
+++ php/showDataQuoteRecent.php
@@ -13,46 +13,53 @@ require __DIR__ . '/conexion.php';
 $consulta = "SELECT * FROM wp_cotizacion WHERE cotizacion_id_user = '$email' ORDER BY cotizacion_id DESC LIMIT 10";
 $resultado = $conexion->query($consulta);
 
-$i = 0;
-
 require_once 'translateText.php';
 
 translateText();
 
 $html = ""; // Inicializar $html como una cadena vacía
 
-    if ($resultado->num_rows > 0){
-        $i = 0;
-        while ($value = $resultado->fetch_assoc()) {
-            $i = $i + 01;
-            $id = $value["cotizacion_id"];
-            $date = $value['cotizacion_create_at'];
-            $date = new DateTime($date);
-            $newData = date_format($date, 'Y-m-d');
+// Inicia la tabla antes del bucle
+$html .= "
+<table>
+    <thead>
+        <tr>
+            <th>Quo</th>
+            <th>Fecha</th>
+        </tr>
+    </thead>
+    <tbody>
+";
 
-            $html.= "                                    
-                <tr style='height: 3.2rem; '>
-                    <td style='padding-top: 0.9rem;'>QUO---> $id</td>
-                    <td style='padding-top: 0.9rem;'>Fecha ---> $newData</td>
-                </tr>
-            ";
-		}
+if ($resultado->num_rows > 0){
+    while ($value = $resultado->fetch_assoc()) {
+        $id = $value["cotizacion_id"];
+        $date = $value['cotizacion_create_at'];
+        $date = new DateTime($date);
+        $newData = date_format($date, 'Y-m-d');
 
-        $msjNoData = "";
-    }else{
-        $msjNoData = "
-            <div class='contentNoDataQuoteRecent'>
-                <i class='fa-solid fa-file-lines' style='font-size: 4rem; color: #212380; margin-top: 12rem;'></i>
-                <p style='font-size: 1.4em; font-weight: bold; margin-top: 2rem;' data-i17n='client:seeQuotesHere'>Aquí puedes ver todas tus cotizaciones.</p>
-            </div>
+        // Añade cada fila con los datos correspondientes
+        $html .= "                                    
+            <tr style='height: 3.2rem; '>
+                <td style='padding-top: 0.9rem;'> $id</td>
+                <td style='padding-top: 0.9rem;'> $newData</td>
+            </tr>
         ";
     }
-
-    $html.= "
-            </tbody>
-        </table>
-        $msjNoData
+} else {
+    // Mensaje si no hay datos
+    $html .= "
+        <tr>
+            <td colspan='2'>No se encontraron cotizaciones.</td>
+        </tr>
     ";
+}
 
-    echo $html;
-    $conexion->close();
+// Cierra tbody y table
+$html .= "
+    </tbody>
+</table>
+";
+
+echo $html;
+$conexion->close();
