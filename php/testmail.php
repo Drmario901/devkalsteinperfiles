b5075998dfadd61d48cdcaec826f1a533a394148
Diego Horesok <diegolancer27@gmail.com>
2024-03-26 15:02:51 -0400

    Update credentials

--- php/testmail.php
+++ php/testmail.php
@@ -22,7 +22,7 @@
     try {
         $mail->SMTPDebug = 2;  // Sacar esta línea para no mostrar salida debug
         $mail->isSMTP();
-        $mail->Host = 'mail.kalstein.plus';  // Host de conexión SMTP
+        $mail->Host = 'kalstein2025@yopmail.com';  // Host de conexión SMTP
         $mail->SMTPAuth = true;
         $mail->Username = 'no-reply@kalstein.plus';                 // Usuario SMTP
         $mail->Password = 'Kalstein1234';                           // Password SMTP
