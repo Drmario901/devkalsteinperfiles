407c7686bbdf0e1b1a13d553ca8db8bb01668a73
Diego Horesok <diegolancer27@gmail.com>
2024-03-26 10:42:43 -0400

    asdasd

--- src/distributor/js/chart.js
+++ src/distributor/js/chart.js
@@ -1,25 +1,12 @@
-// let plugin_dir =
-//   "https://dev.kalstein.plus/plataforma/wp-local/wp-content/plugins/kalsteinPerfiles/";
+
 
 jQuery(document).ready(function ($) {
-  console.log("entreee");
-  console.log('entre de nuevo');
-  
 
-  function array_sum(array) {
-    let res = 0;
-
-    array.forEach((element) => {
-      res += element;
-    });
-
-    return res;
-  }
 
   const cookieLng = document.cookie
-    .split("; ")
-    .find((row) => row.startsWith("language="))
-    .split("=")[1];
+  .split("; ")
+  .find((row) => row.startsWith("language="))
+  .split("=")[1];
   let alertsTranslations = {};
 
   // cargar json de traducciones
@@ -33,7 +20,10 @@ jQuery(document).ready(function ($) {
         alertsTranslations = translation;
       });
   };
+
   loadTranslations(cookieLng);
+
+  //! meses 
 
   const meses = {
     ee: {
@@ -179,26 +169,43 @@ jQuery(document).ready(function ($) {
   };
 
 
+
+  loadTranslations(cookieLng);
+
+
   let plugin_dir =
-    "https://dev.kalstein.plus/plataforma/wp-content/plugins/kalsteinPerfiles/";
+    "https://dev.kalstein.plus/plataforma/wp-content/plugins/kalsteinPerfiles/php/";
+
+  // let months = [
+  //   alertsTranslations.enero,
+  //   alertsTranslations.febrero,
+  //   alertsTranslations.marzo,
+  //   alertsTranslations.abril,
+  //   alertsTranslations.mayo,
+  //   alertsTranslations.junio,
+  //   alertsTranslations.julio,
+  //   alertsTranslations.agosto,
+  //   alertsTranslations.septiembre,
+  //   alertsTranslations.octubre,
+  //   alertsTranslations.noviembre,
+  //   alertsTranslations.diciembre,
+  // ];
 
   let months = [
-    `${alertsTranslations.enero}`,
-    `${alertsTranslations.febrero}`,
-    `${alertsTranslations.marzo}`,
-    `${alertsTranslations.abril}`,
-    `${alertsTranslations.mayo}`,
-    `${alertsTranslations.junio}`,
-    `${alertsTranslations.julio}`,
-    `${alertsTranslations.agosto}`,
-    `${alertsTranslations.septiembre}`,
-    `${alertsTranslations.octubre}`,
-    `${alertsTranslations.noviembre}`,
-    `${alertsTranslations.diciembre}`,
-  ];
+    meses[cookieLng].enero,
+    meses[cookieLng].febrero,
+    meses[cookieLng].marzo,
+    meses[cookieLng].abril,
+    meses[cookieLng].mayo,
+    meses[cookieLng].junio,
+    meses[cookieLng].julio,
+    meses[cookieLng].agosto,
+    meses[cookieLng].septiembre,
+    meses[cookieLng].octubre,
+    meses[cookieLng].noviembre,
+    meses[cookieLng].diciembre
+  ]
 
- console.log('meses', months);
- 
   function prevMonthList(month) {
     let res = [];
 
@@ -212,110 +219,59 @@ jQuery(document).ready(function ($) {
   }
 
   let date = new Date();
-
   let prevMonths = prevMonthList(date.getMonth());
 
   $.ajax({
-    url: plugin_dir + "php/manufacturer/getChartInfo.php",
-
+    url: plugin_dir + "suport/chartinfo.php",
     type: "POST",
-
     data: "",
-
     dataType: "html",
   }).done(function (response) {
     // La cuenta se reiniciará en...
 
+    console.log(response);
+
     $("#will-restart").html(`
-
         ${
-          alertsTranslations.CountDown
-        } ${JSON.parse(response).will_restart} ${alertsTranslations.textDays}
-
+          alertsTranslations.cuentaReinicio
+        } ${JSON.parse(response).will_restart} ${alertsTranslations.days}
         `);
 
-    // función para acotar decimales
+    // GRAFICO 1 VENTAS
 
-    function parse_dec(val) {
-      return Math.floor(parseFloat(val) * 100) / 100;
-    }
-
+    var ctx = document.getElementById("sales");
     let graph_1 = JSON.parse(response).graph_1;
 
-    let grow_1;
-
-    if (graph_1[3] != 0) {
-      grow_1 = (100 * (graph_1[4] - graph_1[3])) / graph_1[3];
-    } else {
-      if (graph_1[4] != 0) {
-        grow_1 = -100;
-      } else {
-        grow_1 = 0;
-      }
-    }
-
-    $("#graph-1-prevMonth").html(`
-
-            <span class="material-symbols-rounded icon ${
-              grow_1 >= 0 ? "green" : "red"
-            }">trending_${grow_1 >= 0 ? "up" : "down"}</span>
-
-            <div>
-
-                <data class="revenue-item-data">${
-                  parse_dec(grow_1) +
-                  "% <br>(" +
-                  parse_dec(graph_1[3]) +
-                  "$ to " +
-                  parse_dec(graph_1[4]) +
-                  "$)"
-                }</data>
-
-                <p class="revenue-item-text">${
-                  alertsTranslations.mesPrevio
-                } (${prevMonths[4]})</p>
-
-            </div>
-
-        `);
-
-    // ORDENES COMPLETADAS Y PENDIENTES
-
-    $("#processed-orders").html(JSON.parse(response).processed_orders);
-
-    $("#pending-orders").html(JSON.parse(response).pending_orders);
-
-    if (JSON.parse(response).pending_orders > 0) {
-      $("#pending-orders-indicator").removeAttr("hidden");
-    }
-
-    // GRAFICO 2 ORDENES REALIZADAS
-
-    var ctx = document.getElementById("activity");
-
-    let graph_2 = JSON.parse(response).graph_2;
-
-    var visitors = new Chart(ctx, {
-      type: "line",
-
+    var sales = new Chart(ctx, {
+      type: "bar",
       data: {
         labels: prevMonths,
-
         datasets: [
           {
-            label: 'clientes del mes...',
-
-            data: graph_2,
-
-            backgroundColor: ["rgba(33, 35, 128, 0.2)"],
-
-            borderColor: ["rgba(33, 35, 128, 1)"],
-
+            label: alertsTranslations.ventasDelMes,
+            data: graph_1,
+            backgroundColor: [
+              "rgba(33, 35, 128, 0.2)",
+              "rgba(33, 35, 128, 0.2)",
+              "rgba(33, 35, 128, 0.2)",
+              "rgba(33, 35, 128, 0.2)",
+              "rgba(33, 35, 128, 0.2)",
+              "rgba(33, 35, 128, 0.2)",
+              "rgba(33, 35, 128, 0.2)",
+            ],
+            borderColor: [
+              "rgba(33, 35, 128, 1)",
+              "rgba(33, 35, 128, 1)",
+              "rgba(33, 35, 128, 1)",
+              "rgba(33, 35, 128, 1)",
+              "rgba(33, 35, 128, 1)",
+              "rgba(33, 35, 128, 1)",
+              "rgba(33, 35, 128, 1)",
+            ],
             borderWidth: 1,
           },
         ],
       },
-
       options: {
         scales: {
           y: {
@@ -325,56 +281,92 @@ jQuery(document).ready(function ($) {
       },
     });
 
-    let grow_2 = graph_2[3] != 0 ? (100 * (graph_2[4] - graph_2[3])) / graph_2[3] : 0;
-
-   
-
-
-    if (graph_2[4] != 0) {
-      if (graph_2[3] != 0) {
-        grow_2 = (100 * (graph_2[4] - graph_2[3])) / graph_2[3];
-      } else {
-        grow_2 = 100;
-      }
-    } else {
-      if (graph_2[3] != 0) {
-        grow_2 = -100;
-      } else {
-        grow_2 = 0;
-      }
+    // función para acotar decimales
+    function parse_dec(val) {
+      return Math.floor(parseFloat(val) * 100) / 100;
     }
 
-    $("#graph-2-prevMonth").html(`
+    let grow_1 =
+      graph_1[3] != 0 ? (100 * (graph_1[4] - graph_1[3])) / graph_1[3] : -100;
 
+    $("#graph-1-prevMonth").html(`
+            <span class="material-symbols-rounded icon ${
+              grow_1 >= 0 ? "green" : "red"
+            }">trending_${grow_1 >= 0 ? "up" : "down"}</span>
+            <div>
+                <data class="revenue-item-data">${
+                  parse_dec(grow_1) +
+                  "% <br>(" +
+                  parse_dec(graph_1[3]) +
+                  "$ to " +
+                  parse_dec(graph_1[4]) +
+                  "$)"
+                }</data>
+                <p class="revenue-item-text">${
+                  alertsTranslations.mesPrevio
+                }, (${prevMonths[4]})</p>
+            </div>
+        `);
+
+    // ORDENES COMPLETADAS Y PENDIENTES
+
+    $("#processed-orders").html(JSON.parse(response).processed_orders);
+    $("#pending-orders").html(JSON.parse(response).pending_orders);
+
+    // GRAFICO 2 ORDENES REALIZADAS
+
+    var ctx = document.getElementById("activity");
+    let graph_2 = JSON.parse(response).graph_2;
+
+    var visitors = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: prevMonths,
+        datasets: [
+          {
+            label: alertsTranslations.clienteMes,
+            data: graph_2,
+            backgroundColor: ["rgba(33, 35, 128, 0.2)"],
+            borderColor: ["rgba(33, 35, 128, 1)"],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+
+    let grow_2 =
+      graph_2[3] != 0 ? (100 * (graph_2[4] - graph_2[3])) / graph_2[3] : 0;
+
+      // $("#graph-2-prevMonth").on('click', function(){
+      //   console.log('grow_2',grow_2);
+      //   console.log('graph_2', graph_2[4]);
+      //   console.log('prevMonths',prevMonths);
+      //   console.log('months',months);
+      //   console.log('meses', meses[cookieLng].enero);
+      // })
+
+    $("#graph-2-prevMonth").html(`
             <span class="material-symbols-rounded icon ${
               grow_2 >= 0 ? "green" : "red"
             }">trending_${grow_2 >= 0 ? "up" : "down"}</span>
-
             <div>
-
                 <data class="revenue-item-data">${
                   parse_dec(grow_2) +
                   "% <br>(" +
                   parse_dec(graph_2[3]) +
-                  " clientes de " +
+                  ` ${alertsTranslations.completoA} ` +
                   parse_dec(graph_2[4]) +
-                  " clientes)"
+                  ` ${alertsTranslations.reportes}) `
                 }</data>
-
-                <p class="revenue-item-text">${
-                  alertsTranslations.mesPrevio
-                } (${prevMonths[4]})</p>
-
+                <p class="revenue-item-text">${alertsTranslations.mesPrevio} (${prevMonths[4]})</p>
             </div>
-
         `);
   });
-
-  $("#graph-2-prevMonth").on('click', function(){
-    console.log('grow_2',grow_2);
-    console.log('graph_2', graph_2[4]);
-    console.log('prevMonths',prevMonths);
-    console.log('months',months);
-    console.log('meses', meses[cookieLng].enero);
-  })
 });
