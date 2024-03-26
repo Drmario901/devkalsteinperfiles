9e1c7e48757980f8465a89d449142951a8341363
Diego Horesok <diegolancer27@gmail.com>
2024-03-26 11:42:32 -0400

    asdasd

--- src/distributor/js/pursaching.js
+++ src/distributor/js/pursaching.js
@@ -2,19 +2,19 @@ jQuery(document).ready(function ($) {
   document.querySelector("#c-panel06 .banner").setAttribute("hidden", "");
   document.querySelector("#c-panel02 .banner").setAttribute("hidden", "");
 
-  //   let nav_links = [
-  //     "#my-quotes-link-widget",
-  //     "#shipping-settings-link-widget",
-  //     "#generate-quote-link-widget",
-  //   ];
+    let nav_links = [
+      "#my-quotes-link-widget",
+      "#shipping-settings-link-widget",
+      "#generate-quote-link-widget",
+    ];
 
-  let nav_links = [
-    "#btnSearch",
-    "#shipping-settings-link-widget",
-    "#generate-quote-link-widget",
-  ];
+  // let nav_links = [
+  //   "#btnSearch",
+  //   "#shipping-settings-link-widget",
+  //   "#generate-quote-link-widget",
+  // ];
 
-  $("#btnSearch").on("click", function () {
+  $("#my-quotes-link-widget").on("click", function () {
     nav_links.forEach((elem) => {
       $(elem).removeClass("active");
     });
