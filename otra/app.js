d5cfc6e429ea75ceb67653601a9b9f3e0760b342
Diego Horesok <diegolancer27@gmail.com>
2024-03-27 12:48:49 -0400

    asdad

--- otra/app.js
+++ otra/app.js
@@ -1,15 +1,15 @@
-let btn = document.getElementById('btnEndingClient');
+// let btn = document.getElementById('btnEndingClient');
     
-btn.addEventListener('click', function() {
-    // Desactiva el botón
-    this.disabled = true;
-    window.alert('activando');
-    // Actualiza el botón para incluir el spinner y cambia el texto
-    this.innerHTML = ' <div class="spinner"></div>';
+// btn.addEventListener('click', function() {
+//     // Desactiva el botón
+//     this.disabled = true;
+//     window.alert('activando');
+//     // Actualiza el botón para incluir el spinner y cambia el texto
+//     this.innerHTML = ' <div class="spinner"></div>';
     
-    // Simula una operación asíncrona con setTimeout
-    setTimeout(() => {
-        this.innerHTML = 'Finalizar'; // Restablece el texto original del botón
-        this.disabled = false; // Reactiva el botón
-    }, 3000);
-});
+//     // Simula una operación asíncrona con setTimeout
+//     setTimeout(() => {
+//         this.innerHTML = 'Finalizar'; // Restablece el texto original del botón
+//         this.disabled = false; // Reactiva el botón
+//     }, 3000);
+// });
