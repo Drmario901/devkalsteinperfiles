6c79d07958e3976e9f3dcc59d537237c3c439639
Diego Horesok <diegolancer27@gmail.com>
2024-03-28 13:49:43 -0400

    cambios,,,,

--- php/manufacturer/selectCountryMaritimal.php
+++ php/manufacturer/selectCountryMaritimal.php
@@ -1,5 +1,8 @@
 <?php
-    require __DIR__.'/../conexion.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+    require_once __DIR__.'/../../db/conexion.php';
 
     $salida = "<option selected value='0'>Choose an option</option>";
     
