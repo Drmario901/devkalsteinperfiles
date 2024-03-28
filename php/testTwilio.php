40b54437860aa9391285a95a1a8c131fa9a73e64
Victor Alarcon <vic.alarcondev@gmail.com>
2024-03-28 10:32:17 -0400

    Prueba

--- php/testTwilio.php
+++ php/testTwilio.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require 'vendor/autoload.php';
 if (session_status() == PHP_SESSION_NONE) {
   session_start();
@@ -7,9 +11,9 @@ if (session_status() == PHP_SESSION_NONE) {
 if (isset($_SESSION["codeVerification"])) {
   $code = $_SESSION["codeVerification"];
 }
-if (isset($_SESSION["telefono"])) {
+/* if (isset($_SESSION["telefono"])) {
   $to = $_SESSION["telefono"];
-}
+} */
 
 echo $code;
 
@@ -19,6 +23,7 @@ $sid = 'AC8c50999f3028ad776a0b422f3e85ce64';
 $token = '55d74f432a32052137bd568946767750';
 $twilioPhoneNumber = '+17608564734';
 $client = new Client($sid, $token);
+$to = '+584243460227';
 
 try {
   $message = $client->messages->create(
