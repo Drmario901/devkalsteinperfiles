501d49e20045e6d545ef474a74577b6d3b4c8a46
Victor Alarcon <vic.alarcondev@gmail.com>
2024-03-28 11:06:29 -0400

    test

--- src/templates-php/accounts/signup.php
+++ src/templates-php/accounts/signup.php
@@ -99,6 +99,9 @@ if (isset($_GET['search'])) {
                         <button type='button' class='btn'
                             style='background-color: #213280; color: #fff; margin-top:1rem; width: 100%; height: 3rem;'
                             id='btnContinueSignUp' data-i18n="account:tittleButton">Continuar</button>
+                            <button type='button' class='btn'
+                            style='background-color: #213280; color: #fff; margin-top:1rem; width: 100%; height: 3rem;'
+                            id='btnContinueSignUp1/2' data-i18n="account:tittleButton">Continuar</button>
                         <button type='button' class='btn'
                             style='background-color: #213280; color: #fff; margin-top:1rem; width: 100%; height: 3rem; display: none;'
                             id='btnContinueSignUp2' data-i18n="account:tittleButton">Continuar</button>
