5720bb14799cef1f0be2a299be284b9335121786
Diego Horesok <diegolancer27@gmail.com>
2024-03-29 10:58:21 -0400

    mas cambios...

--- php/suport/cotizaciones/cotizaciones.php
+++ php/suport/cotizaciones/cotizaciones.php
@@ -4,41 +4,24 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// https://dev.kalstein.plus/plataforma/wp-content/plugins/kalsteinPerfiles/php/suport/cotizaciones/cotizaciones.php
- session_start();
-
+session_start();
 
 require_once __DIR__ . '/../../../db/conexion.php';
 
 $acc_id = $_SESSION['emailAccount'];
-
 
 $consulta = "SELECT cotizacion_total FROM wp_cotizacion WHERE cotizacion_id_remitente = '" . $acc_id . "'";
 
 echo 'hola';
 $resultado = $conexion->query($consulta);
 
-echo $resultado;
-
-// if ($resultado->num_rows > 0){
-//     while ($value = $resultado->fetch_assoc()) {
-//         $id = $value['M_id'];
-//         $nombre = $value['M_nombre_product'];
-//         $imagen = $value['product_image'];
-//         $pdf = $value['M_pdf'];
-
-//         $html .= "
-//         <div class='col-12 col-sm-6 col-md-4 col-lg-3 mb-3'>
-//             <div class='card h-100 mx-2'>
-//             <img src='$imagen' class='card-img-top' alt='...'>
-//             <div class='card-body'>
-//                 <center><h5 class='card-title' style='font-size: 16px;'>$nombre</h5></center>
-//                 <center><button class='_df_button' id='book1' source='https://dev.kalstein.plus/plataforma/wp-content/plugins/kalsteinPerfiles/src/manuals/upload/$pdf' data-i17n='client:vista'>Vista</button></center>
-//             </div>
-//             </div>
-//         </div>
-//         ";
-//     }
-// }
-
+// Verificar si la consulta devolvió filas
+if ($resultado->num_rows > 0) {
+    // Iterar a través de los resultados
+    while ($fila = $resultado->fetch_assoc()) {
+        echo $fila['cotizacion_total'] . "<br>";
+    }
+} else {
+    echo "No se encontraron resultados.";
+}
 ?>
