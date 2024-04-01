f25a8d764d981eb6248474f0aec34cc32083801c
Diego Horesok <diegolancer27@gmail.com>
2024-04-01 10:28:12 -0400

    aaaaaaaaa

--- php/distributor/cotizaciones/cotizaciones.php
+++ php/distributor/cotizaciones/cotizaciones.php
@@ -10,6 +10,40 @@ require_once __DIR__ . '/../../../db/conexion.php';
 
 $acc_id = $_SESSION['emailAccount'];
 
+function obtenerSumaTotalUSD($conexion, $acc_id, $estado, $tasaConversionEURUSD) {
+    $sumaTotalUSD = 0;
+    $consulta = "SELECT cotizacion_total, cotizacion_divisa FROM wp_cotizacion WHERE cotizacion_id_remitente != 'KALSTEIN-INTERNAL' AND cotizacion_status IN ($estado) AND cotizacion_id_remitente === '". $acc_id ."'";
+    $resultado = $conexion->query($consulta);
+
+    if ($resultado->num_rows > 0) {
+        while ($fila = $resultado->fetch_assoc()) {
+            echo $cotizacionTotal = $fila['cotizacion_total'];
+            echo $cotizacionDivisa = $fila['cotizacion_divisa'];
+            
+            // Convertir a USD si la divisa es EUR, de lo contrario asumir que ya está en USD
+            if ($cotizacionDivisa == 'EUR') {
+                $cotizacionTotalUSD = $cotizacionTotal * $tasaConversionEURUSD;
+            } else {
+                $cotizacionTotalUSD = $cotizacionTotal;
+            }
+            
+            // Sumar al total
+            $sumaTotalUSD += $cotizacionTotalUSD;
+        }
+    }
+    return $sumaTotalUSD;
+}
+
+// Asegúrate de que $idsCotizacion no esté vacío antes de llamar a la función
+if (!empty($idsCotizacion)) {
+    $sumaTotalUSDPendiente = obtenerSumaTotalUSD($conexion, $acc_id, "'Pendiente', '0'", $tasaConversionEURUSD, $idsCotizacion) ;
+   $sumaTotalUSDProcesar = obtenerSumaTotalUSD($conexion, $acc_id, "'Procesar', '1'", $tasaConversionEURUSD, $idsCotizacion);
+    $sumaTotalUSDProcesado = obtenerSumaTotalUSD($conexion, $acc_id, "'Procesado', '3'", $tasaConversionEURUSD, $idsCotizacion);
+} else {
+    echo "No hay cotizaciones para calcular.";
+}
+
+
 
 
 ?> 
