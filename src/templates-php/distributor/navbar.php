2d3dd726e23b8d3164dfa219c62d734535e756ad
Diego Horesok <diegolancer27@gmail.com>
2024-04-01 11:39:10 -0400

    adasdsd

--- src/templates-php/distributor/navbar.php
+++ src/templates-php/distributor/navbar.php
@@ -48,14 +48,14 @@
                         <span class="material-symbols-rounded icon" >
                             monetization_on
                         </span>
-                        <div style="display: flex; flex-direction: column; text-align: center;">
+                        <div style="display: flex; flex-direction: column; text-align: start;">
                         <p style="font-size: 1rem; text-align: center; width: 100%; margin-top: 2px;">
                                 Pendiente <strong style="color: #e38512;">$<?php echo $sumaTotalUSDPendiente;?></strong>
                             </p>
-                            <p style="font-size: 1rem; text-align: center; width: 100%;">
+                            <p style="font-size: 1rem; text-align: start; width: 100%;">
                                 Procesar <strong style="color: #0000ff;">$<?php echo $sumaTotalUSDProcesar;?></strong>
                             </p>
-                            <p style="font-size: 1rem; text-align: center; width: 100%;">
+                            <p style="font-size: 1rem; text-align: start; width: 100%;">
                                 Cobrado <strong style="color: #0eab13;">$<?php echo $sumaTotalUSDProcesado;?></strong>
                             </p>
                         </div>
