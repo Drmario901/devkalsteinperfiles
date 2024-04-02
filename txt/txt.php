9061ec8b16b6b5972d9e7a62145806528c712868
Diego Horesok <diegolancer27@gmail.com>
2024-04-02 13:54:30 -0400

    aaaaaaaaaaaaaaaaa

--- txt/txt.php
+++ txt/txt.php
@@ -16,12 +16,13 @@ function procesarArchivoMonetico($archivo, $conexion) {
                 $codeRetour = $jsonData->{'code-retour'} ?? 'No encontrado';
                 $montoTotalConMoneda = $jsonData->montant ?? '0';
 
-                // Extraer solo el monto numérico del string
-                preg_match('/\d+(\.\d+)?/', $montoTotalConMoneda, $matches);
-                $montoTotal = $matches[0] ?? 0;
+                // Extraer solo el monto numérico y la divisa del string
+                preg_match('/(\d+(\.\d+)?)([A-Z]+)/', $montoTotalConMoneda, $matches);
+                $montoTotal = $matches[1] ?? 0; // Monto numérico
+                $divisa = $matches[3] ?? 'No encontrada'; // Divisa
 
                 if ($codeRetour == 'payetest' || $codeRetour == 'paiement') {
-                    insertDataToDatabase($conexion, $reference, $montoTotal);
+                    insertDataToDatabase($conexion, $reference, $montoTotal, $divisa);
                 }
             } else {
                 echo "Error al decodificar JSON: " . json_last_error_msg() . "\n";
@@ -30,7 +31,7 @@ function procesarArchivoMonetico($archivo, $conexion) {
     }
 }
 
-function insertDataToDatabase($conexion, $reference, $montoTotal) {
+function insertDataToDatabase($conexion, $reference, $montoTotal, $divisa) {
     $query = "SELECT id_cotizacion FROM wp_monetico WHERE id_cotizacion = ?";
     $stmt = $conexion->prepare($query);
     $stmt->bind_param("s", $reference);
@@ -38,13 +39,13 @@ function insertDataToDatabase($conexion, $reference, $montoTotal) {
     $result = $stmt->get_result();
     
     if ($result->num_rows === 0) {
-        // Modificar para incluir monto_total en la inserción
-        $insertQuery = "INSERT INTO wp_monetico (id_cotizacion, monto_total) VALUES (?, ?)";
+        // Modificar para incluir monto_total y cotizacion_divisa en la inserción
+        $insertQuery = "INSERT INTO wp_monetico (id_cotizacion, monto_total, cotizacion_divisa) VALUES (?, ?, ?)";
         $insertStmt = $conexion->prepare($insertQuery);
         // Asegúrate de que el tipo de dato corresponda con tu esquema de base de datos
-        $insertStmt->bind_param("sd", $reference, $montoTotal);
+        $insertStmt->bind_param("sds", $reference, $montoTotal, $divisa);
         if ($insertStmt->execute()) {
-            echo "Inserción correcta, ID: " . $insertStmt->insert_id . "\n";
+            echo "Inserción correcta, ID: " . $insertStmt->insert_id . ", Monto: " . $montoTotal . " " . $divisa . "\n";
         } else {
             echo "Error en inserción: " . $insertStmt->error . "\n";
         }
