fadab10efd36e246c7f1b995ca1a6b82b75f9ed5
Diego Horesok <diegolancer27@gmail.com>
2024-04-02 10:01:34 -0400

    prueba....

---
+++ txt/txt.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$archivo = 'txt/prueba.txt';
+
+// Leer el contenido del archivo
+$contenidoJson = file_get_contents($archivo);
+
+// Decodificar el JSON a un objeto PHP
+$objeto = json_decode($contenidoJson);
+
+// Acceder a los valores de montant y reference
+$montant = $objeto->montant;
+$reference = $objeto->reference;
+
+// Imprimir los valores
+echo "Montant: " . $montant . "\n";
+echo "Reference: " . $reference . "\n";
+
+
+?>
