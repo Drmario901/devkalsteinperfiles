134d3cdc63bb92a1ea80a91bff423bc09f689038
Diego Horesok <diegolancer27@gmail.com>
2024-04-02 10:29:52 -0400

    asdasd

--- testResponse.php
+++ testResponse.php
@@ -8,6 +8,7 @@ header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Content-Type');
 
 require '/home/kalsteinplus/public_html/dev.kalstein.plus/plataforma/wp-content/plugins/kalsteinPerfiles/vendor/autoload.php';
+require_once __DIR__ . '/../devkalsteinperfiles/db/conexion.php';
 
 use DansMaCulotte\Monetico\Monetico;
 use DansMaCulotte\Monetico\Responses\PurchaseResponse;
@@ -15,6 +16,9 @@ use DansMaCulotte\Monetico\Responses\PurchaseResponse;
 $data = $_POST;
 
 file_put_contents('monetico_log.txt', date('Y-m-d H:i:s') . " - Datos recibidos: " . json_encode($data) . "\n", FILE_APPEND);
+
+$dataFormat = json_encode($data);
+
 
 if (!isset($_POST) || empty($_POST)) {
     file_put_contents('monetico_log.txt', date('Y-m-d H:i:s') . " - ERROR: No se recibieron datos por POST.\n", FILE_APPEND);
--- txt/txt.php
+++ txt/txt.php
@@ -1,12 +1,15 @@
 <?php
 
 
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 $archivo = __DIR__ . '/txt/prueba.txt';
 
 // Leer el contenido del archivo
 $contenidoJson = file_get_contents($archivo);
+
 
 // Decodificar el JSON a un objeto PHP
 $objeto = json_decode($contenidoJson);
