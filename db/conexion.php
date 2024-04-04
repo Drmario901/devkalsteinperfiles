32c61871580cbfcbbd3c19506192e023f1238468
MoisesDev24 <142364403+MoisesDev24@users.noreply.github.com>
2024-04-04 10:30:35 -0400

    prueba bases de datos

--- db/conexion.php
+++ db/conexion.php
@@ -85,7 +85,7 @@
     }
 
     // Ejecutar la funcion para obtener la configuracion de conexion
-    $configuracionUsuario = obtenerConfiguracion('fr', $configuraciones);
+    $configuracionUsuario = obtenerConfiguracion('plus', $configuraciones);
 
     // Establecer la conexion
     $conexion = new mysqli(
