4f8f532f6a1236fe63ab40a182bbcd20a006cd30
Diego Horesok <diegolancer27@gmail.com>
2024-04-04 09:39:52 -0400

    aaaaa

--- php/showInfoReportRequest.php
+++ php/showInfoReportRequest.php
@@ -71,21 +71,24 @@
         </div>
         <div class='col-6'>    
           <input type='text' id='description-1' class='form-control' placeholder='Descripción' aria-label='Username'>
+
+          <div class='col-3'>
+            <label style='text-aling:center; width:100%;'>Zip-Code</label>
+            <input type='number' id='zipCode-1' class='form-control' placeholder='zip-code' aria-label='Username'>
+            
+            <select name ='precio' id='precio'>
+            <option value='0'>Elegir Tipo de Moneda</option>
+            <option value='USD'>USD</option>
+            <option value='EUR'>EUR</option>
+            </select>
+          </div>
+
         </div>
         <div class='col-6'>
           <input type='number' id='price-1' class='form-control' placeholder='Precio $' aria-label='Username'>
           <input type='hidden' name='precio_uno' id='precio_uno' value=''>
         </div>
-        <div class='col-3'>
-          <label style='text-aling:center; width:100%;'>Zip-Code</label>
-          <input type='number' id='zipCode-1' class='form-control' placeholder='zip-code' aria-label='Username'>
-          
-          <select name ='precio' id='precio'>
-          <option value='0'>Elegir Tipo de Moneda</option>
-          <option value='USD'>USD</option>
-          <option value='EUR'>EUR</option>
-          </select>
-        </div>
+       
 
       
       </div>
