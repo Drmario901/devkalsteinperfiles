73a6157605d0139e28bd118be1cd00dd8eaf41a5
VicAlarDev <vic.alarcondev@gmail.com>
2024-04-04 08:25:10 -0400

    zzzzzzzz

--- src/manufacturer/js/productRequest.js
+++ src/manufacturer/js/productRequest.js
@@ -316,7 +316,7 @@ function imgVal(file, id) {
 
   image.setAttribute("hidden", "");
 
-  /* if (err_msg == "") {
+  if (err_msg == "") {
     image.onload = function () {
       if (image.width < 900 || image.height < 900) {
         iziToast.error({
@@ -324,10 +324,9 @@ function imgVal(file, id) {
           message: alertsTranslations.img900,
           position: "center",
         });
-
       }
     };
-  } */
+  }
   document.querySelector("#" + id).value = "";
 
   image.src = objectUrl;
