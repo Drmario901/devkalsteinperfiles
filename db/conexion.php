c76a3860631e5020b5c67774402286156c5435b5
Katherine <katleald0412@gmail.com>
2024-04-05 10:01:25 -0400

    Prueba base de datos

--- db/conexion.php
+++ db/conexion.php
@@ -7,6 +7,8 @@
     $lang = isset($_COOKIE['language']) ? $_COOKIE['language'] : 'en';
     $country = isset($_COOKIE['country']) ? $_COOKIE['country'] : 'NET_EN';
 
+    echo "pais: ", $country;
+
     // Configuraciones de conexion para cada idioma
     $configuraciones = [
         'plus' => [
