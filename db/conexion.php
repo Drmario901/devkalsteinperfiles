92eed87a73bc3c3837e8d3f0557f43795f2103bc
Katherine <katleald0412@gmail.com>
2024-04-05 14:51:12 -0400

    sisa

--- db/conexion.php
+++ db/conexion.php
@@ -166,7 +166,7 @@
         }
     }
 
-    $configuracionUsuario = obtenerConfiguracion('EC', $configuraciones);
+    $configuracionUsuario = obtenerConfiguracion('plus', $configuraciones);
 
     // Establecer la conexion
     $conexion = new mysqli(
