319f8b9f9c75f4c6213bc422def5d1b79b760d15
Alejandro <tzareg96@gmail.com>
2024-04-04 20:05:47 -0400

    Cambios en el script del formulario de registro

--- src/js/login.script.js
+++ src/js/login.script.js
@@ -738,21 +738,13 @@ jQuery(document).ready(function ($) {
 
   $(document).on('change', '#countryPrefix', function(){
     let  prefix = $(this).find(':selected').data('iso')
-    let prefix2 = prefix.toLowerCase()
-    $.ajax({
-      url: 'https://restcountries.com/v3.1/alpha/'+prefix2, // Ejemplo con Estados Unidos (US)
-      type: 'GET',
-      success: function(result) {
-        console.log(result); // La respuesta del servidor
-        // Para obtener la URL de la bandera
-        var flagUrl = result[0].flags.svg;
-        // Puedes hacer algo como agregar la bandera a una imagen en tu HTML
-        $('#imgCountry').attr('src', flagUrl);
-      },
-      error: function(err) {
-        console.log(err); // Si hay algún error
-      }
-    });
+    let prefix2 = $('#countryPrefix').val()
+    $('#imgCountry').attr('src', plugin_dir+'/src/images/flag-icon/'+prefix+'.png');
+    $('#span-prefix').text(prefix2)
+  })
+
+  $(document).on('click', '#imgCountry', function(){
+    $('#countryPrefix').click();
   })
 
   function validarCorreo(consulta) {
