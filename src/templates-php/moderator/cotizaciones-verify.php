6aee84b10b8f2b715b9295e1d6c0d55a5ac583f5
Diego Horesok <diegolancer27@gmail.com>
2024-04-08 10:20:05 -0400

    aaaaa

--- src/templates-php/moderator/cotizaciones-verify.php
+++ src/templates-php/moderator/cotizaciones-verify.php
@@ -1,1 +1,22 @@
-<h1>cotizaciones verify</h1>
+<?php
+require __DIR__ . '/../../../db/conexion.php';
+/* ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL); */
+
+?>
+
+<header class="header" data-header>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+  <?php
+  include 'navbar.php';
+  ?>
+  <script>
+    let page = "render";
+
+    document.querySelector('#link-' + page).classList.add("active");
+    document.querySelector('#link-' + page).removeAttribute("style");
+  </script>
+</header>
+
