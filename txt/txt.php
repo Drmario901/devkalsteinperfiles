1a74bab78c748bb594602d26a25ec79c1288fe03
Diego Horesok <diegolancer27@gmail.com>
2024-04-08 13:09:58 -0400

    asdasdsd

--- txt/txt.php
+++ txt/txt.php
@@ -1,4 +1,8 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 function procesarArchivoMonetico($archivo, $conexion) {
     if (!is_readable($archivo)) {
         die("El archivo no existe o no se puede leer.");
