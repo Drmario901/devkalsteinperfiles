734a7380314c65f64edb33bd15bee8e9200775b9
Diego Horesok <diegolancer27@gmail.com>
2024-04-08 14:03:37 -0400

    asdasd

--- src/templates-php/moderator/cotizaciones-verify.php
+++ src/templates-php/moderator/cotizaciones-verify.php
@@ -1,8 +1,11 @@
 <?php
-require __DIR__ . '/../../../db/conexion.php';
-/* ini_set('display_errors', 1);
+
+ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL); */
+error_reporting(E_ALL); 
+
+require __DIR__ . '/../../../db/conexion.php';
+
 
  if (isset($_SESSION['privateEmailAccount'])){
         $acc_id = $_SESSION['privateEmailAccount'];
