d9253eac91cc2413780bcfca8a496e484fbf392f
Diego Horesok <diegolancer27@gmail.com>
2024-04-08 14:02:37 -0400

    sadasda

--- src/templates-php/moderator/cotizaciones-verify.php
+++ src/templates-php/moderator/cotizaciones-verify.php
@@ -27,3 +27,45 @@ error_reporting(E_ALL); */
   </script>
 </header>
 
+<body>
+<div class="container mt-5">
+    <table class="table table-responsive-md">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>ID Cotización</th>
+                <th>Monto Total</th>
+                <th>Divisa</th>
+                <th>ID Remitente</th>
+                <th>Estado del Pago</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($datos as $fila): ?>
+            <tr>
+                <td><?php echo htmlspecialchars($fila['id']); ?></td>
+                <td><?php echo htmlspecialchars($fila['id_cotizacion']); ?></td>
+                <td><?php echo htmlspecialchars($fila['monto_total']); ?></td>
+                <td><?php echo htmlspecialchars($fila['cotizacion_divisa']); ?></td>
+                <td><?php echo htmlspecialchars($fila['cotizacion_id_remitente']); ?></td>
+                <td><?php echo htmlspecialchars($fila['status_payment']); ?></td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+
+    <nav>
+        <ul class="pagination">
+            <?php for($i = 1; $i <= $totalPaginas; $i++): ?>
+                <li class="page-item <?php if($i === $pagina) echo 'active'; ?>">
+                    <a class="page-link" href="tabla.php?pagina=<?php echo $i; ?>"><?php echo $i; ?></a>
+                </li>
+            <?php endfor; ?>
+        </ul>
+    </nav>
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.7/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+
