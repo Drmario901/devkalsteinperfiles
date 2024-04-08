68e5861e209d77a524d575c45d75d8e956dad240
Diego Horesok <diegolancer27@gmail.com>
2024-04-08 08:43:33 -0400

    aasas

--- src/templates-php/moderator/quotes.php
+++ src/templates-php/moderator/quotes.php
@@ -1,6 +1,9 @@
 
 <header class="header" data-header>
     <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 
         include 'navbar.php';
     
