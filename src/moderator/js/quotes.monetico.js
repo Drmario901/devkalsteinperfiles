58a89d2a29698e81db5897e730cff39404c2b276
Diego Horesok <diegolancer27@gmail.com>
2024-04-08 15:21:37 -0400

    asdasd

--- src/moderator/js/quotes.monetico.js
+++ src/moderator/js/quotes.monetico.js
@@ -39,9 +39,3 @@ jQuery(document).ready(function() {
         alert('¡Botón clickeado!');
     });
 });
-
-$(document).ready(function() {
-    $(document).on('click', '#boton-verify', function() {
-        alert('¡Botón clickeado!');
-    });
-});
