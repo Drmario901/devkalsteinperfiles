2eeb667091cad4cf4da0e9f7c4e415d88db9a405
Diego Horesok <diegolancer27@gmail.com>
2024-04-09 14:38:56 -0400

    asdsadasd

--- src/moderator/js/quotes.monetico.js
+++ src/moderator/js/quotes.monetico.js
@@ -29,7 +29,7 @@ jQuery(document).ready(function() {
                 console.log('respuestaaa', JSON.parse(response));
                 
                 $('#datos-tabla').html(generarTabla(respuesta.datos));
-                $('#paginacion').html(generarPaginacion(respuesta.paginaActual, respuesta.totalPaginas));
+                // $('#paginacion').html(generarPaginacion(respuesta.paginaActual, respuesta.totalPaginas));
             },
             error: function() {
                 $('#datos-tabla').html('<p>Hubo un error al cargar los datos.</p>');
@@ -51,14 +51,14 @@ jQuery(document).ready(function() {
         return tablaHtml;
     }
 
-    function generarPaginacion(paginaActual, totalPaginas) {
-        let paginacionHtml = '<nav><ul class="pagination">';
-        for(let i = 1; i <= totalPaginas; i++) {
-            paginacionHtml += `<li class="page-item ${paginaActual === i ? 'active' : ''}"><a class="page-link" href="#" onclick="cargarDatos(${i});return false;">${i}</a></li>`;
-        }
-        paginacionHtml += '</ul></nav>';
-        return paginacionHtml;
-    }
+    // function generarPaginacion(paginaActual, totalPaginas) {
+    //     let paginacionHtml = '<nav><ul class="pagination">';
+    //     for(let i = 1; i <= totalPaginas; i++) {
+    //         paginacionHtml += `<li class="page-item ${paginaActual === i ? 'active' : ''}"><a class="page-link" href="#" onclick="cargarDatos(${i});return false;">${i}</a></li>`;
+    //     }
+    //     paginacionHtml += '</ul></nav>';
+    //     return paginacionHtml;
+    // }
 
     $('#boton-verify').click(function() {
         alert('¡Botón clickeado!');
