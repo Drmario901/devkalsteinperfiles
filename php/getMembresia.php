3ee489cc6f32503df1b61b8f443b60e486fa5be9
VicAlarDev <vic.alarcondev@gmail.com>
2024-04-29 14:42:04 -0400

    aaaaa

---
+++ php/getMembresia.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'conexion.php';
+
+$acc_id = $_SESSION['emailAccount'];
+
+$sql = "SELECT tipo_membresia FROM wp_account WHERE account_correo = '$acc_id'";
+
+$result = $conexion->query($sql);
+
+// guardar en la session el tipo de membresia
+if ($result->num_rows > 0) {
+  while ($row = $result->fetch_assoc()) {
+    $_SESSION['tipo_membresia'] = $row['tipo_membresia'];
+  }
+} else {
+  echo "0 results";
+}
