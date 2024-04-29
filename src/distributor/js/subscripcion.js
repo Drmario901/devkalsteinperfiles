0eceb12bca16314f47428fe80513c38d348c699b
Diego Horesok <diegolancer27@gmail.com>
2024-04-29 14:48:56 -0400

    asdas

--- src/distributor/js/subscripcion.js
+++ src/distributor/js/subscripcion.js
@@ -152,7 +152,7 @@ jQuery(document).ready(function ($) {
   function crearBotones() {
     $.each(botones, function (i, boton) {
       let $btn = $("#tbl-botones").append(
-        $("<a>").text(boton.membresia).attr("id", boton.id).addClass("btn")
+        $("<a>").text(boton.membresia).attr("id", boton.id).addClass("btn-tbl")
       );
     });
   }
