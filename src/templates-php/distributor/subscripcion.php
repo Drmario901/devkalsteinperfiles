ab3d8445ee75082ed4d9806364d66c16ae0675f0
Diego Horesok <diegolancer27@gmail.com>
2024-04-29 10:39:19 -0400

    navBar en subs distri

--- src/templates-php/distributor/subscripcion.php
+++ src/templates-php/distributor/subscripcion.php
@@ -1,1 +1,20 @@
+<header class="header" data-header>
+  <?php
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+  if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+  }
+  if (isset($_SESSION['emailAccount'])) {
+    $email = $_SESSION['emailAccount'];
+  }
+  include 'navbar.php';
+  ?>
+  <script>
+    let page = "home";
+    document.querySelector('#link-' + page).classList.add("active");
+    document.querySelector('#link-' + page).removeAttribute("style");
+  </script>
+</header>
 <h1>Subscripcion dis</h1>
