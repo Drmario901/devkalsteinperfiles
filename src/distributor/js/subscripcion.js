300af2dbc7c16513e672b74b7de0bc5f574c363a
Diego Horesok <diegolancer27@gmail.com>
2024-04-29 12:15:05 -0400

    asdasd

--- src/distributor/js/subscripcion.js
+++ src/distributor/js/subscripcion.js
@@ -21,11 +21,18 @@ jQuery(document).ready(function ($) {
   $thead.append($trTitles);
   $(".membership-table").append($thead);
 
+  //
+
   const $tbody = $("<tbody>");
   $.each(informacionTabla, function (index, fila) {
-    const $tr = $("<tr>");
-    $("<td>").text(fila.titulo).appendTo($tr); // Añade el título de la fila
-
+    const $trDatas = $("<tr>").attr("id", "tr-data");
+    $("<td>").text(fila.titulo).appendTo($trDatas); // Añade el título de la fila
+    $("<th>").text(fila.titulo).appendTo($trDatas);
+    informacionTabla.forEach((data) => {
+      data.assets.forEach((e) => {
+        $("<th>").text(e).appendTo($trDatas);
+      });
+    });
     // Añade las celdas de assets, incluyendo un cheque si corresponde
   });
   $(".membership-table").append($tbody);
