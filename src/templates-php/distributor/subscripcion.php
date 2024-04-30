f5ceab89e987ae82eec487035cb87b3729434a41
Diego Horesok <diegolancer27@gmail.com>
2024-04-30 11:48:02 -0400

    adads

--- src/templates-php/distributor/subscripcion.php
+++ src/templates-php/distributor/subscripcion.php
@@ -119,6 +119,55 @@
   .btn-nav-subs {
     color: #213280;
   }
+
+  .btn-tbl-cancelar {
+    padding: 1.1em 2em;
+    background: none;
+    border: 2px solid #fff;
+    font-size: 15px;
+    color: white !important;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+    transition: all 0.3s;
+    border-radius: 12px;
+    background-color: #c1121f;
+    font-weight: bolder;
+    box-shadow: 0 2px 0 2px #000;
+
+    .btn-tbl:before {
+      content: "";
+      position: absolute;
+      width: 100px;
+      height: 120%;
+      background-color: #fff;
+      top: 50%;
+      transform: skewX(30deg) translate(-150%, -50%);
+      transition: all 0.5s;
+    }
+
+    .btn-tbl-cancelar:hover {
+      background-color: #c1121f;
+      color: #fff !important;
+      box-shadow: 0 2px 0 2px #c1121f;
+    }
+
+    .btn-tbl-cancelar:hover::before {
+      transform: skewX(30deg) translate(150%, -50%);
+      transition-delay: 0.1s;
+    }
+
+    .btn-tbl-cancelar:active {
+      transform: scale(0.9);
+    }
+
+    html a:hover,
+    .btLightSkin a:hover,
+    .btn-tbl-cancelar:hover {
+      text-decoration: none;
+      color: #fff !important;
+    }
+  }
 </style>
 
 <header class="header" data-header>
