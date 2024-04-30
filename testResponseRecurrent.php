3f4944a3646765115eeae4ec25a16a11e186d048
Edithson <marioloquendero32@gmail.com>
2024-04-30 11:52:18 -0400

    KDMFLKDMFLKDMFK

--- testResponseRecurrent.php
+++ testResponseRecurrent.php
@@ -15,12 +15,12 @@ use DansMaCulotte\Monetico\Responses\PurchaseResponse;
 
 $data = $_POST;
 
-if (!empty($data)) {
-    file_put_contents('monetico_log_recurrent.txt', date('Y-m-d H:i:s') . " - Datos recibidos: " . json_encode($data) . "\n", FILE_APPEND);
+file_put_contents('monetico_log_recurrent.txt', date('Y-m-d H:i:s') . " - Datos recibidos: " . json_encode($data) . "\n", FILE_APPEND);
 
+if (!empty($data)) {
     $monetico = new Monetico(
-        '7590531', 
-        '530C185A56C2A9F904681A527780EBDB8C0E6C99',
+        '7593339', 
+        '255D023E7A0BDE9EEAC7516959CD93A9854F3991', 
         'kalsteinfr' 
     );
 
@@ -28,15 +28,13 @@ if (!empty($data)) {
     $result = $monetico->validate($response);
 
     if ($result) {
-        echo "version=2\n";
-        echo "cdr=0\n";
+        echo "version=2\ncdr=0"; 
     } else {
-        echo "version=2\n";
-        echo "cdr=1\n";
+        echo "version=2\ncdr=1"; 
     }
 } else {
-    file_put_contents('monetico_log_recurrent.txt', date('Y-m-d H:i:s') . " - ERROR: No se recibieron datos.\n", FILE_APPEND);
     echo "ERROR: No se recibieron datos.";
+    file_put_contents('monetico_log_recurrent.txt', date('Y-m-d H:i:s') . " - ERROR: No se recibieron datos.\n", FILE_APPEND);
 }
 
 ?>
