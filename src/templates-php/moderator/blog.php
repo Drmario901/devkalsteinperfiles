f0349d2b4ef390cd82cfac41221768c3fa040926
katherineleald <katleald0412@gmail.com>
2024-04-30 14:52:05 -0400

    vvubpp

--- src/templates-php/moderator/blog.php
+++ src/templates-php/moderator/blog.php
@@ -6,49 +6,51 @@
     
     ?>
     <script>
-        let page = "accounts";
+    let page = "blog";
 
-        document.querySelector('#link-' + page).classList.add("active");
-        document.querySelector('#link-' + page).removeAttribute("style");
-        </script>
+    document.querySelector('#link-' + page).classList.add("active");
+    document.querySelector('#link-' + page).removeAttribute("style");
+    </script>
 </header>
-<main>   
+<main>
     <article class="container article">
-        
+
         <div class="row">
-        <div class='col-lg-6'>
-                            <div class='card row m-2'>
-                                <div class='col-12'>
-                                    <div class='d-flex'>
-                                    <div>
-                                        <img class='mx-1' src='https://pbs.twimg.com/media/F9QcGzgXEAAPqLo.jpg' width=150>
-                                    </div>
-                                    <div>
-                                        <h6>Titulo del Artículo</h6>
-                                        <p class='mb-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean elit risus, placerat et condimentum eget, sodales sed elit. Sed nec.</p>
-                                    </div>
-                                    </div>
-                                </div>
-                                <p><i class='fa-solid fa-user'></i><b>Published by: user@mail.com</b></p>
-                                <div class='col-12'>
-                                    <a href='https://dev.kalstein.plus/plataforma/index.php/moderator/view-blog?accid=$aid'>
-                                        <button type='button' id='btnUpdate' class='btn btn-info btn-block p-2 px-4'>Check</button>
-                                    </a>
-                                </div>
-                                <div class='mt-2'>
-                                <div class='fw-bold card' style='border: solid 1px #27aa3f; border-radius: 5px; background-color: #86e397; padding: 10px 20px;'>
-                            <p class='m-0 p-0'><i class='fa-regular fa-circle-check'></i> Verifying by: mod@mail.com</p>
+            <div class='col-lg-6'>
+                <div class='card row m-2'>
+                    <div class='col-12'>
+                        <div class='row'>
+                            <div col-6>
+                                <img class='mx-1' src='https://pbs.twimg.com/media/F9QcGzgXEAAPqLo.jpg' width=150>
                             </div>
-                                <div class='col-12 mt-2'>
-                                    <i class='fas fa-clock'></i>
-                                    Hace 6 minutos
-                                </div>
-                                </div>
+                            <div class='col-6'>
+                                <h6>Titulo del Artículo</h6>
+                                <p class='mb-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean elit
+                                    risus, placerat et condimentum eget, sodales sed elit. Sed nec.</p>
                             </div>
                         </div>
+                    </div>
+                    <p><i class='fa-solid fa-user'></i><b>Published by: user@mail.com</b></p>
+                    <div class='col-12'>
+                        <a href='https://dev.kalstein.plus/plataforma/index.php/moderator/view-blog?accid=$aid'>
+                            <button type='button' id='btnUpdate' class='btn btn-info btn-block p-2 px-4'>Check</button>
+                        </a>
+                    </div>
+                    <div class='mt-2'>
+                        <div class='fw-bold card'
+                            style='border: solid 1px #27aa3f; border-radius: 5px; background-color: #86e397; padding: 10px 20px;'>
+                            <p class='m-0 p-0'><i class='fa-regular fa-circle-check'></i> Verifying by: mod@mail.com</p>
+                        </div>
+                        <div class='col-12 mt-2'>
+                            <i class='fas fa-clock'></i>
+                            Hace 6 minutos
+                        </div>
+                    </div>
+                </div>
+            </div>
             <!-- <?php
 
-                require __DIR__.'/../../../php/conexion.php';
+                // require __DIR__.'/../../../php/conexion.php';
 
                 function time_elapsed_string($datetime, $full = false) {
                     $now = new DateTime;
