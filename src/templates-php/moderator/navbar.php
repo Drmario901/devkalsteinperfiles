a603d4fd5eca524571a7cc9564fdc3273a83ea12
Alejandro <tzareg96@gmail.com>
2024-04-30 12:59:41 -0400

    asdasdas

--- src/templates-php/moderator/navbar.php
+++ src/templates-php/moderator/navbar.php
@@ -155,7 +155,7 @@ if ($acc_img == '') {
                     <li>
                         <a href='https://dev.kalstein.plus/plataforma/private-kalstein-moderator/guide'
                             id="link-accounts" class='navbar-link icon-box'>
-                            <span data-i18n="moderator:Informational Guides">Informational Guides</span>
+                            <span data-i18n="moderator:Guides">Informational Guides</span>
                         </a>
                     </li>
                     <li>
