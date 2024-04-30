c57919527dd8e2ae1e42e7a8fd4377ef5da04463
Diego Horesok <diegolancer27@gmail.com>
2024-04-30 08:56:54 -0400

    adasd

--- src/templates-php/distributor/subscripcion.php
+++ src/templates-php/distributor/subscripcion.php
@@ -1,25 +1,3 @@
-<header class="header" data-header>
-  <?php
-  ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);
-  error_reporting(E_ALL);
-  if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-  }
-  if (isset($_SESSION['emailAccount'])) {
-    $email = $_SESSION['emailAccount'];
-  }
-  include 'navbar.php';
-  ?>
-  <script>
-    let page = "home";
-    document.querySelector('#link-' + page).classList.add("active");
-    document.querySelector('#link-' + page).removeAttribute("style");
-  </script>
-</header>
-
-
-
 <style>
   section {
     font-family: Arial, sans-serif;
@@ -138,7 +116,26 @@
     color: #fff !important;
   }
 </style>
-</head>
+
+<header class="header" data-header>
+  <?php
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+  if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+  }
+  if (isset($_SESSION['emailAccount'])) {
+    $email = $_SESSION['emailAccount'];
+  }
+  include 'navbar.php';
+  ?>
+  <script>
+    let page = "home";
+    document.querySelector('#link-' + page).classList.add("active");
+    document.querySelector('#link-' + page).removeAttribute("style");
+  </script>
+</header>
 
 <section style="margin-top: 2rem;">
 
