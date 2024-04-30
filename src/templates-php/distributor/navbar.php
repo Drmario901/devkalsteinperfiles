e5ba0bd59bd7c469024e8b58463af5c5708a28c3
Diego Horesok <diegolancer27@gmail.com>
2024-04-30 09:16:50 -0400

    asdsad

--- src/templates-php/distributor/navbar.php
+++ src/templates-php/distributor/navbar.php
@@ -46,7 +46,7 @@ if ($acc_img == '') {
                 <div class="d-flex flex-row">
                     <li title="Redireccionar a subscripcion">
                         <a href="https://dev.kalstein.plus/plataforma/distribuidor/subscripcion/" class='navbar-link icon-box'>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-credit-card navbar-link icon-box" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-credit-card" viewBox="0 0 16 16">
                                 <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v1h14V4a1 1 0 0 0-1-1zm13 4H1v5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1z" />
                                 <path d="M2 10a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z" />
                             </svg>
