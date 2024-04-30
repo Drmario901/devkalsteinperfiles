908158695484d318a48051548ab1d575b804a900
katherineleald <katleald0412@gmail.com>
2024-04-30 15:03:17 -0400

    eeeee

--- src/templates-php/moderator/blog.php
+++ src/templates-php/moderator/blog.php
@@ -30,7 +30,7 @@
                             </div>
                         </div>
                     </div>
-                    <p class='mt-2 mb-0'><i class='fa-solid fa-user'></i> <b>Published by: user@mail.com</b></p>
+                    <p class='mt-2 mb-1'><i class='fa-solid fa-user'></i> <b>Published by: user@mail.com</b></p>
                     <div class='col-12'>
                         <a href='https://dev.kalstein.plus/plataforma/index.php/moderator/view-blog?accid=$aid'>
                             <button type='button' id='btnUpdate' class='btn btn-info btn-block p-2 px-4'>Check</button>
