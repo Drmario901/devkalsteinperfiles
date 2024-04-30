d69e4fd72d2f92de4529aff2048f76d53f7dd218
Edithson <marioloquendero32@gmail.com>
2024-04-30 10:51:27 -0400

    LDHVLSDHJSHDFJKHSDFKJH

---
+++ testResponseRecurrent.php
@@ -0,0 +1,46 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require '/home/kalsteinplus/public_html/dev.kalstein.plus/plataforma/wp-content/plugins/kalsteinPerfiles/vendor/autoload.php';
+
+$archivoLog = "/home/kalsteinplus/public_html/dev.kalstein.plus/plataforma/wp-content/plugins/kalsteinPerfiles/monetico_recurrent_log.txt"; 
+
+use DansMaCulotte\Monetico\Monetico;
+use DansMaCulotte\Monetico\Responses\PurchaseResponse;
+
+$data = $_POST;
+
+file_put_contents('monetico_recurrent_log.txt', date('Y-m-d H:i:s') . " - Datos recibidos: " . json_encode($data) . "\n", FILE_APPEND);
+
+$dataFormat = json_encode($data);
+
+print_r($dataFormat);
+
+
+if (!isset($_POST) || empty($_POST)) {
+    file_put_contents('monetico_recurrent_log.txt', date('Y-m-d H:i:s') . " - ERROR: No se recibieron datos por POST.\n", FILE_APPEND);
+    echo "ERROR: No se recibieron datos por POST.";
+    exit(); 
+}
+
+$monetico = new Monetico(
+    '7590531',
+    '530C185A56C2A9F904681A527780EBDB8C0E6C99',
+    'kalsteinfr'
+);
+
+$response = new PurchaseResponse($data);
+
+$result = $monetico->validate($response);
+
+if ($result) {
+    echo "version=2\n";
+    echo "cdr=0\n";
+} else {
+    echo "version=2\n";
+    echo "cdr=1\n";
+}
+
+?>
