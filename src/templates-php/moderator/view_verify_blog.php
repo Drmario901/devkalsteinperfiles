102546a3d4da60c013567d50213aedef4fddc582
katherineleald <katleald0412@gmail.com>
2024-05-02 10:26:29 -0400

    aaaaaaaaa

--- src/templates-php/moderator/view_verify_blog.php
+++ src/templates-php/moderator/view_verify_blog.php
@@ -76,7 +76,26 @@ h5 {
                         Description
                     </h5>
                     <div class='my-2 p-2' style='border: solid 1px #c9c9c9; borde-radius: 10px'>
-                        <p style='text-align: justify;'>aaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+                        <p style='text-align: justify;'>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed faucibus purus, id bibendum
+                            arcu. Nunc dictum nibh lorem. In hac habitasse platea dictumst. Pellentesque habitant morbi
+                            tristique senectus et netus et malesuada fames ac turpis egestas. Donec vitae enim feugiat,
+                            blandit dolor at, placerat ex. Nullam finibus ex in quam elementum tempor. Mauris et
+                            sollicitudin velit, vel elementum neque. Nullam facilisis iaculis ligula, quis euismod ipsum
+                            lacinia sit amet. Proin tincidunt sapien in leo volutpat dignissim. Quisque porta urna vitae
+                            lacus ultricies, ut blandit diam pellentesque. Aliquam erat volutpat. Cras sit amet placerat
+                            eros, id tempor quam. Cras varius placerat posuere.
+
+                            Integer nisl massa, pretium eget dictum a, posuere ut purus. Pellentesque cursus turpis et
+                            semper ultrices. Phasellus at vehicula erat. Pellentesque tincidunt euismod erat sit amet
+                            malesuada. Aenean sodales tincidunt tellus quis dictum. Aenean dapibus rutrum feugiat.
+                            Maecenas dignissim, nulla at sagittis sagittis, justo arcu egestas massa, vel fringilla ex
+                            magna non massa. Maecenas nec ullamcorper ligula. Morbi sapien tellus, iaculis in mollis eu,
+                            dapibus sit amet urna. Suspendisse a ipsum quis libero accumsan tempus. Aenean rhoncus
+                            posuere lectus. Suspendisse facilisis nec justo ac auctor. Vivamus in quam in quam hendrerit
+                            scelerisque vitae nec enim. Nunc libero nisl, aliquam vitae mauris eu, suscipit fermentum
+                            nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+                            curae; Nam vitae ex luctus, mattis velit id, sollicitudin arcu.</p>
                     </div>
                     <p>
                         <label for=''>Links or self-promotion</label>
@@ -101,13 +120,16 @@ h5 {
                 <h6 class='text-start'>Lorem Ipsum</h6>
 
             </div>
-            <div class='row mt-2'>
+            <div class='row mt-3'>
                 <h5>
                     <i class='fas fa-circle-info'></i>
                     Description
                 </h5>
                 <div class='my-2 p-2' style='border: solid 1px #c9c9c9; borde-radius: 10px'>
-                    <p style='text-align: justify;'>aaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+                    <p style='text-align: justify;'>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed faucibus purus, id bibendum arcu. Nunc dictum nibh lorem. In hac habitasse platea dictumst. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec vitae enim feugiat, blandit dolor at, placerat ex. Nullam finibus ex in quam elementum tempor. Mauris et sollicitudin velit, vel elementum neque. Nullam facilisis iaculis ligula, quis euismod ipsum lacinia sit amet. Proin tincidunt sapien in leo volutpat dignissim. Quisque porta urna vitae lacus ultricies, ut blandit diam pellentesque. Aliquam erat volutpat. Cras sit amet placerat eros, id tempor quam. Cras varius placerat posuere.
+
+Integer nisl massa, pretium eget dictum a, posuere ut purus. Pellentesque cursus turpis et semper ultrices. Phasellus at vehicula erat. Pellentesque tincidunt euismod erat sit amet malesuada. Aenean sodales tincidunt tellus quis dictum. Aenean dapibus rutrum feugiat. Maecenas dignissim, nulla at sagittis sagittis, justo arcu egestas massa, vel fringilla ex magna non massa. Maecenas nec ullamcorper ligula. Morbi sapien tellus, iaculis in mollis eu, dapibus sit amet urna. Suspendisse a ipsum quis libero accumsan tempus. Aenean rhoncus posuere lectus. Suspendisse facilisis nec justo ac auctor. Vivamus in quam in quam hendrerit scelerisque vitae nec enim. Nunc libero nisl, aliquam vitae mauris eu, suscipit fermentum nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nam vitae ex luctus, mattis velit id, sollicitudin arcu.</p>
                 </div>
                 <p>
                     <label for=''>Links or self-promotion</label>
@@ -131,13 +153,16 @@ h5 {
                 <h6 class='text-start'>Lorem Ipsum</h6>
 
             </div>
-            <div class='row mt-2'>
+            <div class='row mt-3'>
                 <h5>
                     <i class='fas fa-circle-info'></i>
                     Description
                 </h5>
                 <div class='my-2 p-2' style='border: solid 1px #c9c9c9; borde-radius: 10px'>
-                    <p style='text-align: justify;'>aaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+                    <p style='text-align: justify;'>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed faucibus purus, id bibendum arcu. Nunc dictum nibh lorem. In hac habitasse platea dictumst. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec vitae enim feugiat, blandit dolor at, placerat ex. Nullam finibus ex in quam elementum tempor. Mauris et sollicitudin velit, vel elementum neque. Nullam facilisis iaculis ligula, quis euismod ipsum lacinia sit amet. Proin tincidunt sapien in leo volutpat dignissim. Quisque porta urna vitae lacus ultricies, ut blandit diam pellentesque. Aliquam erat volutpat. Cras sit amet placerat eros, id tempor quam. Cras varius placerat posuere.
+
+Integer nisl massa, pretium eget dictum a, posuere ut purus. Pellentesque cursus turpis et semper ultrices. Phasellus at vehicula erat. Pellentesque tincidunt euismod erat sit amet malesuada. Aenean sodales tincidunt tellus quis dictum. Aenean dapibus rutrum feugiat. Maecenas dignissim, nulla at sagittis sagittis, justo arcu egestas massa, vel fringilla ex magna non massa. Maecenas nec ullamcorper ligula. Morbi sapien tellus, iaculis in mollis eu, dapibus sit amet urna. Suspendisse a ipsum quis libero accumsan tempus. Aenean rhoncus posuere lectus. Suspendisse facilisis nec justo ac auctor. Vivamus in quam in quam hendrerit scelerisque vitae nec enim. Nunc libero nisl, aliquam vitae mauris eu, suscipit fermentum nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nam vitae ex luctus, mattis velit id, sollicitudin arcu.</p>
                 </div>
                 <p>
                     <label for=''>Links or self-promotion</label>
@@ -160,13 +185,16 @@ h5 {
                 <h6 class='text-start'>Lorem Ipsum</h6>
 
             </div>
-            <div class='row mt-2'>
+            <div class='row mt-3'>
                 <h5>
                     <i class='fas fa-circle-info'></i>
                     Description
                 </h5>
                 <div class='my-2 p-2' style='border: solid 1px #c9c9c9; borde-radius: 10px'>
-                    <p style='text-align: justify;'>aaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+                    <p style='text-align: justify;'>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed faucibus purus, id bibendum arcu. Nunc dictum nibh lorem. In hac habitasse platea dictumst. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec vitae enim feugiat, blandit dolor at, placerat ex. Nullam finibus ex in quam elementum tempor. Mauris et sollicitudin velit, vel elementum neque. Nullam facilisis iaculis ligula, quis euismod ipsum lacinia sit amet. Proin tincidunt sapien in leo volutpat dignissim. Quisque porta urna vitae lacus ultricies, ut blandit diam pellentesque. Aliquam erat volutpat. Cras sit amet placerat eros, id tempor quam. Cras varius placerat posuere.
+
+Integer nisl massa, pretium eget dictum a, posuere ut purus. Pellentesque cursus turpis et semper ultrices. Phasellus at vehicula erat. Pellentesque tincidunt euismod erat sit amet malesuada. Aenean sodales tincidunt tellus quis dictum. Aenean dapibus rutrum feugiat. Maecenas dignissim, nulla at sagittis sagittis, justo arcu egestas massa, vel fringilla ex magna non massa. Maecenas nec ullamcorper ligula. Morbi sapien tellus, iaculis in mollis eu, dapibus sit amet urna. Suspendisse a ipsum quis libero accumsan tempus. Aenean rhoncus posuere lectus. Suspendisse facilisis nec justo ac auctor. Vivamus in quam in quam hendrerit scelerisque vitae nec enim. Nunc libero nisl, aliquam vitae mauris eu, suscipit fermentum nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nam vitae ex luctus, mattis velit id, sollicitudin arcu.</p>
                 </div>
                 <p>
                     <label for=''>Links or self-promotion</label>
