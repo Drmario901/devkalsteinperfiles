2b7efe06559aabc9b9b3e664ab2c5a8e28456ab5
katherineleald <katleald0412@gmail.com>
2024-05-02 11:59:07 -0400

    aaaaaaaaaa

--- src/templates-php/moderator/view_verify_guide.php
+++ src/templates-php/moderator/view_verify_guide.php
@@ -123,7 +123,7 @@ h5 {
                         Tipo de producto #1
                         <input class='d-inline' type='checkbox' id='name'>
                     </h6>
-                    <h6 class='text-start' style='font-size: 1.25em;'>Lorem Ipsum</h6>
+                    <h6 class='text-start' style='font-size: 1.15em;'>Lorem Ipsum</h6>
                     <a TARGET='_blank' href='#'>
                         <img class='my-3 d-flex justify-content-start' style='margin: auto; border: 1px solid #999'
                             width=200
@@ -188,7 +188,7 @@ h5 {
                         Tipo de producto #2
                         <input class='d-inline' type='checkbox' id='name'>
                     </h6>
-                    <h6 class='text-start' style='font-size: 1.25em;'>Lorem Ipsum</h6>
+                    <h6 class='text-start' style='font-size: 1.15em;'>Lorem Ipsum</h6>
                     <a TARGET='_blank' href='#'>
                         <img class='my-3 d-flex justify-content-start' style='margin: auto; border: 1px solid #999'
                             width=200
@@ -320,56 +320,17 @@ h5 {
                 </h5>
             </div>
             <div class='row mt-3 p-2' style='border: solid 1px #c9c9c9; border-radius: 10px;'>
-                <div class='col-md-3 align-items-center'>
-                    <div>
+                <div class='row align-items-center'>
+                    <div class='col-md-2'>
                         <a TARGET='_blank' href='#'>
                             <img class='my-3 d-flex justify-content-start' style='margin: auto; border: 1px solid #999'
                                 width=200
                                 src='https://pm1.aminoapps.com/7768/20eb76b2324a56cc2e29e6222882dd2146f49920r1-300-300v2_uhq.jpg'>
                         </a>
                     </div>
-                    <div>
+                    <div class='col-md-10'>
                         <h6 class='text-start'>Lorem Ipsum <input class='d-inline' type='checkbox' id='name'></h6>
-                        <p><b>Category:</b> Jorgitox2 <input class='d-inline' type='checkbox' id='name'></p>
-                    </div>
-                </div>
-                <div class='col-md-3 align-items-center'>
-                    <div>
-                        <a TARGET='_blank' href='#'>
-                            <img class='my-3 d-flex justify-content-start' style='margin: auto; border: 1px solid #999'
-                                width=200
-                                src='https://pm1.aminoapps.com/7768/20eb76b2324a56cc2e29e6222882dd2146f49920r1-300-300v2_uhq.jpg'>
-                        </a>
-                    </div>
-                    <div>
-                        <h6 class='text-start'>Lorem Ipsum <input class='d-inline' type='checkbox' id='name'></h6>
-                        <p><b>Category:</b> Jorgitox2 <input class='d-inline' type='checkbox' id='name'></p>
-                    </div>
-                </div>
-                <div class='col-md-3 align-items-center'>
-                    <div>
-                        <a TARGET='_blank' href='#'>
-                            <img class='my-3 d-flex justify-content-start' style='margin: auto; border: 1px solid #999'
-                                width=200
-                                src='https://pm1.aminoapps.com/7768/20eb76b2324a56cc2e29e6222882dd2146f49920r1-300-300v2_uhq.jpg'>
-                        </a>
-                    </div>
-                    <div>
-                        <h6 class='text-start'>Lorem Ipsum <input class='d-inline' type='checkbox' id='name'></h6>
-                        <p><b>Category:</b> Jorgitox2 <input class='d-inline' type='checkbox' id='name'></p>
-                    </div>
-                </div>
-                <div class='col-md-3 align-items-center'>
-                    <div>
-                        <a TARGET='_blank' href='#'>
-                            <img class='my-3 d-flex justify-content-start' style='margin: auto; border: 1px solid #999'
-                                width=200
-                                src='https://pm1.aminoapps.com/7768/20eb76b2324a56cc2e29e6222882dd2146f49920r1-300-300v2_uhq.jpg'>
-                        </a>
-                    </div>
-                    <div>
-                        <h6 class='text-start'>Lorem Ipsum <input class='d-inline' type='checkbox' id='name'></h6>
-                        <p><b>Category:</b> Jorgitox2 <input class='d-inline' type='checkbox' id='name'></p>
+                        <p><b>Model:</b> Jorgitox1 <input class='d-inline' type='checkbox' id='name'></p>
                     </div>
                 </div>
 
@@ -378,55 +339,67 @@ h5 {
         <div class='card mb-3'>
             <div class='row text-sm-start text-md-center'>
                 <h5>
-                    <i class='fas fa-pen'></i>
-                    Título de extracto #3
-                    <input class='d-inline' type='checkbox' id='name'>
+                    <i class='fa-regular fa-newspaper'></i>
+                    Articulos destacados
                 </h5>
-                <h6 class='text-start'>Lorem Ipsum</h6>
-
             </div>
-            <div class='row mt-3'>
-                <h5>
-                    <i class='fas fa-circle-info'></i>
-                    Description
-                </h5>
-                <div class='my-2 p-2' style='border: solid 1px #c9c9c9; borde-radius: 10px'>
-                    <p style='text-align: justify;'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed faucibus purus, id bibendum
-                        arcu. Nunc dictum nibh lorem. In hac habitasse platea dictumst. Pellentesque habitant morbi
-                        tristique senectus et netus et malesuada fames ac turpis egestas. Donec vitae enim feugiat,
-                        blandit dolor at, placerat ex. Nullam finibus ex in quam elementum tempor. Mauris et
-                        sollicitudin velit, vel elementum neque. Nullam facilisis iaculis ligula, quis euismod ipsum
-                        lacinia sit amet. Proin tincidunt sapien in leo volutpat dignissim. Quisque porta urna vitae
-                        lacus ultricies, ut blandit diam pellentesque. Aliquam erat volutpat. Cras sit amet placerat
-                        eros, id tempor quam. Cras varius placerat posuere.
-
-                        Integer nisl massa, pretium eget dictum a, posuere ut purus. Pellentesque cursus turpis et
-                        semper ultrices. Phasellus at vehicula erat. Pellentesque tincidunt euismod erat sit amet
-                        malesuada. Aenean sodales tincidunt tellus quis dictum. Aenean dapibus rutrum feugiat.
-                        Maecenas
-                        dignissim, nulla at sagittis sagittis, justo arcu egestas massa, vel fringilla ex magna non
-                        massa. Maecenas nec ullamcorper ligula. Morbi sapien tellus, iaculis in mollis eu, dapibus
-                        sit
-                        amet urna. Suspendisse a ipsum quis libero accumsan tempus. Aenean rhoncus posuere lectus.
-                        Suspendisse facilisis nec justo ac auctor. Vivamus in quam in quam hendrerit scelerisque
-                        vitae
-                        nec enim. Nunc libero nisl, aliquam vitae mauris eu, suscipit fermentum nunc. Vestibulum
-                        ante
-                        ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nam vitae ex luctus,
-                        mattis velit id, sollicitudin arcu.</p>
+            <div class='row mt-3 p-2' style='border: solid 1px #c9c9c9; border-radius: 10px;'>
+                <div class='col-md-3 align-items-center'>
+                    <div>
+                        <a TARGET='_blank' href='#'>
+                            <img class='my-3 d-flex justify-content-start' style='margin: auto; border: 1px solid #999'
+                                width=200
+                                src='https://pm1.aminoapps.com/7768/20eb76b2324a56cc2e29e6222882dd2146f49920r1-300-300v2_uhq.jpg'>
+                        </a>
+                    </div>
+                    <div>
+                        <h6 class='text-start'>Lorem Ipsum <input class='d-inline' type='checkbox' id='name'></h6>
+                        <p><b>Category:</b> Jorgitox2 <input class='d-inline' type='checkbox' id='name'></p>
+                    </div>
                 </div>
-                <p>
-                    <label for=''>Links or self-promotion</label>
-                    <input class='d-inline' type='checkbox' id='promotions-d'><br>
-                </p>
-                <p>
-                    <label for=''>Professionalism</label>
-                    <input class='d-inline' type='checkbox' id='professionalism-d'>
-                </p>
+                <div class='col-md-3 align-items-center'>
+                    <div>
+                        <a TARGET='_blank' href='#'>
+                            <img class='my-3 d-flex justify-content-start' style='margin: auto; border: 1px solid #999'
+                                width=200
+                                src='https://pm1.aminoapps.com/7768/20eb76b2324a56cc2e29e6222882dd2146f49920r1-300-300v2_uhq.jpg'>
+                        </a>
+                    </div>
+                    <div>
+                        <h6 class='text-start'>Lorem Ipsum <input class='d-inline' type='checkbox' id='name'></h6>
+                        <p><b>Category:</b> Jorgitox2 <input class='d-inline' type='checkbox' id='name'></p>
+                    </div>
+                </div>
+                <div class='col-md-3 align-items-center'>
+                    <div>
+                        <a TARGET='_blank' href='#'>
+                            <img class='my-3 d-flex justify-content-start' style='margin: auto; border: 1px solid #999'
+                                width=200
+                                src='https://pm1.aminoapps.com/7768/20eb76b2324a56cc2e29e6222882dd2146f49920r1-300-300v2_uhq.jpg'>
+                        </a>
+                    </div>
+                    <div>
+                        <h6 class='text-start'>Lorem Ipsum <input class='d-inline' type='checkbox' id='name'></h6>
+                        <p><b>Category:</b> Jorgitox2 <input class='d-inline' type='checkbox' id='name'></p>
+                    </div>
+                </div>
+                <div class='col-md-3 align-items-center'>
+                    <div>
+                        <a TARGET='_blank' href='#'>
+                            <img class='my-3 d-flex justify-content-start' style='margin: auto; border: 1px solid #999'
+                                width=200
+                                src='https://pm1.aminoapps.com/7768/20eb76b2324a56cc2e29e6222882dd2146f49920r1-300-300v2_uhq.jpg'>
+                        </a>
+                    </div>
+                    <div>
+                        <h6 class='text-start'>Lorem Ipsum <input class='d-inline' type='checkbox' id='name'></h6>
+                        <p><b>Category:</b> Jorgitox2 <input class='d-inline' type='checkbox' id='name'></p>
+                    </div>
+                </div>
 
             </div>
         </div>
+        
         <textarea class='mx-auto my-2' style='width: 100%; height: 150px;'
             placeholder='Especifica porqué se está denegando la información' id='message'></textarea>
         <p class='d-flex justify-content-start' id='strikeContainer'>
