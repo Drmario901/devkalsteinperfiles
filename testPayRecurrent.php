880b520243035b6de83df6ec0144eb54260c2116
VicAlarDev <vic.alarcondev@gmail.com>
2024-05-02 08:54:39 -0400

    see

--- testPayRecurrent.php
+++ testPayRecurrent.php
@@ -1,8 +1,8 @@
 <?php
 //ERROR DETECTION LINES.
-ini_set('display_errors', 1);
+/* ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL); */
 
 require '/home/kalsteinplus/public_html/dev.kalstein.plus/plataforma/wp-content/plugins/kalsteinPerfiles/php/conexion.php';
 session_start();
