d6e1a0a08a3bb8b2119bc991599872a925e60718
Diego Horesok <diegolancer27@gmail.com>
2024-05-02 10:21:46 -0400

    asdsd

--- dataTime.php
+++ dataTime.php
@@ -1,5 +1,7 @@
 <?php
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 $fechaInicio = new DateTime(); // Fecha actual
 $fechaFinal = new DateTime();
 $fechaFinal->modify('+30 days');
