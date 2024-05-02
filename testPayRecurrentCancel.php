1cad580c03c3fa0ed69cf67f4623a031a4e8e0cf
Edithson <marioloquendero32@gmail.com>
2024-05-02 13:51:54 -0400

    KLFLKDFLNDFNDF

--- testPayRecurrentCancel.php
+++ testPayRecurrentCancel.php
@@ -3,6 +3,8 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE);
+
 
 // COMPOSER DEPENDENCIES.
 require '/home/kalsteinplus/public_html/dev.kalstein.plus/plataforma/wp-content/plugins/kalsteinPerfiles/vendor/autoload.php';
