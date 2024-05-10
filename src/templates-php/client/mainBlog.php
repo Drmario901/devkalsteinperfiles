0196ab46d3462ce17c96f1314a003f9232eec8a1
Edithson <marioloquendero32@gmail.com>
2024-05-10 11:04:29 -0400

    DJEHDIOEHDIUEHDIUHED

--- src/templates-php/client/mainBlog.php
+++ src/templates-php/client/mainBlog.php
@@ -1,4 +1,4 @@
-<div id='c-panel16' style='display: none;'>
+<!--div id='c-panel16' style='display: none;'-->
     <br><br><br>
     <script>alert.log('AQUI ESTOY MMGVO')</script>
-</div>
+<!--/div-->
