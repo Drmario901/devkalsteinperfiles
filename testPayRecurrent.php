d7c4c542aefbbab0bef6efa33cb1fe244f0155ae
Diego Horesok <diegolancer27@gmail.com>
2024-05-22 11:57:21 -0400

    asdasd

--- testPayRecurrent.php
+++ testPayRecurrent.php
@@ -120,6 +120,14 @@ $purchase->setClient($client);
 
 $url = base64_decode(encryptURL());
 $fields = $monetico->getFields($purchase);
+echo '<br>';
+echo '<br>';
+echo '<br>';
+echo '<br>';
+echo '<br>';
+echo '<br>';
+echo '<br>';
+var_dump('aquiii', $fields);
 
 
 ?>
@@ -144,16 +152,16 @@ $fields = $monetico->getFields($purchase);
             }
         }
     </style>
-    <form name="payment_form" action="<?php echo $url; ?>" method="post">
+    <!-- <form name="payment_form" action="<?php echo $url; ?>" method="post">
         <?php foreach ($fields as $key => $value) : ?>
             <input type="hidden" name="<?php echo $key; ?>" value="<?php echo $value; ?>">
         <?php endforeach; ?>
         <!--input type="submit" value="Pagar con Monetico"-->
-        <center><?php echo $esText ?></center>
-        <center>
-            <div class="custom-loader"></div>
-        </center>
-    </form>
+    <center><?php echo $esText ?></center>
+    <center>
+        <div class="custom-loader"></div>
+    </center>
+    </form> -->
 </body>
 
 </html>
