0cc22b048db79a47166af7db328feb5f586dbb66
Diego Horesok <diegolancer27@gmail.com>
2024-05-22 12:06:08 -0400

    ASDASD

--- testPayRecurrent.php
+++ testPayRecurrent.php
@@ -91,7 +91,7 @@ $monetico = new Monetico(
 );
 
 $purchase = new PurchaseRequest([
-    'reference' => 'QUO3454323',
+    'reference' => 'QUO3454212',
     'description' => 'uniqid: ' . $row['account_sub_id'],
     'language' => 'ES',
     'email' => $row['account_correo'],
