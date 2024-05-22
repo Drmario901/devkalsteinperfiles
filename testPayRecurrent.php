cd5e54e911e45db995132d1daf22715724a41cdd
Diego Horesok <diegolancer27@gmail.com>
2024-05-22 13:00:50 -0400

    asdasd

--- testPayRecurrent.php
+++ testPayRecurrent.php
@@ -135,20 +135,20 @@ $fields = $monetico->getFields($purchase);
 var_dump('aaaaa  ', json_encode($fields));
 
 // Generate the form with hidden inputs
-// echo '<html><body>';
-// echo '<form id="paymentForm" action="' . htmlspecialchars($url) . '" method="POST">';
-// foreach ($fields as $key => $value) {
-//     echo '<input type="hidden" name="' . htmlspecialchars($key) . '" value="' . htmlspecialchars($value) . '">';
-// }
-// echo '<noscript><input type="submit" value="Continue"></noscript>';
-// echo '</form>';
+echo '<html><body>';
+echo '<form id="paymentForm" action="' . htmlspecialchars($url) . '" method="POST">';
+foreach ($fields as $key => $value) {
+    echo '<input type="hidden" name="' . htmlspecialchars($key) . '" value="' . htmlspecialchars($value) . '">';
+}
+echo '<noscript><input type="submit" value="Continue"></noscript>';
+echo '</form>';
 
-// // Add JavaScript to submit the form automatically
+// Add JavaScript to submit the form automatically
 // echo '<script type="text/javascript">
 //         document.getElementById("paymentForm").submit();
 //       </script>';
 
-// echo '</body></html>';
+echo '</body></html>';
 
 
 
