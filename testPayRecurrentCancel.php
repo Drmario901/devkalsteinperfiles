3131c7863ab68895101b8c60b01d559b5f250ff8
Edithson <marioloquendero32@gmail.com>
2024-05-23 15:47:47 -0400

    KJXLKJSDKJSKDJKLSDJK

--- testPayRecurrentCancel.php
+++ testPayRecurrentCancel.php
@@ -39,12 +39,12 @@ if (!preg_match('/^\d{2}\/\d{2}\/\d{4}$/', $date_commande)) {
 }
 
 // Resto de los parámetros
-$securityKey = '255D023E7A0BDE9EEAC7516959CD93A9854F3991'; // Asegúrate de que esta clave es correcta
+$securityKey = '255D023E7A0BDE9EEAC7516959CD93A9854F3991';
 $tpe = '7593339';
-$montant = '10.00USD'; // Asegúrate de que el formato es correcto y en USD
-$montant_a_capturer = '0.00USD';
-$montant_deja_capture = '0.00USD';
-$montant_restant = '0.00USD';
+$montant = '10.00EUR'; // Asegúrate de que el formato es correcto y en EUR
+$montant_a_capturer = '0.00EUR';
+$montant_deja_capture = '0.00EUR';
+$montant_restant = '0.00EUR';
 $reference = 'SUB1-1716474386';
 $version = '3.0';
 $lgue = 'FR'; // Ajustado a FR según la documentación
@@ -79,20 +79,15 @@ $response = $client->request('POST', $url, [
 ]);
 
 $responseBody = $response->getBody()->getContents();
+$responseStatusCode = $response->getStatusCode();
 
-if ($response->getStatusCode() == 200) {
-    echo "Pago recurrente cancelado exitosamente.";
-} else {
-    echo "Error al cancelar el pago recurrente: " . $responseBody;
+echo "Solicitud Enviada:<br>";
+foreach ($fields as $key => $value) {
+    echo $key . ': ' . htmlspecialchars($value) . '<br>';
 }
-?>
-<html>
-<body onload="document.forms['cancel_form'].submit();">
-    <form name="cancel_form" action="<?php echo $url; ?>" method="post">
-        <?php foreach ($fields as $key => $value): ?>
-            <input type="hidden" name="<?php echo $key; ?>" value="<?php echo htmlspecialchars($value); ?>">
-        <?php endforeach; ?>
-        <center><div class="custom-loader"></div></center>
-    </form>
-</body>
-</html>
+
+echo "<br>MAC Calculada: " . $mac . "<br>";
+
+echo "<br>Respuesta del Servidor:<br>";
+echo "Código de Estado: " . $responseStatusCode . "<br>";
+echo "Cuerpo de la Respuesta: " 
