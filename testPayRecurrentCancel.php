8ae2bc0c8434f2f24e3ef2c873a63a890c1a401f
Edithson <marioloquendero32@gmail.com>
2024-05-24 14:26:07 -0400

    ASDASDASDASDASD

--- testPayRecurrentCancel.php
+++ testPayRecurrentCancel.php
@@ -38,11 +38,11 @@ if (!preg_match('/^\d{2}\/\d{2}\/\d{4}$/', $date_commande)) {
 
 $securityKey = '255D023E7A0BDE9EEAC7516959CD93A9854F3991'; 
 $tpe = '7593339';
-$montant = '10USD'; 
+$montant = '20.00USD'; 
 $montant_a_capturer = '0.00USD';
 $montant_deja_capture = '0.00USD';
 $montant_restant = '0.00USD';
-$reference = 'SUB1-1716573462';
+$reference = 'SUB2-1716572310';
 $version = '3.0';
 $lgue = 'FR'; 
 $societe = 'kalsteinfr';
