3ce69bf12da5f520c7afe52902185a41acd40017
Diego Horesok <diegolancer27@gmail.com>
2024-05-24 13:40:49 -0400

    adasd

--- testPayRecurrent.php
+++ testPayRecurrent.php
@@ -47,166 +47,160 @@ echo '<br>';
 
 echo 'aaaa2' . $getUser;
 
-// $membershipPrices = [
-//     'SUB1' => 10,
-//     'SUB2' => 20
-// ];
+$membershipPrices = [
+    'SUB1' => 10,
+    'SUB2' => 20
+];
 
-// if (!isset($membershipPrices[$idMembership])) {
-//     die('Error: No price.');
-// }
+if (!isset($membershipPrices[$idMembership])) {
+    die('Error: No price.');
+}
 
-// $membershipPrice = $membershipPrices[$idMembership];
+$membershipPrice = $membershipPrices[$idMembership];
 
-// //MAIN QUERYS
-// $consulta = "SELECT * FROM wp_account WHERE account_correo = '$email'";
-// $row = $conexion->query($consulta)->fetch_assoc();
+//MAIN QUERYS
+$consulta = "SELECT * FROM wp_account WHERE account_correo = '$email'";
+$row = $conexion->query($consulta)->fetch_assoc();
 
-// $consulta2 = "SELECT tipo_membresia FROM wp_account WHERE account_correo = '$email'";
-// $resultado = $conexion->query($consulta2);
+$consulta2 = "SELECT tipo_membresia FROM wp_account WHERE account_correo = '$email'";
+$resultado = $conexion->query($consulta2);
 
-// if ($resultado) {
-//     if ($resultado->num_rows > 0) {
-//         $row2 = $resultado->fetch_assoc();
-//         $tipo_membresia = $row['tipo_membresia'];
+if ($resultado) {
+    if ($resultado->num_rows > 0) {
+        $row2 = $resultado->fetch_assoc();
+        $tipo_membresia = $row['tipo_membresia'];
 
-//         if ($tipo_membresia == 0) {
-//             $id_unico = uniqid(md5($email), true);
+        if ($tipo_membresia == 0) {
+            $id_unico = uniqid(md5($email), true);
 
-//             $updateQuery = "UPDATE wp_account SET account_sub_id = '$id_unico' WHERE account_correo = '$email'";
-//             if ($conexion->query($updateQuery) === TRUE) {
-//                 //echo "ID Generated.";
-//             } else {
-//                 //echo "Error updating: " . $conexion->error;
-//             }
-//         } else {
-//             //echo "The user doesn't require a Uniqid.";
-//         }
-//     } else {
-//         //echo "User not found.";
-//     }
-// } else {
-//     echo "Error executing the query: " . $conexion->error;
-// }
-// //Reference with max 12chars
+            $updateQuery = "UPDATE wp_account SET account_sub_id = '$id_unico' WHERE account_correo = '$email'";
+            if ($conexion->query($updateQuery) === TRUE) {
+                //echo "ID Generated.";
+            } else {
+                //echo "Error updating: " . $conexion->error;
+            }
+        } else {
+            //echo "The user doesn't require a Uniqid.";
+        }
+    } else {
+        //echo "User not found.";
+    }
+} else {
+    echo "Error executing the query: " . $conexion->error;
+}
+//Reference with max 12chars
 
-// $reference = $idMembership . '-' . time();
+$reference = $idMembership . '-' . time();
 
-// //COMPOSER DEPENDENCIES.
-// require '/home/kalsteinplus/public_html/dev.kalstein.plus/plataforma/wp-content/plugins/kalsteinPerfiles/vendor/autoload.php';
+//COMPOSER DEPENDENCIES.
+require '/home/kalsteinplus/public_html/dev.kalstein.plus/plataforma/wp-content/plugins/kalsteinPerfiles/vendor/autoload.php';
 
-// use DansMaCulotte\Monetico\Monetico;
-// use DansMaCulotte\Monetico\Requests\PurchaseRequest;
-// use DansMaCulotte\Monetico\Resources\BillingAddressResource;
-// use DansMaCulotte\Monetico\Resources\ShippingAddressResource;
-// use DansMaCulotte\Monetico\Resources\ClientResource;
+use DansMaCulotte\Monetico\Monetico;
+use DansMaCulotte\Monetico\Requests\PurchaseRequest;
+use DansMaCulotte\Monetico\Resources\BillingAddressResource;
+use DansMaCulotte\Monetico\Resources\ShippingAddressResource;
+use DansMaCulotte\Monetico\Resources\ClientResource;
 
-// $monetico = new Monetico(
-//     '7593339',
-//     '255D023E7A0BDE9EEAC7516959CD93A9854F3991',
-//     'kalsteinfr'
-// );
+$monetico = new Monetico(
+    '7593339',
+    '255D023E7A0BDE9EEAC7516959CD93A9854F3991',
+    'kalsteinfr'
+);
 
-// $purchase = new PurchaseRequest([
-//     'reference' => $reference,
-//     'description' => 'uniqid: ' . $row['account_sub_id'] . '  ' . 'userID:' . $row['user_tag'],
-//     'language' => 'ES',
-//     'email' => $row['account_correo'],
-//     'amount' => $membershipPrice,
-//     'currency' => 'USD',
-//     'dateTime' => new DateTime(),
-//     'successUrl' => 'https://dev.kalstein.plus/plataforma/subscripcion-aprobada/',
-//     'errorUrl' => 'https://dev.kalstein.plus/plataforma/failed-subscripcion-k/',
-// ]);
+$purchase = new PurchaseRequest([
+    'reference' => $reference,
+    'description' => 'uniqid: ' . $row['account_sub_id'] . '  ' . 'userID:' . $row['user_tag'],
+    'language' => 'ES',
+    'email' => $row['account_correo'],
+    'amount' => $membershipPrice,
+    'currency' => 'USD',
+    'dateTime' => new DateTime(),
+    'successUrl' => 'https://dev.kalstein.plus/plataforma/subscripcion-aprobada/',
+    'errorUrl' => 'https://dev.kalstein.plus/plataforma/failed-subscripcion-k/',
+]);
 
-// $billingAddress = new BillingAddressResource([
-//     'name' => $row['account_nombre'],
-//     'addressLine1' => $row['account_direccion'],
-//     'city' => $row['account_ciudad'],
-//     'postalCode' => $row['account_zipcode'],
-//     'country' => $row['account_pais']
-// ]);
+$billingAddress = new BillingAddressResource([
+    'name' => $row['account_nombre'],
+    'addressLine1' => $row['account_direccion'],
+    'city' => $row['account_ciudad'],
+    'postalCode' => $row['account_zipcode'],
+    'country' => $row['account_pais']
+]);
 
-// $purchase->setBillingAddress($billingAddress);
+$purchase->setBillingAddress($billingAddress);
 
-// $client = new ClientResource([
-//     'firstName' => $row['account_nombre'],
-//     'lastName' => $row['account_apellido'],
-// ]);
-// $purchase->setClient($client);
+$client = new ClientResource([
+    'firstName' => $row['account_nombre'],
+    'lastName' => $row['account_apellido'],
+]);
+$purchase->setClient($client);
 
-// $url = base64_decode(encryptURL());
-// $fields = $monetico->getFields($purchase);
-
-
+$url = base64_decode(encryptURL());
+$fields = $monetico->getFields($purchase);
 
 
 
-// '<br>';
-// '<br>';
-// '<br>';
-// '<br>';
-// '<br>';
-// '<br>';
-
-// var_dump('aaaaa  ', $fields);
-
-// // Generate the form with hidden inputs
-// echo '<html><body>';
-// echo '<form id="paymentForm" action="' . htmlspecialchars($url) . '" method="POST">';
-// foreach ($fields as $key => $value) {
-//     echo '<input type="hidden" name="' . htmlspecialchars($key) . '" value="' . htmlspecialchars($value) . '">';
-// }
-// echo '<noscript><input type="submit" value="Continue"></noscript>';
-// echo '</form>';
-
-// // Add JavaScript to submit the form automatically
-// echo '<script type="text/javascript">
-//         document.getElementById("paymentForm").submit();
-//       </script>';
-
-// echo '</body></html>';
 
 
+'<br>';
+'<br>';
+'<br>';
+'<br>';
+'<br>';
+'<br>';
 
-// 
+var_dump('aaaaa  ', $fields);
+
+// Generate the form with hidden inputs
+echo '<html><body>';
+echo '<form id="paymentForm" action="' . htmlspecialchars($url) . '" method="POST">';
+foreach ($fields as $key => $value) {
+    echo '<input type="hidden" name="' . htmlspecialchars($key) . '" value="' . htmlspecialchars($value) . '">';
+}
+echo '<noscript><input type="submit" value="Continue"></noscript>';
+echo '</form>';
+
+// Add JavaScript to submit the form automatically
+echo '<script type="text/javascript">
+        document.getElementById("paymentForm").submit();
+      </script>';
+
+echo '</body></html>';
+
+
+
 ?>
-// <html>
-
-//
+<html>
 
 <body onload="document.forms['payment_form'].submit();">
-    // <style>
-        //         .custom-loader {
-        //             width: 50px;
-        //             height: 50px;
-        //             border-radius: 50%;
-        //             background:
-        //                 radial-gradient(farthest-side, #322380 94%, #0000) top/8px 8px no-repeat,
-        //                 conic-gradient(#0000 30%, #322380);
-        //             -webkit-mask: radial-gradient(farthest-side, #0000 calc(100% - 8px), #000 0);
-        //             animation: s3 1s infinite linear;
-        //         }
+    <style>
+        .custom-loader {
+            width: 50px;
+            height: 50px;
+            border-radius: 50%;
+            background:
+                radial-gradient(farthest-side, #322380 94%, #0000) top/8px 8px no-repeat,
+                conic-gradient(#0000 30%, #322380);
+            -webkit-mask: radial-gradient(farthest-side, #0000 calc(100% - 8px), #000 0);
+            animation: s3 1s infinite linear;
+        }
 
-        //         @keyframes s3 {
-        //             100% {
-        //                 transform: rotate(1turn)
-        //             }
-        //         }
-        //     
+        @keyframes s3 {
+            100% {
+                transform: rotate(1turn)
+            }
+        }
     </style>
-    // <!-- <form name="payment_form" action="<?php echo $url; ?>" method="post">
-//         <?php foreach ($fields as $key => $value) : ?>
-//             <input type="hidden" name="<?php echo $key; ?>" value="<?php echo $value; ?>">
-//         <?php endforeach; ?>
-//         <!--input type="submit" value="Pagar con Monetico"-->
-    // <center><?php echo $esText ?></center>
-    // <center>
-        // <div class="custom-loader"></div>
-        // </center>
-    // </form> -->
-    // </body>
-
-//
+    <!-- <form name="payment_form" action="<?php echo $url; ?>" method="post">
+        <?php foreach ($fields as $key => $value) : ?>
+            <input type="hidden" name="<?php echo $key; ?>" value="<?php echo $value; ?>">
+        <?php endforeach; ?>
+        <!--input type="submit" value="Pagar con Monetico"-->
+    <center><?php echo $esText ?></center>
+    <center>
+        <div class="custom-loader"></div>
+    </center>
+    </form> -->
+</body>
 
 </html>
