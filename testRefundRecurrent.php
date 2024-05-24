2608eb9aae8e7283d50b17f339c5683f36ab48dd
Diego Horesok <diegolancer27@gmail.com>
2024-05-24 11:01:32 -0400

    prueba

--- testRefundRecurrent.php
+++ testRefundRecurrent.php
@@ -68,94 +68,60 @@ echo 'el idddd pero de ' . $referencia_pago;
 // COMPOSER DEPENDENCIES.
 require '/home/kalsteinplus/public_html/dev.kalstein.plus/plataforma/wp-content/plugins/kalsteinPerfiles/vendor/autoload.php';
 
-use DansMaCulotte\Monetico\Monetico;
-use DansMaCulotte\Monetico\Requests\RefundRequest;
-use DansMaCulotte\Monetico\Responses\RefundResponse;
 use GuzzleHttp\Client;
 
-// JSON response from Monetico
-$jsonResponse = '{"TPE":"7593339","date":"23\/05\/2024_a_19:48:35","montant":"20USD","reference":"SUB2-1716486501","texte-libre":"uniqid: 03ca08b782afed967aa1405e4ddd54d2664f7f0828e290.19661886  userID:@valfonsob13","code-retour":"payetest","cvx":"oui","vld":"1226","brand":"VI","motifrefus":"","usage":"inconnu","typecompte":"inconnu","ecard":"non","originecb":"FRA","bincb":"00000100","hpancb":"B552FD6DB65EC37C5B5C95F0E02A00841634AD86","ipclient":"185.28.22.84","originetr":"USA","cbmasquee":"00000100******21","modepaiement":"CB","authentification":"ewogICAiZGV0YWlscyIgOiB7CiAgICAgICJsaWFiaWxpdHlTaGlmdCIgOiAiTkEiLAogICAgICAibWVyY2hhbnRQcmVmZXJlbmNlIiA6ICJjaGFsbGVuZ2VfbWFuZGF0ZWQiCiAgIH0sCiAgICJwcm90b2NvbCIgOiAiM0RTZWN1cmUiLAogICAic3RhdHVzIiA6ICJub3RfZW5yb2xsZWQiLAogICAidmVyc2lvbiIgOiAiMi4yLjAiCn0K","MAC":"6B60B40BBE358A1D5665FE25D027C6215FF8063C"}';
+// Datos proporcionados para la prueba
+$securityKey = '255D023E7A0BDE9EEAC7516959CD93A9854F3991';
+$tpe = '7593339';
+$dateTime = new DateTime('now', new DateTimeZone('Europe/Paris'));
+$date = $dateTime->format('d/m/Y:H:i:s');
+$date_commande = $dateTime->format('d/m/Y');
+$montant = '10.00USD';
+$montant_recredit = '5.00USD';
+$reference = 'SUB1-1716474386';
+$version = '3.0';
+$lgue = 'FR';
+$societe = 'kalsteinfr';
 
-// Decode the JSON response
-$responseData = json_decode($jsonResponse, true);
-
-// Print the date value for debugging
-echo 'Date from JSON: ' . $responseData['date'] . '<br>';
-
-// Initialize Monetico with credentials
-$monetico = new Monetico(
-  '7593339',
-  '255D023E7A0BDE9EEAC7516959CD93A9854F3991',
-  'kalsteinfr'
-);
-
-// Clean the date string by removing '_a_' and replace it with a space
-$dateString = str_replace('_a_', ' ', trim($responseData['date']));
-
-// Print the cleaned date string for debugging
-echo 'Cleaned date string: ' . $dateString . '<br>';
-
-// Convert date string to DateTime object
-$orderDate = DateTime::createFromFormat('d/m/Y H:i:s', $dateString);
-if (!$orderDate) {
-  echo 'DateTime::getLastErrors(): ';
-  print_r(DateTime::getLastErrors());
-  die('Invalid order date format.');
-}
-
-// Print the orderDate for debugging
-echo 'Order Date: ';
-var_dump($orderDate);
-
-// Validate reference length
-$reference = $responseData['reference'];
-if (strlen($reference) > 50) {
-  die('Reference value is too long, should be 50 characters maximum: ' . $reference);
-}
-
-// Create the data array for RefundRequest
-$data = [
-  'dateTime' => new DateTime(),
-  'orderDate' => $orderDate,
-  'recoveryDate' => new DateTime(),
-  'authorizationNumber' => $responseData['vld'],
+$fields = [
+  'version' => $version,
+  'TPE' => $tpe,
+  'date' => $date,
+  'date_commande' => $date_commande,
+  'montant' => $montant,
+  'montant_recredit' => $montant_recredit,
   'reference' => $reference,
-  'language' => 'FR',
-  'currency' => 'USD',
-  'amount' => 2000, // Amount in cents (20 USD * 100)
-  'refundAmount' => 2000, // Refund the full amount, in cents
-  'maxRefundAmount' => 2000, // Maximum refund amount, in cents
+  'lgue' => $lgue,
+  'societe' => $societe
 ];
 
-// Print the data array for debugging
-echo 'Data array: ';
-var_dump($data);
+// Calcular el MAC
+function calculateMAC($securityKey, $fields)
+{
+  ksort($fields, SORT_STRING);
+  $dataString = '';
+  foreach ($fields as $key => $value) {
+    $dataString .= $key . '=' . $value . '*';
+  }
+  $dataString = rtrim($dataString, '*');
+  return strtoupper(hash_hmac('sha1', $dataString, pack('H*', $securityKey)));
+}
 
-// Create a RefundRequest with the required fields
-$refund = new RefundRequest($data);
+$mac = calculateMAC($securityKey, $fields);
+$fields['MAC'] = $mac;
 
-// Print the RefundRequest for debugging
-echo 'Refund Request: ';
-var_dump($refund);
+$url = "https://p.monetico-services.com/test/recredit_paiement.cgi";
 
-// Get the fields for the refund request
-$fields = $monetico->getFields($refund);
-
-// Print the fields for debugging
-echo 'Fields: ';
-var_dump($fields);
-
-// Send the refund request using GuzzleHttp Client
+// Enviar la solicitud de recrédito
 $client = new Client();
-$url = 'https://p.monetico-services.com/test/paiement.cgi'; // Monetico payment gateway URL
 $response = $client->request('POST', $url, [
   'form_params' => $fields
 ]);
 
-// Handle the response from Monetico
-$refundResponse = new RefundResponse($response);
-if ($refundResponse->isSuccess()) {
-  echo 'Refund successful!';
+$responseBody = $response->getBody()->getContents();
+
+if ($response->getStatusCode() == 200) {
+  echo "Recrédito realizado exitosamente: " . $responseBody;
 } else {
-  echo 'Refund failed: ' . $refundResponse->getMessage();
+  echo "Error al realizar el recrédito: " . $responseBody;
 }
