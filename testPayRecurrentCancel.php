76c81f23df8033a679cc568a99a2dd223cc0687b
Edithson <marioloquendero32@gmail.com>
2024-05-24 12:42:19 -0400

    HFDUFHUDYFH

--- testPayRecurrentCancel.php
+++ testPayRecurrentCancel.php
@@ -12,8 +12,7 @@ require '/home/kalsteinplus/public_html/dev.kalstein.plus/plataforma/wp-content/
 
 use GuzzleHttp\Client;
 
-function calculateMAC($securityKey, $fields)
-{
+function calculateMAC($securityKey, $fields) {
     ksort($fields, SORT_STRING);
 
     $dataString = '';
@@ -40,31 +39,50 @@ if (!preg_match('/^\d{2}\/\d{2}\/\d{4}$/', $date_commande)) {
 }
 
 // Resto de los parámetros
-$securityKey = '255D023E7A0BDE9EEAC7516959CD93A9854F3991'; // Asegúrate de que esta clave es correcta
+$securityKey = '255D023E7A0BDE9EEAC7516959CD93A9854F3991';
 $tpe = '7593339';
-$montant = '10.00USD'; // Asegúrate de que el formato es correcto y en USD
-$montant_a_capturer = '0.00USD';
-$montant_deja_capture = '0.00USD';
-$montant_restant = '0.00USD';
-$reference = 'SUB1-1716474386';
-$version = '3.0';
-$lgue = 'FR'; // Ajustado a FR según la documentación
-$societe = 'kalsteinfr';
-$stoprecurrence = 'OUI';
+$montant = '10USD'; // Asegúrate de que el formato es correcto y en USD
+$reference = 'SUB1-1716563690';
+$texte_libre = 'uniqid: c15a3f97b46c7ce010e5a49ec3b6b3a2664f7f01063ea1.38026363  userID:@valfonsob12';
+$code_retour = 'payetest';
+$cvx = 'oui';
+$vld = '1226';
+$brand = 'VI';
+$motifrefus = '';
+$usage = 'inconnu';
+$typecompte = 'inconnu';
+$ecard = 'non';
+$originecb = 'FRA';
+$bincb = '00000100';
+$hpancb = 'B552FD6DB65EC37C5B5C95F0E02A00841634AD86';
+$ipclient = '185.28.22.84';
+$originetr = 'USA';
+$cbmasquee = '00000100******21';
+$modepaiement = 'CB';
+$authentification = 'ewogICAiZGV0YWlscyIgOiB7CiAgICAgICJsaWFiaWxpdHlTaGlmdCIgOiAiTkEiLAogICAgICAibWVyY2hhbnRQcmVmZXJlbmNlIiA6ICJjaGFsbGVuZ2VfbWFuZGF0ZWQiCiAgIH0sCiAgICJwcm90b2NvbCIgOiAiM0RTZWN1cmUiLAogICAic3RhdHVzIiA6ICJub3RfZW5yb2xsZWQiLAogICAidmVyc2lvbiIgOiAiMi4yLjAiCn0K';
 
 $fields = [
-    'version' => $version,
     'TPE' => $tpe,
     'date' => $date,
-    'date_commande' => $date_commande,
     'montant' => $montant,
-    'montant_a_capturer' => $montant_a_capturer,
-    'montant_deja_capture' => $montant_deja_capture,
-    'montant_restant' => $montant_restant,
     'reference' => $reference,
-    'lgue' => $lgue,
-    'societe' => $societe,
-    'stoprecurrence' => $stoprecurrence
+    'texte-libre' => $texte_libre,
+    'code-retour' => $code_retour,
+    'cvx' => $cvx,
+    'vld' => $vld,
+    'brand' => $brand,
+    'motifrefus' => $motifrefus,
+    'usage' => $usage,
+    'typecompte' => $typecompte,
+    'ecard' => $ecard,
+    'originecb' => $originecb,
+    'bincb' => $bincb,
+    'hpancb' => $hpancb,
+    'ipclient' => $ipclient,
+    'originetr' => $originetr,
+    'cbmasquee' => $cbmasquee,
+    'modepaiement' => $modepaiement,
+    'authentification' => $authentification
 ];
 
 $mac = calculateMAC($securityKey, $fields);
@@ -80,24 +98,32 @@ $response = $client->request('POST', $url, [
 ]);
 
 $responseBody = $response->getBody()->getContents();
+$responseStatusCode = $response->getStatusCode();
 
-if ($response->getStatusCode() == 200) {
+echo "Solicitud Enviada:<br>";
+foreach ($fields as $key => $value) {   
+    echo $key . ': ' . htmlspecialchars($value) . '<br>';
+}
+
+echo "<br>MAC Calculada: " . $mac . "<br>";
+
+echo "<br>Respuesta del Servidor:<br>";
+echo "Código de Estado: " . $responseStatusCode . "<br>";
+echo "Cuerpo de la Respuesta: " . htmlspecialchars($responseBody) . "<br>";
+
+if ($responseStatusCode == 200) {
     echo "Pago recurrente cancelado exitosamente.";
 } else {
     echo "Error al cancelar el pago recurrente: " . $responseBody;
 }
 ?>
 <html>
-
 <body onload="document.forms['cancel_form'].submit();">
     <form name="cancel_form" action="<?php echo $url; ?>" method="post">
-        <?php foreach ($fields as $key => $value) : ?>
+        <?php foreach ($fields as $key => $value): ?>
             <input type="hidden" name="<?php echo $key; ?>" value="<?php echo htmlspecialchars($value); ?>">
         <?php endforeach; ?>
-        <center>
-            <div class="custom-loader"></div>
-        </center>
+        <center><div class="custom-loader"></div></center>
     </form>
 </body>
-
 </html>
