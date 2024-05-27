556adf14a655731193f564bdc68f43a3144d76bb
Diego Horesok <diegolancer27@gmail.com>
2024-05-27 11:36:57 -0400

    prueba...

---
+++ testQuerys.php
@@ -0,0 +1,27 @@
+<?php
+// ERROR DETECTION LINES.
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require '/home/kalsteinplus/public_html/dev.kalstein.plus/plataforma/wp-content/plugins/kalsteinPerfiles/php/conexion.php';
+session_start();
+
+if (isset($_SESSION["emailAccount"])) {
+  $email = $_SESSION["emailAccount"];
+}
+
+
+$sql = "SELECT * FROM wp_account WHERE account_email = '$email'";
+$result = $conexion->query($sql);
+$row = $result->fetch_assoc();
+$id = $row['account_aid'];
+
+
+
+$sqlSubscripcion = "SELECT * FROM wp_subscripcion WHERE user_id = '$id'";
+
+$result2 = $conexion->query($sqlSubscripcion);
+$row = $result2->fetch_assoc();
+
+var_dump('aqui van', $row);
