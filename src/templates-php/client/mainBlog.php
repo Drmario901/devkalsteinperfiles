6e6fcd4223a31b9f5fef8105ab4fc95047596c08
Alejandro <tzareg96@gmail.com>
2024-05-27 14:03:40 -0400

    ihogyitfuyiipoijbytvycrfhjbkljhgvfyguhbjl

--- src/templates-php/client/mainBlog.php
+++ src/templates-php/client/mainBlog.php
@@ -1,25 +1,4 @@
 <div id='c-panel20'>
-    <?php
-			// Incluir el archivo de traducciones
-			include __DIR__.'/../../../php/translations.php';
-			
-			$banner_img = 'Header-usuario-IMG.png';
-
-			// Obtener el idioma del cookie
-			$language = isset($_COOKIE['language']) ? $_COOKIE['language'] : 'en';
-
-			// Obtener el texto del banner según el idioma
-			$banner_text_key = 'banner_text_blog'; // Clave para el texto de los catálogos (por defecto)
-			if (isset($translations[$language]['banner_text_blog'])) {
-				$banner_text_key = 'banner_text_blog';
-			}
-
-			// Obtener el texto del banner
-			$banner_text = isset($translations[$language][$banner_text_key]) ? $translations[$language][$banner_text_key] : 'Blog';
-
-			include 'banner.php';
-		?>
-
     <style>
     @media (max-width: 768px) {
         .contenedor_guiasInf {
