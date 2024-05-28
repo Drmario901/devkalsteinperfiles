afff7dd2f7827994f6231861684db7075aba13e4
Alejandro <tzareg96@gmail.com>
2024-05-28 14:38:42 -0400

    aiosjdasoujdlasljd

--- src/js/blog.js
+++ src/js/blog.js
@@ -123,10 +123,10 @@ jQuery(document).ready(function($) {
                         <div class="footer-guia" style="display: flex; align-items: center; justify-content: space-between">
                             <button class="btn_guias_informativas" id="blog_articulos" source="#">Ver más <i class="fa-solid fa-arrow-right"></i></button>
                             <p style="font-family: Roboto; margin: 0; display: flex;">
-                                <svg style="display: inline; width:15px; height:15px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                <svg style="display: inline; width:15px; height:15px; margin-top: 5px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                                     <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/>
                                 </svg>
-                                Publicado por <span class='txt-author'><b>${author}</b><span>
+                                Publicado por <span class='txt-author'><b> ${author}</b><span>
                             </p>
                         </div>
                     </div>
