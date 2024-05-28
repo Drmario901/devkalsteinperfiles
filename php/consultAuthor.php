74f0c3cbaf7129da247c6f760270a7a5ecd83ddf
Alejandro <tzareg96@gmail.com>
2024-05-28 12:36:28 -0400

    uiyhjnpaohusyvigfkaskiduhibasndjask

---
+++ php/consultAuthor.php
@@ -0,0 +1,22 @@
+<?php
+    require_once __DIR__ . '/../db/conexion.php';
+
+    $id = $_POST['id'];
+
+    $sql = "SELECT * FROM wp_account WHERE account_aid = '$id'";
+
+    $resultado = $conexion->query($sql);
+    $row = mysqli_fetch_array($resultado);
+    $correo = $row['account_correo'];
+
+    $sql2 = "SELECT * FROM tienda_virtual WHERE ID_user = '$correo'";
+    $resultado2 = $conexion->query($sql2);
+    $row2 = mysqli_fetch_array($resultado2);
+    $store = $row2['titulo_t'];
+
+    // header('Content-Type: application/json');
+    // echo json_encode($datos);
+
+    echo json_encode([
+        'store' => $store
+    ]);
