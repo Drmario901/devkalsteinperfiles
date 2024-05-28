9384ca022cfb73c0a46b3e31d6e3dee063dc567e
VicAlarDev <vic.alarcondev@gmail.com>
2024-05-28 12:28:20 -0400

    aaaa

--- checkAndUpdate.php
+++ checkAndUpdate.php
@@ -1,7 +1,7 @@
 <?php
-ini_set('display_errors', 1);
+/* ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL); */
 // Crear la carpeta 'monetico' si no existe
 $logDir = __DIR__ . '/monetico';
 if (!is_dir($logDir)) {
