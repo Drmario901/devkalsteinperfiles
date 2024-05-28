cbe1b61b99875d26ab86b28541e3060b50ed729a
Alejandro <tzareg96@gmail.com>
2024-05-28 16:09:01 -0400

    iabhsjbdnkalsjbhdknalskdjbhkasnlkdask

--- src/templates-php/client/mainBlog.php
+++ src/templates-php/client/mainBlog.php
@@ -185,66 +185,6 @@
                                 }
                             }
                         ?>
-                            <div class="contenedor_vistaprevia_destacados btn_ver_articulo"
-                                style="padding: 10px; border-bottom: solid 1px #c9c9c9; cursor: pointer">
-                                <div class="thumbnail_guia">
-                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzXZEbAw2rTlg6VfF0t7bVOTESl1YMUmp2QvwDtar4SQ&s"
-                                        alt="guia" width='150' />
-                                </div>
-                                <div class="contenido_guia" style="margin-top: 1em">
-                                    <h5 class="titulo_guia" style="font-family: Montserrat;">Titulo del articulo
-                                    </h5>
-                                    <p style="font-family: Roboto; margin: 0">
-                                        <svg style="display: inline; width:15px; height:15px;"
-                                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                                            <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-                                            <path
-                                                d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
-                                        </svg>
-                                        Publicado por <b>Autor</b>
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="contenedor_vistaprevia_destacados btn_ver_articulo"
-                                style="padding: 10px; border-bottom: solid 1px #c9c9c9; cursor: pointer">
-                                <div class="thumbnail_guia">
-                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzXZEbAw2rTlg6VfF0t7bVOTESl1YMUmp2QvwDtar4SQ&s"
-                                        alt="guia" width='150' />
-                                </div>
-                                <div class="contenido_guia" style="margin-top: 1em">
-                                    <h5 class="titulo_guia" style="font-family: Montserrat;">Titulo del articulo
-                                    </h5>
-                                    <p style="font-family: Roboto; margin: 0">
-                                        <svg style="display: inline; width:15px; height:15px;"
-                                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                                            <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-                                            <path
-                                                d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
-                                        </svg>
-                                        Publicado por <b>Autor</b>
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="contenedor_vistaprevia_destacados btn_ver_articulo"
-                                style="padding: 10px; border-bottom: solid 1px #c9c9c9; cursor: pointer">
-                                <div class="thumbnail_guia">
-                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzXZEbAw2rTlg6VfF0t7bVOTESl1YMUmp2QvwDtar4SQ&s"
-                                        alt="guia" width='150' />
-                                </div>
-                                <div class="contenido_guia" style="margin-top: 1em">
-                                    <h5 class="titulo_guia" style="font-family: Montserrat;">Titulo del articulo
-                                    </h5>
-                                    <p style="font-family: Roboto; margin: 0">
-                                        <svg style="display: inline; width:15px; height:15px;"
-                                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                                            <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-                                            <path
-                                                d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
-                                        </svg>
-                                        Publicado por <b>Autor</b>
-                                    </p>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
