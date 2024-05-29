9bc7ddad1ab4bc1c9cc279609e150707517f0d7b
VicAlarDev <vic.alarcondev@gmail.com>
2024-05-29 14:16:04 -0400

    aaaaaaaa

--- src/templates-php/moderator/barbacoas.php
+++ src/templates-php/moderator/barbacoas.php
@@ -65,7 +65,7 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <p class='mt-2 mb-1' style='font-size: 1.15em'><i class='fa-solid fa-user'></i> <b>Published by:</b> user@mail.com</p>
+                                    <p class='mt-2 mb-1' style='font-size: 1.15em'><i class='fa-solid fa-user'></i> <b>Published by:</b> $guideMaker</p>
                                     <div class='col-12'>
                                         <a href='https://dev.kalstein.plus/plataforma/index.php/moderator/view-guide?accid=$guideId'>
                                             <button type='button' id='btnUpdate' class='btn btn-info btn-block p-2 px-4'>Check</button>
