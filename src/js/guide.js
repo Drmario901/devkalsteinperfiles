2a48c7f7389a8c9d00fc3fb295319bf6756f264d
Alejandro <tzareg96@gmail.com>
2024-05-29 14:57:08 -0400

    iajdilasdknasjbhjfnkñasdoapindk

--- src/js/guide.js
+++ src/js/guide.js
@@ -89,24 +89,18 @@ jQuery(document).ready(function($) {
                             <h5 class="titulo_guia" style="font-family: Montserrat; padding: 0">${categorie}</h5>
                             <hr
                                 style="height:3px; width:75px; border:none; color:#213280; background-color:#213280; opacity: 1; margin: 10px 0;">
-                            <p style="font-family: Roboto; line-height: 1.5em; margin-bottom: 10px">Lorem ipsum
-                                dolor sit
-                                amet, consectetur
-                                adipiscing elit. Cras in mauris vitae justo vehicula viverra in in nulla. Vivamus at
-                                pretium velit. Suspendisse mollis eros sit amet ultrices gravida. Cras libero ipsum,
-                                ultricies vel est quis, condimentum ultrices ante. Ut maximus velit quis neque
-                                auctor, quis auctor neque tristique.</p>
+                            <p style="font-family: Roboto; line-height: 1.5em; margin-bottom: 10px">${description}.</p>
                             <div class="footer-guia"
                                 style="display: flex; align-items: center; justify-content: space-between">
                                 <button class="btn_guias_informativas ._df_button" id="guias_informativas" source="#">Ver mas</button>
-                                <p style="font-family: Roboto; margin: 0">
-                                    <svg style="display: inline; width:15px; height:15px;"
+                                <p style="font-family: Roboto; margin: 0; display: flex;">
+                                    <svg style="display: inline; width:15px; height:15px; margin-top: 5px;"
                                         xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                                         <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                                         <path
                                             d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
                                     </svg>
-                                    Publicado por <b>Autor</b>
+                                    &nbsp;Publicado por&nbsp;<span class='txt-author author-store' meta-href="${slug}">${author}<span>
                                 </p>
                             </div>
                         </div>
