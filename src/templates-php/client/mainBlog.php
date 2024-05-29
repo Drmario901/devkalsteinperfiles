d4cc13791064baefb517e7521e458425812a3ff4
Alejandro <tzareg96@gmail.com>
2024-05-28 20:09:41 -0400

    ubhiaosoduybiasodoasidbhjasighdjjansigd

--- src/templates-php/client/mainBlog.php
+++ src/templates-php/client/mainBlog.php
@@ -151,15 +151,15 @@
                             if ($resultado->num_rows > 0){
                                 while($fila = $resultado->fetch_assoc()) {
                                     $id = $fila['art_id_user'];
-                                    $sql = "SELECT * FROM wp_account WHERE account_aid = '$id'";
-                                    $resultado = $conexion->query($sql);
-                                    $row = mysqli_fetch_array($resultado);
-                                    $correo = $row['account_correo'];
-
-                                    $sql2 = "SELECT * FROM tienda_virtual WHERE ID_user = '$correo'";
+                                    $sql2 = "SELECT * FROM wp_account WHERE account_aid = '$id'";
                                     $resultado2 = $conexion->query($sql2);
                                     $row2 = mysqli_fetch_array($resultado2);
-                                    $store = $row2[2] ?? 'No tiene tienda';
+                                    $correo = $row2['account_correo'];
+
+                                    $sql3 = "SELECT * FROM tienda_virtual WHERE ID_user = '$correo'";
+                                    $resultado3 = $conexion->query($sql3);
+                                    $row3 = mysqli_fetch_array($resultado3);
+                                    $store = $row3[2];
 
                                     echo '
                                         <div class="contenedor_vistaprevia_destacados btn_ver_articulo"
