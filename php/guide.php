cc0688db5c24e2b9570224c825d5d0f96fc78c36
Alejandro <tzareg96@gmail.com>
2024-05-29 13:40:26 -0400

    oigtcdfghjbkjluotucydfhghbjligfh

--- php/guide.php
+++ php/guide.php
@@ -1,9 +1,7 @@
 <?php
     require_once __DIR__ . '/../db/conexion.php';
 
-    $sql = "SELECT * FROM wp_guides
-        FULL JOIN wp_guides_details
-        ON wp_guides.guide_id = wp_guides_details.guide_id";
+    $sql = "SELECT * FROM wp_guides WHERE guide_status_id = '1'";
 
     $resultado = $conexion->query($sql);
 
