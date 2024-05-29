f2844c6001de057576616323fb111a8c497d4606
Alejandro <tzareg96@gmail.com>
2024-05-28 20:16:26 -0400

    huobhknasñdophuasknd ipas

--- php/blog.php
+++ php/blog.php
@@ -1,7 +1,7 @@
 <?php
     require_once __DIR__ . '/../db/conexion.php';
 
-    $sql = "SELECT * FROM wp_art_blog";
+    $sql = "SELECT * FROM wp_art_blog WHERE id_status = '1'";
 
     $resultado = $conexion->query($sql);
 
--- src/templates-php/client/mainBlog.php
+++ src/templates-php/client/mainBlog.php
@@ -114,7 +114,7 @@
     <?php
         require_once '/home/kalsteinplus/public_html/dev.kalstein.plus/plataforma/wp-content/plugins/kalsteinPerfiles/db/conexion.php';
 
-        $sql = "SELECT * FROM wp_art_blog ORDER BY art_views DESC LIMIT 3";
+        $sql = "SELECT * FROM wp_art_blog WHERE id_status = '1' ORDER BY art_views DESC LIMIT 3";
         $resultado = $conexion->query($sql);
     ?>
 
