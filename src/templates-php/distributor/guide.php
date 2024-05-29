94f9afaefb99072772fdb542f5ddf0134d652008
Alejandro <tzareg96@gmail.com>
2024-05-29 15:26:11 -0400

    jbk avsndioasbhkdkasnbhkdnasmknbhk dasndk

--- src/templates-php/distributor/guide.php
+++ src/templates-php/distributor/guide.php
@@ -4,7 +4,7 @@
             include 'navbar.php';
         ?>
         <script>
-            let page = "mail";
+            let page = "Guías Informativas";
 
             document.querySelector('#link-' + page).classList.add("active");
             document.querySelector('#link-' + page).removeAttribute("style");
@@ -13,12 +13,13 @@
 
     <article class="container article">
         <?php
-            $banner_img = 'Header-fabricante-IMG.png';
+            $banner_img = 'Header-distribuidor-IMG.jpg';
 
             require __DIR__. '/../../../php/translateTextBanner.php';
-            $banner = 'banner_text_ConnectWithOtherUsers';
-            $banner_text = translateTextBanner($banner);
-            include 'banner.php';
+            // $banner = 'banner_text_ConnectWithOtherUsers';
+            // $banner_text = translateTextBanner($banner);
+            $banner_text = 'Guías Informativas';
+            include __DIR__.'/../manufacturer/banner.php';
         ?>
         
         <main>
