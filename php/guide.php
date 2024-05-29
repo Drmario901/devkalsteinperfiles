4becc16a157825d8c15211407254ea221f876d44
Alejandro <tzareg96@gmail.com>
2024-05-29 13:34:07 -0400

    JHFJKSHFKJDF

--- php/guide.php
+++ php/guide.php
@@ -2,7 +2,7 @@
     require_once __DIR__ . '/../db/conexion.php';
 
     $sql = "SELECT * FROM wp_guides
-        LEFT JOIN wp_guides_details
+        FULL JOIN wp_guides_details
         ON wp_guides.guide_id = wp_guides_details.guide_id";
 
     $resultado = $conexion->query($sql);
