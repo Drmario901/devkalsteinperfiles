33f6b7fcbec9194220be19beba040495627405a8
VicAlarDev <vic.alarcondev@gmail.com>
2024-05-29 12:06:16 -0400

    aaaaaaaaaaaa

--- src/templates-php/moderator/blog.php
+++ src/templates-php/moderator/blog.php
@@ -26,9 +26,11 @@
             SELECT wp_art_blog.*, wp_account.*, 8x7MM_posts.*
             FROM kalsteinplus_he270716_wp_es.wp_art_blog
             INNER JOIN kalsteinplus_he270716_wp_es.wp_account ON wp_art_blog.art_id_user = wp_account.account_aid
-            INNER JOIN kalsteinplus_plataforma_desarrollo.8x7MM_posts ON wp_art_blog.title_slug = 8x7MM_posts.post_title
+            INNER JOIN kalsteinplus_plataforma_desarrollo.8x7MM_posts 
+                ON wp_art_blog.title_slug COLLATE utf8mb4_unicode_ci = 8x7MM_posts.post_title COLLATE utf8mb4_unicode_ci
             WHERE 8x7MM_posts.post_status = 'draft'
             ORDER BY wp_art_blog.art_id_user DESC;
+            ;
         ";
 
             $result = $conexion->query($query);
