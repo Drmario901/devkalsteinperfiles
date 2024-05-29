bb85cf7650508d2c4421f7e6a0283ac733a9e73e
katherineleald <katleald0412@gmail.com>
2024-05-29 14:05:05 -0400

    prueba

--- src/templates-php/distributor/subscripcion.php
+++ src/templates-php/distributor/subscripcion.php
@@ -147,6 +147,12 @@ html a:hover,
     document.querySelector('#link-' + page).classList.add("active");
     document.querySelector('#link-' + page).removeAttribute("style");
     </script>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
 </header>
 
 <section style="margin-top: 2rem;">
