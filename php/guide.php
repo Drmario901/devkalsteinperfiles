d90c835aad833be5167967eb2814b886519abbed
Alejandro <tzareg96@gmail.com>
2024-05-29 13:31:16 -0400

    oicxfghgjllcf

--- php/guide.php
+++ php/guide.php
@@ -1,9 +1,9 @@
 <?php
     require_once __DIR__ . '/../db/conexion.php';
 
-    $sql = "SELECT * FROM wp_guides
-        INNER JOIN wp_guides_details
-        ON wp_guides.guide_id = wp_guides_details.guide_id";
+    $sql = "SELECT * FROM wp_guides AS a
+        FULL JOIN wp_guides_details AS d
+        ON a.guide_id = d.guide_id";
 
     $resultado = $conexion->query($sql);
 
