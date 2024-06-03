94b0e6073c61b9c8d2c0028fb5faac07a5f91a62
VicAlarDev <vic.alarcondev@gmail.com>
2024-06-03 11:51:46 -0400

    aaaaaaa

--- src/js/register.script1.js
+++ src/js/register.script1.js
@@ -1,6 +1,20 @@
 jQuery(document).ready(function($){
 
-    if(cookieMembersia == '1'){
+    function getCookie(name) {
+        let cookieArr = document.cookie.split(";");
+        for(let i = 0; i < cookieArr.length; i++) {
+            let cookiePair = cookieArr[i].split("=");
+            if(name == cookiePair[0].trim()) {
+                return decodeURIComponent(cookiePair[1]);
+            }
+        }
+        return null;
+    }
+
+    // Obtener la cookie 'membersia'
+    let cookieMembersia = getCookie('membersia');
+
+    if(!cookieMembersia){
         document.getElementById('cmbPrimary').value = '2';
         document.getElementById('profileRole').value = '2';
         
