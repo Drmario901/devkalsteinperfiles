d05e83ef474956288e962e654833c1e2a169f5ff
Diego Horesok <diegolancer27@gmail.com>
2024-06-03 09:13:53 -0400

    prueba

--- src/js/diego.js
+++ src/js/diego.js
@@ -1,22 +1,38 @@
-jQuery(document).ready(function($){
-    $(document).on('click', '#hola', function (){
-        console.log('hola');
-        
-        iziToast.success({
-            title : 'Success',
-            message : 'hola',
-            position: 'center'
-          });
+jQuery(document).ready(function ($) {
+  $(document).on("click", "#hola", function () {
+    console.log("hola");
+
+    iziToast.success({
+      title: "Success",
+      message: "hola",
+      position: "center",
     });
+  });
 
-    const elboton = document.querySelector('#hola');
-    console.log('elboton', elboton);
-    let algo = () => {
-        console.log('algo');
+  const elboton = document.querySelector("#hola");
+  console.log("elboton", elboton);
+  let algo = () => {
+    console.log("algo");
+  };
+  algo();
+
+  function getCookie(name) {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == " ") c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
     }
-    algo();
+    return null;
+  }
 
-    
-
-
+  document.addEventListener("DOMContentLoaded", function () {
+    let cookieValue = getCookie("roll_usuario");
+    if (cookieValue) {
+      console.log('El valor de la cookie "usuario" es: ' + cookieValue);
+    } else {
+      console.log('La cookie "usuario" no está configurada.');
+    }
+  });
 });
--- src/templates-php/diego-index.php
+++ src/templates-php/diego-index.php
@@ -1,2 +1,12 @@
+<?php
+$nombre_cookie = "roll_usuario";
+$valor_cookie = "3";
+$tiempo_expiracion = time() + (86400 * 30); // La cookie expirará en 30 días
+
+// Guardar la cookie
+setcookie($nombre_cookie, $valor_cookie, $tiempo_expiracion, "/");
+?>
+
 <h1>hola</h1>
-<button id="hola">Hola</button>
+
+<button id="hola" value="<?php echo $valor_cookie ?>">Hola</button>
