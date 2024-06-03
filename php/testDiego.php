6374f376e6a2c2f92aea6a5d83dd71256ebd902b
Diego Horesok <diegolancer27@gmail.com>
2024-06-03 09:24:43 -0400

    aasda

---
+++ php/testDiego.php
@@ -0,0 +1,13 @@
+<?php
+$nombre_cookie = "roll_usuario";
+$valor_cookie = "3";
+$tiempo_expiracion = time() + (86400 * 30); // La cookie expirará en 30 días
+
+// Guardar la cookie
+setcookie($nombre_cookie, $valor_cookie, $tiempo_expiracion, "/");
+
+if (isset($_COOKIE[$nombre_cookie])) {
+  echo "La cookie '" . $nombre_cookie . "' se ha establecido correctamente con el valor '" . $_COOKIE[$nombre_cookie] . "'.";
+} else {
+  echo "No se pudo establecer la cookie.";
+}
