484ac78f31a1d586af9c7e031bc56fe79bc21a72
Diego Horesok <diegolancer27@gmail.com>
2024-06-03 09:20:21 -0400

    adasd

--- src/templates-php/diego-index.php
+++ src/templates-php/diego-index.php
@@ -5,7 +5,14 @@ $tiempo_expiracion = time() + (86400 * 30); // La cookie expirará en 30 días
 
 // Guardar la cookie
 setcookie($nombre_cookie, $valor_cookie, $tiempo_expiracion, "/");
+
+if (isset($_COOKIE[$nombre_cookie])) {
+  echo "La cookie '" . $nombre_cookie . "' se ha establecido correctamente con el valor '" . $_COOKIE[$nombre_cookie] . "'.";
+} else {
+  echo "No se pudo establecer la cookie.";
+}
 ?>
+
 
 <h1>hola</h1>
 
