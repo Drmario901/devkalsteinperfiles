67412015d9bc7bef691cc7a6c2588f6d62448807
VicAlarDev <vic.alarcondev@gmail.com>
2024-06-03 09:43:31 -0400

    aaaaaaaaa

--- php/distributor/uploadProductData.php
+++ php/distributor/uploadProductData.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
 
-ini_set('display_errors', 1);
+/* ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL); */
 
-require_once __DIR__ . '/../../db/conexion.php';
+require_once __DIR__ . '/../conexion.php';
 require __DIR__ . '/validateProductData.php';
 
 $acc_id = $_SESSION['emailAccount'];
